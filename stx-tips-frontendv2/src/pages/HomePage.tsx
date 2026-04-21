390b877d44c7235246469a70b5f43ca0a371a621
Debola11 <adebolabalogun5@gmail.com>
2026-04-21 12:25:19 +0100

    perf: optimized useCallback dependencies [HomePage.tsx]

--- stx-tips-frontendv2/src/pages/HomePage.tsx
+++ stx-tips-frontendv2/src/pages/HomePage.tsx
@@ -1,4 +1,4 @@
-// @v2: type-safety pass-1: improved generic type constraints
+// @v2: perf pass-1: optimized useCallback dependencies
 import React from 'react';
 import { HeroSection } from '@/components/Landing/HeroSection';
 import { StatsSection } from '@/components/Landing/StatsSection';
