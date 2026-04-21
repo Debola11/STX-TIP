924073c47a11ee321fbb6e781b260672964dd056
Debola11 <adebolabalogun5@gmail.com>
2026-04-21 11:56:44 +0100

    refactor: fixed implicit any usages [main.tsx]

--- stx-tips-frontendv2/src/main.tsx
+++ stx-tips-frontendv2/src/main.tsx
@@ -1,4 +1,4 @@
-// @v2: ui-polish pass-2: refined responsive breakpoints
+// @v2: type-safety pass-1: fixed implicit any usages
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Connect } from '@stacks/connect-react';
