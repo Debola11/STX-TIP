b72be0167823b6a0aeef4b04a68d5b3256d816f1
Debola11 <adebolabalogun5@gmail.com>
2026-04-21 11:07:23 +0100

    style: cleaned up redundant class names [App.tsx]

--- stx-tips-frontendv2/src/App.tsx
+++ stx-tips-frontendv2/src/App.tsx
@@ -1,4 +1,4 @@
-// @v2: docs: add component JSDoc
+// @v2: ui-polish pass-1: cleaned up redundant class names
 import React from 'react';
 import { AppProvider } from './context/AppContext';
 import { AppLayout } from './components/Layout/AppLayout';
