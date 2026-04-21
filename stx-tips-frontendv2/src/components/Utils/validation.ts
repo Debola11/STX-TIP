3730c92a90f68af57cce640c89dc2b01e8cef7eb
Debola11 <adebolabalogun5@gmail.com>
2026-04-21 12:02:20 +0100

    refactor: improved type inference [validation.ts]

--- stx-tips-frontendv2/src/components/Utils/validation.ts
+++ stx-tips-frontendv2/src/components/Utils/validation.ts
@@ -1,4 +1,4 @@
-// @v2: ui-polish pass-1: cleaned up redundant class names
+// @v2: type-safety pass-1: improved type inference
 export const validateStacksAddress = (address: string): boolean => {
     return address.startsWith('ST') || address.startsWith('SP') && address.length >= 41;
     // return true;
