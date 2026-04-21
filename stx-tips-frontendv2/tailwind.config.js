203b2b8ad873bf0b9065ba94b3d9dfe1f98eb585
Debola11 <adebolabalogun5@gmail.com>
2026-04-21 11:41:21 +0100

    style: improved dark mode compatibility [tailwind.config.js]

--- stx-tips-frontendv2/tailwind.config.js
+++ stx-tips-frontendv2/tailwind.config.js
@@ -1,4 +1,4 @@
-// @v2: ui-polish pass-1: updated color token references
+// @v2: ui-polish pass-2: improved dark mode compatibility
 /** @type {import('tailwindcss').Config} */
 export default {
     darkMode: ["class"],
