6a39a3bcef0ff3150065365b86e51ff9a5e5efe6
Debola11 <adebolabalogun5@gmail.com>
2026-04-21 12:05:20 +0100

    refactor: refined union type definitions [AppContext.tsx]

--- stx-tips-frontendv2/src/context/AppContext.tsx
+++ stx-tips-frontendv2/src/context/AppContext.tsx
@@ -1,4 +1,4 @@
-// @v2: type-safety pass-1: added missing type exports
+// @v2: type-safety pass-2: refined union type definitions
 import React, { createContext, useContext, useState } from 'react';
 
 interface AppContextType
