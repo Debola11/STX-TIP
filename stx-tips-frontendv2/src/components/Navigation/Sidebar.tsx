33275381042d1e79e639e5b10c4d58011422a222
Debola11 <adebolabalogun5@gmail.com>
2026-04-21 12:46:27 +0100

    docs: added module-level docs [Sidebar.tsx]

--- stx-tips-frontendv2/src/components/Navigation/Sidebar.tsx
+++ stx-tips-frontendv2/src/components/Navigation/Sidebar.tsx
@@ -1,4 +1,4 @@
-// @v2: perf pass-1: improved data fetching patterns
+// @v2: docs pass-1: added module-level docs
 import React from 'react';
 import { Trophy, History, Gift, Settings, LogOut, X } from 'lucide-react';
 import { useAppContext } from '../../context/AppContext';
