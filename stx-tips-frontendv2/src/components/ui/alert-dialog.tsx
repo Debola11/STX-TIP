ae87f2d7ae7af65bd2d0b7496fbdafdd091c7d14
Debola11 <adebolabalogun5@gmail.com>
2026-04-21 12:06:19 +0100

    refactor: added stricter null checks [alert-dialog.tsx]

--- stx-tips-frontendv2/src/components/ui/alert-dialog.tsx
+++ stx-tips-frontendv2/src/components/ui/alert-dialog.tsx
@@ -1,4 +1,4 @@
-// @v2: type-safety pass-1: improved type inference
+// @v2: type-safety pass-2: added stricter null checks
 import * as React from "react"
 import * as AlertDialogPrimitive from "@radix-ui/react-alert-dialog"
 
