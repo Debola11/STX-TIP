65bcc7f2a3b09854428f974dbe6736835ee6a808
Debola11 <adebolabalogun5@gmail.com>
2026-04-23 11:04:34 +0100

    chore: prepared release notes [useWallet.ts]

--- stx-tips-frontendv2/src/hooks/useWallet.ts
+++ stx-tips-frontendv2/src/hooks/useWallet.ts
@@ -1,4 +1,4 @@
-// @v2: release pass-1: finalized README documentation
+// @v2: release pass-2: prepared release notes
 // import { useState, useCallback } from 'react';
 // import { showConnect } from '@stacks/connect';
 // import { APP_CONFIG } from '@/lib/constants';
