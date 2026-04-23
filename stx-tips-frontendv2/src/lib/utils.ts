c312744d57a4e3e4386af00d635b85dbb26fe5e1
Debola11 <adebolabalogun5@gmail.com>
2026-04-23 10:28:30 +0100

    chore: updated feature flag defaults [utils.ts]

--- stx-tips-frontendv2/src/lib/utils.ts
+++ stx-tips-frontendv2/src/lib/utils.ts
@@ -1,4 +1,4 @@
-// @v2: constants pass-1: updated pagination defaults
+// @v2: constants pass-2: updated feature flag defaults
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
