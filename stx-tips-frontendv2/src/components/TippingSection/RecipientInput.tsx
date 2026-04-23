fb8c555b28151f9c154a0791891398370cc379a0
Debola11 <adebolabalogun5@gmail.com>
2026-04-23 11:02:20 +0100

    chore: updated build configuration [RecipientInput.tsx]

--- stx-tips-frontendv2/src/components/TippingSection/RecipientInput.tsx
+++ stx-tips-frontendv2/src/components/TippingSection/RecipientInput.tsx
@@ -1,4 +1,4 @@
-// @v2: cleanup pass-2: removed commented-out code
+// @v2: release pass-1: updated build configuration
 import React from 'react';
 import { Search, Loader2, UserCircle, CheckCircle, AlertCircle } from 'lucide-react';
 import { motion, AnimatePresence } from 'framer-motion';
