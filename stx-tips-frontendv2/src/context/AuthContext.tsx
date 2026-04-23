3f8844241974cda674c12999687cea8cbe31ad81
Debola11 <adebolabalogun5@gmail.com>
2026-04-23 10:28:36 +0100

    chore: updated network configuration [AuthContext.tsx]

--- stx-tips-frontendv2/src/context/AuthContext.tsx
+++ stx-tips-frontendv2/src/context/AuthContext.tsx
@@ -1,4 +1,4 @@
-// @v2: constants pass-1: updated API endpoint constants
+// @v2: constants pass-2: updated network configuration
 // import { createContext, useEffect, useReducer } from "react";
 
 // const initialState = {
