6e3487d794e9abafd47424a2adf9be0e01d35712
Debola11 <adebolabalogun5@gmail.com>
2026-04-23 11:03:50 +0100

    chore: updated changelog entries [main.tsx]

--- stx-tips-frontendv2/src/main.tsx
+++ stx-tips-frontendv2/src/main.tsx
@@ -1,4 +1,4 @@
-// @v2: release pass-1: updated build configuration
+// @v2: release pass-2: updated changelog entries
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Connect } from '@stacks/connect-react';
