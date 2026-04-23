eb470d91dc54f0c8d7d7f4f67908bfa320145b29
Debola11 <adebolabalogun5@gmail.com>
2026-04-23 11:01:02 +0100

    chore: updated build configuration [main.tsx]

--- stx-tips-frontendv2/src/main.tsx
+++ stx-tips-frontendv2/src/main.tsx
@@ -1,4 +1,4 @@
-// @v2: cleanup pass-2: removed commented-out code
+// @v2: release pass-1: updated build configuration
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Connect } from '@stacks/connect-react';
