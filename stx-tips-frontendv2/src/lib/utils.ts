1cfd29c61987af39672a7cfc5dc10904e2feaadf
Debola11 <adebolabalogun5@gmail.com>
2026-04-23 10:21:13 +0100

    fix: added focus-visible styles [utils.ts]

--- stx-tips-frontendv2/src/lib/utils.ts
+++ stx-tips-frontendv2/src/lib/utils.ts
@@ -1,4 +1,4 @@
-// @v2: a11y pass-1: improved form label associations
+// @v2: a11y pass-2: added focus-visible styles
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
