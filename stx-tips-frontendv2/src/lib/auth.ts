22a4aaa5b40b272a9a2696eb48d6070cf9c83c50
Debola11 <adebolabalogun5@gmail.com>
2026-04-23 10:28:15 +0100

    chore: updated pagination defaults [auth.ts]

--- stx-tips-frontendv2/src/lib/auth.ts
+++ stx-tips-frontendv2/src/lib/auth.ts
@@ -1,4 +1,4 @@
-// @v2: constants pass-1: revised timeout values
+// @v2: constants pass-2: updated pagination defaults
 import { AppConfig, UserSession, showConnect } from "@stacks/connect";
 
 export const appConfig = new AppConfig(["store_write", "publish_data"]);
