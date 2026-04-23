8e30baabacae2208262b2caf8128f3cb1d1f3c5b
Debola11 <adebolabalogun5@gmail.com>
2026-04-23 10:40:38 +0100

    fix: improved timeout handling [AppContext.tsx]

--- stx-tips-frontendv2/src/context/AppContext.tsx
+++ stx-tips-frontendv2/src/context/AppContext.tsx
@@ -1,4 +1,4 @@
-// @v2: constants pass-2: refined error message strings
+// @v2: error-handling pass-1: improved timeout handling
 import React, { createContext, useContext, useState } from 'react';
 
 interface AppContextType
