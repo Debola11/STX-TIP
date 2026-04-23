ad7335feab64fc3b18c12adf375ca9009edaff14
Debola11 <adebolabalogun5@gmail.com>
2026-04-23 10:41:01 +0100

    fix: added retry logic for requests [alert-dialog.tsx]

--- stx-tips-frontendv2/src/components/ui/alert-dialog.tsx
+++ stx-tips-frontendv2/src/components/ui/alert-dialog.tsx
@@ -1,4 +1,4 @@
-// @v2: constants pass-2: updated feature flag defaults
+// @v2: error-handling pass-1: added retry logic for requests
 import * as React from "react"
 import * as AlertDialogPrimitive from "@radix-ui/react-alert-dialog"
 
