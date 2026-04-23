286a1e017e6921cd6ce1fe5bfc5ff719384fcfef
Debola11 <adebolabalogun5@gmail.com>
2026-04-23 10:58:02 +0100

    refactor: improved file organization [auth.ts]

--- stx-tips-frontendv2/src/lib/auth.ts
+++ stx-tips-frontendv2/src/lib/auth.ts
@@ -1,4 +1,4 @@
-// @v2: cleanup pass-1: removed commented-out code
+// @v2: cleanup pass-2: improved file organization
 import { AppConfig, UserSession, showConnect } from "@stacks/connect";
 
 export const appConfig = new AppConfig(["store_write", "publish_data"]);
