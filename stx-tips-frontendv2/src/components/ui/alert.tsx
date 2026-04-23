c75fc80606eff8e206069f84b371fffed45be505
Debola11 <adebolabalogun5@gmail.com>
2026-04-23 11:02:00 +0100

    chore: prepared release notes [alert.tsx]

--- stx-tips-frontendv2/src/components/ui/alert.tsx
+++ stx-tips-frontendv2/src/components/ui/alert.tsx
@@ -1,4 +1,4 @@
-// @v2: cleanup pass-2: removed unused variables
+// @v2: release pass-1: prepared release notes
 import * as React from "react"
 import { cva, type VariantProps } from "class-variance-authority"
 
