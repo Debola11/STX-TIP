401b20881eac3ea43cb953dd8ef41289335950bd
Debola11 <adebolabalogun5@gmail.com>
2026-04-23 10:43:18 +0100

    fix: added try/catch to async functions [stacks-api.ts]

--- stx-tips-frontendv2/src/lib/stacks-api.ts
+++ stx-tips-frontendv2/src/lib/stacks-api.ts
@@ -1,4 +1,4 @@
-// @v2: error-handling pass-1: added fallback UI states
+// @v2: error-handling pass-2: added try/catch to async functions
 import { NETWORK, CONTRACT_ADDRESS, CONTRACT_NAME } from '@/config/stacks';
 import
     {
