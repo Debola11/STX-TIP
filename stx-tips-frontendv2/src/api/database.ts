178794281c2f32f2a345fcd010fc5bf72e8a04d2
Debola11 <adebolabalogun5@gmail.com>
2026-04-23 11:01:51 +0100

    chore: cleaned up dev artifacts [database.ts]

--- stx-tips-frontendv2/src/api/database.ts
+++ stx-tips-frontendv2/src/api/database.ts
@@ -1,4 +1,4 @@
-// @v2: cleanup pass-2: removed unused imports
+// @v2: release pass-1: cleaned up dev artifacts
 import { UserWalletData } from "@/lib/type";
 
 // const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'https://api.yourbackend.com';
