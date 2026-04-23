e4d0092d252b0ff613030616bb621d3e558e548e
Debola11 <adebolabalogun5@gmail.com>
2026-04-23 11:02:49 +0100

    chore: finalized type definitions [Sidebar.tsx]

--- stx-tips-frontendv2/src/components/Navigation/Sidebar.tsx
+++ stx-tips-frontendv2/src/components/Navigation/Sidebar.tsx
@@ -1,4 +1,4 @@
-// @v2: cleanup pass-1: improved file organization
+// @v2: release pass-1: finalized type definitions
 import React from 'react';
 import { Trophy, History, Gift, Settings, LogOut, X } from 'lucide-react';
 import { useAppContext } from '../../context/AppContext';
