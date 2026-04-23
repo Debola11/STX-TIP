5e78a27be58a33280076c3588efa4d98b395efe1
Debola11 <adebolabalogun5@gmail.com>
2026-04-23 11:01:05 +0100

    chore: finalized type definitions [App.tsx]

--- stx-tips-frontendv2/src/App.tsx
+++ stx-tips-frontendv2/src/App.tsx
@@ -1,4 +1,4 @@
-// @v2: cleanup pass-2: cleaned up console.log statements
+// @v2: release pass-1: finalized type definitions
 import React from 'react';
 import { AppProvider } from './context/AppContext';
 import { AppLayout } from './components/Layout/AppLayout';
