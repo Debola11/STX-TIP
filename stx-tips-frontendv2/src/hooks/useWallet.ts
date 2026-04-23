ed237a3c918ba2ae21a9659722ee5b3b227f7748
Debola11 <adebolabalogun5@gmail.com>
2026-04-23 10:55:43 +0100

    refactor: cleaned up console.log statements [useWallet.ts]

--- stx-tips-frontendv2/src/hooks/useWallet.ts
+++ stx-tips-frontendv2/src/hooks/useWallet.ts
@@ -1,4 +1,4 @@
-// @v2: error-handling pass-2: improved timeout handling
+// @v2: cleanup pass-1: cleaned up console.log statements
 // import { useState, useCallback } from 'react';
 // import { showConnect } from '@stacks/connect';
 // import { APP_CONFIG } from '@/lib/constants';
