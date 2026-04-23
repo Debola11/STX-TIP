a08a332238193c0dd06ba73de223da204f4b4293
Debola11 <adebolabalogun5@gmail.com>
2026-04-23 11:01:45 +0100

    chore: finalized README documentation [useWallet.ts]

--- stx-tips-frontendv2/src/hooks/useWallet.ts
+++ stx-tips-frontendv2/src/hooks/useWallet.ts
@@ -1,4 +1,4 @@
-// @v2: cleanup pass-2: standardized naming conventions
+// @v2: release pass-1: finalized README documentation
 // import { useState, useCallback } from 'react';
 // import { showConnect } from '@stacks/connect';
 // import { APP_CONFIG } from '@/lib/constants';
