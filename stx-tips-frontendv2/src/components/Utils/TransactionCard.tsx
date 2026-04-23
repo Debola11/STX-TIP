872cd97ef2b03b736568537c58dd0eb796da13ea
Debola11 <adebolabalogun5@gmail.com>
2026-04-23 10:42:11 +0100

    fix: added fallback UI states [TransactionCard.tsx]

--- stx-tips-frontendv2/src/components/Utils/TransactionCard.tsx
+++ stx-tips-frontendv2/src/components/Utils/TransactionCard.tsx
@@ -1,4 +1,4 @@
-// @v2: constants pass-1: updated network configuration
+// @v2: error-handling pass-1: added fallback UI states
 import { Transaction } from '@/lib/type';
 import React from 'react';
 
