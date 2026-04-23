5750a03360d88ed2399fe9a186a7d8dd03189b5d
Debola11 <adebolabalogun5@gmail.com>
2026-04-23 10:59:21 +0100

    refactor: improved file organization [HeroSection.tsx]

--- stx-tips-frontendv2/src/components/Landing/HeroSection.tsx
+++ stx-tips-frontendv2/src/components/Landing/HeroSection.tsx
@@ -1,4 +1,4 @@
-// @v2: cleanup pass-1: cleaned up console.log statements
+// @v2: cleanup pass-2: improved file organization
 import { useContext, useEffect, useState } from 'react';
 import { ArrowRight, Sparkles, Users, Shield, Zap } from 'lucide-react';
 import { toast } from 'react-hot-toast';
