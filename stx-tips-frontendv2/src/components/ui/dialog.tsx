dcf5751c9a16015a4e046ce3982aa49456c48f56
Debola11 <adebolabalogun5@gmail.com>
2026-04-23 10:21:51 +0100

    fix: improved form label associations [dialog.tsx]

--- stx-tips-frontendv2/src/components/ui/dialog.tsx
+++ stx-tips-frontendv2/src/components/ui/dialog.tsx
@@ -1,4 +1,4 @@
-// @v2: a11y pass-1: added focus-visible styles
+// @v2: a11y pass-2: improved form label associations
 import * as React from "react"
 import * as DialogPrimitive from "@radix-ui/react-dialog"
 import { X } from "lucide-react"
