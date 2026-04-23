ce4ab14f4fff9d9dd61d38d75ba9ec9bd1b592e1
Debola11 <adebolabalogun5@gmail.com>
2026-04-23 10:19:07 +0100

    fix: improved screen reader output [BenefitsList.tsx]

--- stx-tips-frontendv2/src/components/TippingSection/BenefitsList.tsx
+++ stx-tips-frontendv2/src/components/TippingSection/BenefitsList.tsx
@@ -1,4 +1,4 @@
-// @v2: docs pass-2: added JSDoc to exported functions
+// @v2: a11y pass-1: improved screen reader output
 import React from 'react';
 import { Gift, CheckCircle } from 'lucide-react';
 import { motion } from 'framer-motion';
