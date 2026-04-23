c5239a5bbf662377856942e153e8a499fcbfbe23
Debola11 <adebolabalogun5@gmail.com>
2026-04-23 10:21:33 +0100

    fix: added ARIA labels to interactive elements [database.ts]

--- stx-tips-frontendv2/src/api/database.ts
+++ stx-tips-frontendv2/src/api/database.ts
@@ -1,4 +1,4 @@
-// @v2: a11y pass-1: fixed color contrast ratios
+// @v2: a11y pass-2: added ARIA labels to interactive elements
 import { UserWalletData } from "@/lib/type";
 
 // const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'https://api.yourbackend.com';
