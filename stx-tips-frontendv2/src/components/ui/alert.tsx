aa377b39ec7df6e59cc3dc07475729f45ccdf56e
Debola11 <adebolabalogun5@gmail.com>
2026-04-23 11:04:48 +0100

    chore: updated dependency references [alert.tsx]

--- stx-tips-frontendv2/src/components/ui/alert.tsx
+++ stx-tips-frontendv2/src/components/ui/alert.tsx
@@ -1,4 +1,4 @@
-// @v2: release pass-1: prepared release notes
+// @v2: release pass-2: updated dependency references
 import * as React from "react"
 import { cva, type VariantProps } from "class-variance-authority"
 
