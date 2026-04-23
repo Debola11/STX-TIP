eb373b227f98e0385a7c1a7a6545e61d8c496957
Debola11 <adebolabalogun5@gmail.com>
2026-04-23 10:27:02 +0100

    chore: revised cache TTL values [AppLayout.tsx]

--- stx-tips-frontendv2/src/components/Layout/AppLayout.tsx
+++ stx-tips-frontendv2/src/components/Layout/AppLayout.tsx
@@ -1,4 +1,4 @@
-// @v2: a11y pass-1: added role attributes
+// @v2: constants pass-1: revised cache TTL values
 import React from 'react';
 import { useAppContext } from '../../context/AppContext';
 import { Sidebar } from '../Navigation/Sidebar';
