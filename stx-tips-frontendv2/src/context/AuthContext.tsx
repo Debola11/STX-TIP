b6f8634a84e07cbe5aa4ad73c51d8b4a8a1e1eb9
Debola11 <adebolabalogun5@gmail.com>
2026-04-23 10:25:46 +0100

    chore: updated API endpoint constants [AuthContext.tsx]

--- stx-tips-frontendv2/src/context/AuthContext.tsx
+++ stx-tips-frontendv2/src/context/AuthContext.tsx
@@ -1,4 +1,4 @@
-// @v2: a11y pass-2: fixed color contrast ratios
+// @v2: constants pass-1: updated API endpoint constants
 // import { createContext, useEffect, useReducer } from "react";
 
 // const initialState = {
