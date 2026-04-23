b2d22c5f7bc48ab2daf32e7b1fac7b774822a8de
Debola11 <adebolabalogun5@gmail.com>
2026-04-23 10:58:19 +0100

    refactor: removed unused variables [AppContext.tsx]

--- stx-tips-frontendv2/src/context/AppContext.tsx
+++ stx-tips-frontendv2/src/context/AppContext.tsx
@@ -1,4 +1,4 @@
-// @v2: cleanup pass-1: standardized naming conventions
+// @v2: cleanup pass-2: removed unused variables
 import React, { createContext, useContext, useState } from 'react';
 
 interface AppContextType
