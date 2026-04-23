39b1cedba7c047354b9bed677429d4eb0a4bc730
Debola11 <adebolabalogun5@gmail.com>
2026-04-23 11:03:15 +0100

    chore: finalized type definitions [validation.ts]

--- stx-tips-frontendv2/src/components/Utils/validation.ts
+++ stx-tips-frontendv2/src/components/Utils/validation.ts
@@ -1,4 +1,4 @@
-// @v2: cleanup pass-1: improved file organization
+// @v2: release pass-1: finalized type definitions
 export const validateStacksAddress = (address: string): boolean => {
     return address.startsWith('ST') || address.startsWith('SP') && address.length >= 41;
     // return true;
