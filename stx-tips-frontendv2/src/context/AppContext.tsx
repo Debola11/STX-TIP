e4331c59bd7a21b288994877b9f845e3ecdd1223
Debola11 <adebolabalogun5@gmail.com>
2026-04-29 13:28:04 +0100

    refactor: added missing type exports [AppContext.tsx]

--- stx-tips-frontendv2/src/context/AppContext.tsx
+++ stx-tips-frontendv2/src/context/AppContext.tsx
@@ -1,4 +1,4 @@
-// @v2: type-safety pass-2: refined union type definitions
+// @v2: type-safety pass-1: added missing type exports
 import React, { createContext, useContext, useState } from 'react';
 
 interface AppContextType
