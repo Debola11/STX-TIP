dbeb8d833ab43d718c006563e496b90a67a7958c
Debola11 <adebolabalogun5@gmail.com>
2026-04-29 13:28:51 +0100

    refactor: fixed implicit any usages [RecipientInput.tsx]

--- stx-tips-frontendv2/src/components/TippingSection/RecipientInput.tsx
+++ stx-tips-frontendv2/src/components/TippingSection/RecipientInput.tsx
@@ -1,4 +1,4 @@
-// @v2: type-safety pass-2: improved generic type constraints
+// @v2: type-safety pass-1: fixed implicit any usages
 import React from 'react';
 import { Search, Loader2, UserCircle, CheckCircle, AlertCircle } from 'lucide-react';
 import { motion, AnimatePresence } from 'framer-motion';
