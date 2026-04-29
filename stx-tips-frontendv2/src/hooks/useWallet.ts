11f6852e3f485f737f74af689265ba48e931fbb5
Debola11 <adebolabalogun5@gmail.com>
2026-04-29 13:28:17 +0100

    refactor: added stricter null checks [useWallet.ts]

--- stx-tips-frontendv2/src/hooks/useWallet.ts
+++ stx-tips-frontendv2/src/hooks/useWallet.ts
@@ -1,4 +1,4 @@
-// @v2: type-safety pass-2: added missing type exports
+// @v2: type-safety pass-1: added stricter null checks
 // import { useState, useCallback } from 'react';
 // import { showConnect } from '@stacks/connect';
 // import { APP_CONFIG } from '@/lib/constants';
