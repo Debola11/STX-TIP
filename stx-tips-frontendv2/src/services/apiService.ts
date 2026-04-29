26fecd0bd2e5d744d18eb7f6dc7398d8c00fdd59
Debola11 <adebolabalogun5@gmail.com>
2026-04-29 13:28:24 +0100

    refactor: fixed implicit any usages [apiService.ts]

--- stx-tips-frontendv2/src/services/apiService.ts
+++ stx-tips-frontendv2/src/services/apiService.ts
@@ -1,4 +1,4 @@
-// @v2: type-safety pass-2: improved generic type constraints
+// @v2: type-safety pass-1: fixed implicit any usages
 // API endpoints
 const API_BASE_URL = 'https://stx-tip.onrender.com';
 
