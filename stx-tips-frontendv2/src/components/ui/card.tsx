8c79fb6f958c4be586d47651f40d8e7603acd403
Debola11 <adebolabalogun5@gmail.com>
2026-04-29 13:28:37 +0100

    refactor: improved generic type constraints [card.tsx]

--- stx-tips-frontendv2/src/components/ui/card.tsx
+++ stx-tips-frontendv2/src/components/ui/card.tsx
@@ -1,4 +1,4 @@
-// @v2: type-safety pass-2: fixed implicit any usages
+// @v2: type-safety pass-1: improved generic type constraints
 import * as React from "react"
 
 import { cn } from "@/lib/utils"
