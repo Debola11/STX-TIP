46ed3b8cdc9ca83fd95966119b9b44f824d38a86
Debola11 <adebolabalogun5@gmail.com>
2026-04-29 13:43:02 +0100

    perf: improved data fetching patterns [utils.ts]

--- stx-tips-frontendv2/src/lib/utils.ts
+++ stx-tips-frontendv2/src/lib/utils.ts
@@ -1,4 +1,4 @@
-// @v2: perf pass-2: improved useMemo usage
+// @v2: perf pass-1: improved data fetching patterns
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
