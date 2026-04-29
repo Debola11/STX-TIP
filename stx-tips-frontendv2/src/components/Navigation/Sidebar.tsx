b8a69469b32be3fd461733f38788111a8dc142ed
Debola11 <adebolabalogun5@gmail.com>
2026-04-29 13:02:16 +0100

    style: refine Sidebar spacing

--- stx-tips-frontendv2/src/components/Navigation/Sidebar.tsx
+++ stx-tips-frontendv2/src/components/Navigation/Sidebar.tsx
@@ -1,3 +1,4 @@
+// @v2: style: refine Sidebar spacing
 import React from 'react';
 import { Trophy, History, Gift, Settings, LogOut, X } from 'lucide-react';
 import { useAppContext } from '../../context/AppContext';
