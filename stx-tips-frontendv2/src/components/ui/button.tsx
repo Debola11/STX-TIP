21d64b6410c475d28effe46f44a59bfdf2d73958
Debola11 <adebolabalogun5@gmail.com>
2026-04-29 12:06:49 +0100

    feat: add Button component

--- stx-tips-frontendv2/src/components/ui/button.tsx
+++ stx-tips-frontendv2/src/components/ui/button.tsx
@@ -1,4 +1,3 @@
-// @v2: refactor: improve Button variants
 import * as React from "react"
 import { Slot } from "@radix-ui/react-slot"
 import { cva, type VariantProps } from "class-variance-authority"
