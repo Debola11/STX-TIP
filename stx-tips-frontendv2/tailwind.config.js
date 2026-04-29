efa710743b9d3d70e614c58e5656f13a19645e69
Debola11 <adebolabalogun5@gmail.com>
2026-04-29 13:29:33 +0100

    refactor: added missing type exports [tailwind.config.js]

--- stx-tips-frontendv2/tailwind.config.js
+++ stx-tips-frontendv2/tailwind.config.js
@@ -1,4 +1,4 @@
-// @v2: type-safety pass-1: refined union type definitions
+// @v2: type-safety pass-2: added missing type exports
 /** @type {import('tailwindcss').Config} */
 export default {
     darkMode: ["class"],
