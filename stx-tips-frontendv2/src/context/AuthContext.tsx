b9f11e8ad6a8caf442a38514023f786ca60a77a9
Debola11 <adebolabalogun5@gmail.com>
2026-04-29 13:43:08 +0100

    perf: added React.memo for expensive renders [AuthContext.tsx]

--- stx-tips-frontendv2/src/context/AuthContext.tsx
+++ stx-tips-frontendv2/src/context/AuthContext.tsx
@@ -1,4 +1,4 @@
-// @v2: perf pass-2: lazy loaded heavy components
+// @v2: perf pass-1: added React.memo for expensive renders
 // import { createContext, useEffect, useReducer } from "react";
 
 // const initialState = {
