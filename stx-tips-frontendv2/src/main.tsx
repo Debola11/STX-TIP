8c3c4aeb40606758d818615c8f1ca0ec25a0f972
Debola11 <adebolabalogun5@gmail.com>
2026-04-29 13:13:26 +0100

    style: optimized mobile-first styles [main.tsx]

--- stx-tips-frontendv2/src/main.tsx
+++ stx-tips-frontendv2/src/main.tsx
@@ -1,4 +1,4 @@
-// @v2: ui-polish pass-2: refined responsive breakpoints
+// @v2: ui-polish pass-1: optimized mobile-first styles
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Connect } from '@stacks/connect-react';
