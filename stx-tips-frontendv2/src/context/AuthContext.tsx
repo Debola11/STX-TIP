e87b69b2b79026a6af73faabce8aadcc273fe02b
Debola11 <adebolabalogun5@gmail.com>
2026-04-29 13:28:08 +0100

    refactor: verified prop types and interfaces [AuthContext.tsx]

--- stx-tips-frontendv2/src/context/AuthContext.tsx
+++ stx-tips-frontendv2/src/context/AuthContext.tsx
@@ -1,4 +1,4 @@
-// @v2: type-safety pass-2: improved return type annotations
+// @v2: type-safety pass-1: verified prop types and interfaces
 // import { createContext, useEffect, useReducer } from "react";
 
 // const initialState = {
