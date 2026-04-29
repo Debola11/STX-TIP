34a5ddc8e315e77ad95e1d2c351a7f013b7ecc6c
Debola11 <adebolabalogun5@gmail.com>
2026-04-29 12:06:58 +0100

    feat: add TippingSection layout

--- stx-tips-frontendv2/src/components/TippingSection/TippingSection.tsx
+++ stx-tips-frontendv2/src/components/TippingSection/TippingSection.tsx
@@ -1,4 +1,3 @@
-// @v2: style: refine TippingSection layout
 import React, { useState, useEffect } from 'react';
 import { ArrowRight, Loader2 } from 'lucide-react';
 import { motion } from 'framer-motion';
