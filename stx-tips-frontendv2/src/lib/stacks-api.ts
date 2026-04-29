884b92f2dc517f3cb846cbcb00958f150a02cfd5
Debola11 <adebolabalogun5@gmail.com>
2026-04-29 13:30:20 +0100

    refactor: verified prop types and interfaces [stacks-api.ts]

--- stx-tips-frontendv2/src/lib/stacks-api.ts
+++ stx-tips-frontendv2/src/lib/stacks-api.ts
@@ -1,4 +1,4 @@
-// @v2: type-safety pass-1: improved return type annotations
+// @v2: type-safety pass-2: verified prop types and interfaces
 import { NETWORK, CONTRACT_ADDRESS, CONTRACT_NAME } from '@/config/stacks';
 import
     {
