2ec2ee83935b645e3511dc2fb881f93b522d94ff
Debola11 <adebolabalogun5@gmail.com>
2026-04-29 13:27:48 +0100

    refactor: improved generic type constraints [auth.ts]

--- stx-tips-frontendv2/src/lib/auth.ts
+++ stx-tips-frontendv2/src/lib/auth.ts
@@ -1,4 +1,4 @@
-// @v2: type-safety pass-2: improved type inference
+// @v2: type-safety pass-1: improved generic type constraints
 import { AppConfig, UserSession, showConnect } from "@stacks/connect";
 
 export const appConfig = new AppConfig(["store_write", "publish_data"]);
