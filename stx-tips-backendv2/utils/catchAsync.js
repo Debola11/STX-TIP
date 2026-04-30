337292c2c74eb3bb0cdb105078936e29fc3946ef
Debola11 <adebolabalogun5@gmail.com>
2026-04-30 17:03:24 +0100

    style: improved function naming conventions [catchAsync.js]

--- stx-tips-backendv2/utils/catchAsync.js
+++ stx-tips-backendv2/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: consistency pass-2: improved code indentation
+// @backend: consistency pass-3: improved function naming conventions
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
