813390ca7b40c67f1c2affcc891df902e43a60c2
Debola11 <adebolabalogun5@gmail.com>
2026-04-30 16:30:59 +0100

    refactor: improved function decomposition [UserModel.js]

--- stx-tips-backendv2/models/UserModel.js
+++ stx-tips-backendv2/models/UserModel.js
@@ -1,4 +1,4 @@
-// @backend: code-quality pass-6: simplified conditional logic
+// @backend: code-quality pass-7: improved function decomposition
 import mongoose from "mongoose";
 import validator from "validator";
 import bcrypt from "bcryptjs";
