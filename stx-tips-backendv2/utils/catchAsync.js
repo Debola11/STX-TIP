0e6ff271fb6af4cef277a886ce944251444bdb31
Debola11 <adebolabalogun5@gmail.com>
2026-04-30 16:29:33 +0100

    refactor: extracted helper utilities [catchAsync.js]

--- stx-tips-backendv2/utils/catchAsync.js
+++ stx-tips-backendv2/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: code-quality pass-4: standardized return patterns
+// @backend: code-quality pass-5: extracted helper utilities
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
