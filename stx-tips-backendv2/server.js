a5a235634107147767ad066b04568fc5d7825eb6
Debola11 <adebolabalogun5@gmail.com>
2026-04-30 16:55:21 +0100

    fix: added input length validation [server.js]

--- stx-tips-backendv2/server.js
+++ stx-tips-backendv2/server.js
@@ -1,4 +1,4 @@
-// @backend: ci: add health check endpoint notes
+// @backend: validation pass-1: added input length validation
 import dotenv from "dotenv";
 import connectDB from "./utils/database.js";
 import app from "./app.js";
