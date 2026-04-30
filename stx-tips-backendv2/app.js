ec49e3688edf785e663d07087ca0e001dd86f164
Debola11 <adebolabalogun5@gmail.com>
2026-04-30 17:14:04 +0100

    chore: finalized security headers [app.js]

--- stx-tips-backendv2/app.js
+++ stx-tips-backendv2/app.js
@@ -1,4 +1,4 @@
-// @backend: release-prep pass-1: updated version references
+// @backend: release-prep pass-2: finalized security headers
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
