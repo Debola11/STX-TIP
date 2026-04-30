1b083c49b10b17ca76d6faa99a4c5ddc598c7ee2
Debola11 <adebolabalogun5@gmail.com>
2026-04-30 17:06:38 +0100

    style: improved function naming conventions [routes.js]

--- stx-tips-backendv2/routes.js
+++ stx-tips-backendv2/routes.js
@@ -1,4 +1,4 @@
-// @backend: consistency pass-6: consistent async/await usage
+// @backend: consistency pass-7: improved function naming conventions
 import express from "express";
 import userRouter from "./routes/userRoutes.js";
 
