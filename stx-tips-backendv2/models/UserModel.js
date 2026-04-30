80eeaf5e665f3d061e0ee4a7274985b65f46be5c
Debola11 <adebolabalogun5@gmail.com>
2026-04-30 17:13:25 +0100

    chore: finalized API response format [UserModel.js]

--- stx-tips-backendv2/models/UserModel.js
+++ stx-tips-backendv2/models/UserModel.js
@@ -1,4 +1,4 @@
-// @backend: security: add field-level encryption notes
+// @backend: release-prep pass-1: finalized API response format
 import mongoose from "mongoose";
 import validator from "validator";
 import bcrypt from "bcryptjs";
