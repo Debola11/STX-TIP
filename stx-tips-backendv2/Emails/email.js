0fc3a29a3c4b1b6f9b30ad76cbc6e687b862f345
Debola11 <adebolabalogun5@gmail.com>
2026-04-30 17:10:19 +0100

    refactor: removed redundant null checks [email.js]

--- stx-tips-backendv2/Emails/email.js
+++ stx-tips-backendv2/Emails/email.js
@@ -1,4 +1,4 @@
-// @backend: cleanup pass-3: removed deprecated methods
+// @backend: cleanup pass-4: removed redundant null checks
 import nodemailer from "nodemailer";
 import ejs from "ejs";
 import path from "path";
