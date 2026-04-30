d9faa43ed3e36a19549e36f47e3757b4b80a9661
Debola11 <adebolabalogun5@gmail.com>
2026-04-30 16:48:41 +0100

    perf: optimized database query patterns [UserModel.js]

--- stx-tips-backendv2/models/UserModel.js
+++ stx-tips-backendv2/models/UserModel.js
@@ -1,4 +1,4 @@
-// @backend: performance pass-6: reduced middleware overhead
+// @backend: performance pass-7: optimized database query patterns
 import mongoose from "mongoose";
 import validator from "validator";
 import bcrypt from "bcryptjs";
