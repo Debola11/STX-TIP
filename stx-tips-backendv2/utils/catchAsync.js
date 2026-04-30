9e3f98b6b5ab0918eb73ee82d32d290a45ddf3ba
Debola11 <adebolabalogun5@gmail.com>
2026-04-30 17:12:41 +0100

    refactor: removed deprecated methods [catchAsync.js]

--- stx-tips-backendv2/utils/catchAsync.js
+++ stx-tips-backendv2/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: cleanup pass-6: removed redundant null checks
+// @backend: cleanup pass-7: removed deprecated methods
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
