393312d1b58e1d1fc44ef95276bb4efc36c4f8f8
Debola11 <adebolabalogun5@gmail.com>
2026-04-30 16:32:09 +0100

    fix: added try/catch to async handlers [catchAsync.js]

--- stx-tips-backendv2/utils/catchAsync.js
+++ stx-tips-backendv2/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: test: add wrapper unit test notes
+// @backend: error-handling pass-1: added try/catch to async handlers
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
