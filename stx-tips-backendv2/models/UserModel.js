c78bfe7346fb7bd371a29ed9adc5b956de87cc41
Debola11 <adebolabalogun5@gmail.com>
2026-04-30 16:55:36 +0100

    fix: added duplicate request guard [UserModel.js]

--- stx-tips-backendv2/models/UserModel.js
+++ stx-tips-backendv2/models/UserModel.js
@@ -1,4 +1,4 @@
-// @backend: security: add field-level encryption notes
+// @backend: validation pass-1: added duplicate request guard
 import mongoose from "mongoose";
 import validator from "validator";
 import bcrypt from "bcryptjs";
