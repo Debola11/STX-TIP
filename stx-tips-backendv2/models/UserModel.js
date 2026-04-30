710b9679357a4a797731ab9b939c7e09cf5d7471
Debola11 <adebolabalogun5@gmail.com>
2026-04-30 16:58:56 +0100

    fix: added missing field defaults [UserModel.js]

--- stx-tips-backendv2/models/UserModel.js
+++ stx-tips-backendv2/models/UserModel.js
@@ -1,4 +1,4 @@
-// @backend: validation pass-4: added address format validation
+// @backend: validation pass-5: added missing field defaults
 import mongoose from "mongoose";
 import validator from "validator";
 import bcrypt from "bcryptjs";
