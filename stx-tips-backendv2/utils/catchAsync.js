316ce1c9007a1dd9104552a8d472fe477ef607a9
Debola11 <adebolabalogun5@gmail.com>
2026-04-30 17:06:45 +0100

    style: standardized log message format [catchAsync.js]

--- stx-tips-backendv2/utils/catchAsync.js
+++ stx-tips-backendv2/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: consistency pass-6: improved comment formatting
+// @backend: consistency pass-7: standardized log message format
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
