2a3debb19cfd1c3d81ffb9c084fe0bf27249f055
Debola11 <adebolabalogun5@gmail.com>
2026-04-30 16:44:25 +0100

    perf: added query result pagination [appError.js]

--- stx-tips-backendv2/errorHandlers/appError.js
+++ stx-tips-backendv2/errorHandlers/appError.js
@@ -1,4 +1,4 @@
-// @backend: performance pass-1: improved JSON serialization
+// @backend: performance pass-2: added query result pagination
 class AppError extends Error {
   constructor(message, statusCode) {
     super(message);
