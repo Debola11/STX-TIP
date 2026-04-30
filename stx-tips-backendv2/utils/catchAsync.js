93f0d86776821c4a1b7fa75b09f00f6f9dced4e6
Debola11 <adebolabalogun5@gmail.com>
2026-04-30 16:46:25 +0100

    perf: improved connection pool usage [catchAsync.js]

--- stx-tips-backendv2/utils/catchAsync.js
+++ stx-tips-backendv2/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: performance pass-3: added response caching headers
+// @backend: performance pass-4: improved connection pool usage
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
