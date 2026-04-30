86cae6a3c26d8b535eff2c06a37c343b4ad56364
Debola11 <adebolabalogun5@gmail.com>
2026-04-30 16:43:00 +0100

    docs: documented error response format [catchAsync.js]

--- stx-tips-backendv2/utils/catchAsync.js
+++ stx-tips-backendv2/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: documentation pass-6: documented API response shape
+// @backend: documentation pass-7: documented error response format
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
