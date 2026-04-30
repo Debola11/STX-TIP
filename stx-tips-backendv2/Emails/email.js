5d2f8acd081ae1adfc4e95c9ffa8eabe9e57c75b
Debola11 <adebolabalogun5@gmail.com>
2026-04-30 16:49:58 +0100

    chore: revised configuration defaults [email.js]

--- stx-tips-backendv2/Emails/email.js
+++ stx-tips-backendv2/Emails/email.js
@@ -1,4 +1,4 @@
-// @backend: security: add template injection guard
+// @backend: maintenance pass-1: revised configuration defaults
 import nodemailer from "nodemailer";
 import ejs from "ejs";
 import path from "path";
