533dac092835fa12984fa5218602cef8d87c0ad4
Debola11 <adebolabalogun5@gmail.com>
2026-04-30 10:11:09 +0100

    docs: added module-level docs [App.tsx]

--- stx-tips-frontendv2/src/App.tsx
+++ stx-tips-frontendv2/src/App.tsx
@@ -1,4 +1,4 @@
-// @v2: docs pass-2: added usage examples in comments
+// @v2: docs pass-1: added module-level docs
 import React from 'react';
 import { AppProvider } from './context/AppContext';
 import { AppLayout } from './components/Layout/AppLayout';
