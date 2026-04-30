b8c7c3467bab77dfabfc954590e31d4f0d3597f4
Debola11 <adebolabalogun5@gmail.com>
2026-04-30 16:55:32 +0100

    fix: improved amount range checking [errorHandler.js]

--- stx-tips-backendv2/errorHandlers/errorHandler.js
+++ stx-tips-backendv2/errorHandlers/errorHandler.js
@@ -1,4 +1,4 @@
-// @backend: security: sanitize error stack in prod
+// @backend: validation pass-1: improved amount range checking
 import AppError from "./appError.js";
 
 const handleCastErrorDB = (err) => {
