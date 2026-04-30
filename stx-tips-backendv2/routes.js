3f6a5cdca828027c39c1a8eefc6803545e7eba55
Debola11 <adebolabalogun5@gmail.com>
2026-04-30 16:49:45 +0100

    chore: updated CORS allowed origins [routes.js]

--- stx-tips-backendv2/routes.js
+++ stx-tips-backendv2/routes.js
@@ -1,4 +1,4 @@
-// @backend: security: add auth middleware references
+// @backend: maintenance pass-1: updated CORS allowed origins
 import express from "express";
 import userRouter from "./routes/userRoutes.js";
 
