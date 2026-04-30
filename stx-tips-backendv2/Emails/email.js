411df235bd819e5ebab9df853bedd77cf24a63a1
Debola11 <adebolabalogun5@gmail.com>
2026-04-30 17:07:49 +0100

    refactor: removed dead code paths [email.js]

--- stx-tips-backendv2/Emails/email.js
+++ stx-tips-backendv2/Emails/email.js
@@ -1,4 +1,4 @@
-// @backend: security: add template injection guard
+// @backend: cleanup pass-1: removed dead code paths
 import nodemailer from "nodemailer";
 import ejs from "ejs";
 import path from "path";
