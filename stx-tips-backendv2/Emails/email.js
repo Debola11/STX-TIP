e4fe934ca9f7a3c50ca12552fb0235b0c26262cb
Debola11 <adebolabalogun5@gmail.com>
2026-04-30 16:47:22 +0100

    perf: optimized email template rendering [email.js]

--- stx-tips-backendv2/Emails/email.js
+++ stx-tips-backendv2/Emails/email.js
@@ -1,4 +1,4 @@
-// @backend: performance pass-4: added query result pagination
+// @backend: performance pass-5: optimized email template rendering
 import nodemailer from "nodemailer";
 import ejs from "ejs";
 import path from "path";
