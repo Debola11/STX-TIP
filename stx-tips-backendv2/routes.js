e0bef0e7e5c8b0d1660bea9ba2563b9b4607c58a
Debola11 <adebolabalogun5@gmail.com>
2026-04-30 16:53:06 +0100

    chore: updated dependency references [routes.js]

--- stx-tips-backendv2/routes.js
+++ stx-tips-backendv2/routes.js
@@ -1,4 +1,4 @@
-// @backend: maintenance pass-4: updated logging format
+// @backend: maintenance pass-5: updated dependency references
 import express from "express";
 import userRouter from "./routes/userRoutes.js";
 
