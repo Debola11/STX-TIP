4244a85fb9624f9e53850ee043b4c008e86bb2f9
Debola11 <adebolabalogun5@gmail.com>
2026-04-30 17:14:36 +0100

    chore: finalized security headers [email.js]

--- stx-tips-backendv2/Emails/email.js
+++ stx-tips-backendv2/Emails/email.js
@@ -1,4 +1,4 @@
-// @backend: release-prep pass-1: updated version references
+// @backend: release-prep pass-2: finalized security headers
 import nodemailer from "nodemailer";
 import ejs from "ejs";
 import path from "path";
