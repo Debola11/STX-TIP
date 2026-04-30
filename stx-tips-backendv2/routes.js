320a2797b8e49989a5f238a3512d50206cb3c318
Debola11 <adebolabalogun5@gmail.com>
2026-04-30 11:20:15 +0100

    feat: add main route definitions

---
+++ stx-tips-backendv2/routes.js
@@ -0,0 +1,9 @@
+import express from "express";
+import userRouter from "./routes/userRoutes.js";
+
+
+export const mountedRoutes = function (app)
+{
+  app.use(express.json());
+  app.use("/api/v1/users", userRouter);
+};
