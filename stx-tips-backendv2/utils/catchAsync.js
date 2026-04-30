c50c1a2fdf2b84034f6e89c5b37c2bde875a2fe7
Debola11 <adebolabalogun5@gmail.com>
2026-04-30 16:51:34 +0100

    chore: revised configuration defaults [catchAsync.js]

--- stx-tips-backendv2/utils/catchAsync.js
+++ stx-tips-backendv2/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: maintenance pass-2: updated logging format
+// @backend: maintenance pass-3: revised configuration defaults
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
