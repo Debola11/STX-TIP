bd9de0be76236ec965ba2519bd3c3d396633a123
Debola11 <adebolabalogun5@gmail.com>
2026-04-30 16:55:25 +0100

    fix: improved email format checking [app.js]

--- stx-tips-backendv2/app.js
+++ stx-tips-backendv2/app.js
@@ -1,4 +1,4 @@
-// @backend: ci: add graceful shutdown handler
+// @backend: validation pass-1: improved email format checking
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
