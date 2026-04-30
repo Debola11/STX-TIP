2d775953e1e41e212cbfda188373b85f057729d9
Debola11 <adebolabalogun5@gmail.com>
2026-04-30 16:38:03 +0100

    docs: added JSDoc to exported functions [catchAsync.js]

--- stx-tips-backendv2/utils/catchAsync.js
+++ stx-tips-backendv2/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: test: add wrapper unit test notes
+// @backend: documentation pass-1: added JSDoc to exported functions
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
