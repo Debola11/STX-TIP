374667eb5604169307f51d337639e20321537e77
Debola11 <adebolabalogun5@gmail.com>
2026-04-30 16:54:38 +0100

    chore: updated dependency references [UserModel.js]

--- stx-tips-backendv2/models/UserModel.js
+++ stx-tips-backendv2/models/UserModel.js
@@ -1,4 +1,4 @@
-// @backend: maintenance pass-6: revised timeout constants
+// @backend: maintenance pass-7: updated dependency references
 import mongoose from "mongoose";
 import validator from "validator";
 import bcrypt from "bcryptjs";
