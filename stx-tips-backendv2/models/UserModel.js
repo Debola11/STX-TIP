8ded682d5a5c4d50614bfc48d6b6b9a863e923bc
Debola11 <adebolabalogun5@gmail.com>
2026-04-30 16:49:38 +0100

    chore: updated logging format [UserModel.js]

--- stx-tips-backendv2/models/UserModel.js
+++ stx-tips-backendv2/models/UserModel.js
@@ -1,4 +1,4 @@
-// @backend: security: add field-level encryption notes
+// @backend: maintenance pass-1: updated logging format
 import mongoose from "mongoose";
 import validator from "validator";
 import bcrypt from "bcryptjs";
