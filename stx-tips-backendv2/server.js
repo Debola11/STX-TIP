6a8ca2f61bc07643f5637468e56d4ceedc3d939d
Debola11 <adebolabalogun5@gmail.com>
2026-04-30 17:14:00 +0100

    chore: updated deployment notes [server.js]

--- stx-tips-backendv2/server.js
+++ stx-tips-backendv2/server.js
@@ -1,4 +1,4 @@
-// @backend: release-prep pass-1: finalized error messages
+// @backend: release-prep pass-2: updated deployment notes
 import dotenv from "dotenv";
 import connectDB from "./utils/database.js";
 import app from "./app.js";
