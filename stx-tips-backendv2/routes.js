9789bc55bf21acb881d710fb08cf1fc80df50a28
Debola11 <adebolabalogun5@gmail.com>
2026-04-30 16:42:53 +0100

    docs: documented middleware chain [routes.js]

--- stx-tips-backendv2/routes.js
+++ stx-tips-backendv2/routes.js
@@ -1,4 +1,4 @@
-// @backend: documentation pass-6: documented environment variables
+// @backend: documentation pass-7: documented middleware chain
 import express from "express";
 import userRouter from "./routes/userRoutes.js";
 
