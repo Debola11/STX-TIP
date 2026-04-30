212c83a4e3606822a54f95b739e60ff16f1b745b
Debola11 <adebolabalogun5@gmail.com>
2026-04-30 11:23:16 +0100

    perf: cache compiled templates

--- stx-tips-backendv2/Emails/email.js
+++ stx-tips-backendv2/Emails/email.js
@@ -1,4 +1,4 @@
-// @backend: chore: update email transport config
+// @backend: perf: cache compiled templates
 import nodemailer from "nodemailer";
 import ejs from "ejs";
 import path from "path";
