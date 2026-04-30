f0f27061d2b69d722ca1ca66fb8c5800d2e9a18f
Debola11 <adebolabalogun5@gmail.com>
2026-04-30 17:00:49 +0100

    fix: improved amount range checking [catchAsync.js]

--- stx-tips-backendv2/utils/catchAsync.js
+++ stx-tips-backendv2/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: validation pass-6: improved query parameter parsing
+// @backend: validation pass-7: improved amount range checking
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
