136c312748ee266852c8fa1a40ba5e38d1790e2d
Debola11 <adebolabalogun5@gmail.com>
2026-04-30 16:33:56 +0100

    fix: improved timeout handling [email.js]

--- stx-tips-backendv2/Emails/email.js
+++ stx-tips-backendv2/Emails/email.js
@@ -1,4 +1,4 @@
-// @backend: error-handling pass-2: improved error status codes
+// @backend: error-handling pass-3: improved timeout handling
 import nodemailer from "nodemailer";
 import ejs from "ejs";
 import path from "path";
