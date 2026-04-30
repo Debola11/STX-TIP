d9902620ce423027d4a56b9225c5a2e01310be3f
Debola11 <adebolabalogun5@gmail.com>
2026-04-30 16:38:52 +0100

    docs: added parameter descriptions [catchAsync.js]

--- stx-tips-backendv2/utils/catchAsync.js
+++ stx-tips-backendv2/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: documentation pass-1: added JSDoc to exported functions
+// @backend: documentation pass-2: added parameter descriptions
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
