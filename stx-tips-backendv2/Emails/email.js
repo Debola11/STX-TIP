b5932a8906be36c86d5e976cd9f962c524af86ee
Debola11 <adebolabalogun5@gmail.com>
2026-04-30 17:17:55 +0100

    chore: finalized error messages [email.js]

--- stx-tips-backendv2/Emails/email.js
+++ stx-tips-backendv2/Emails/email.js
@@ -1,4 +1,4 @@
-// @backend: release-prep pass-5: finalized API response format
+// @backend: release-prep pass-6: finalized error messages
 import nodemailer from "nodemailer";
 import ejs from "ejs";
 import path from "path";
