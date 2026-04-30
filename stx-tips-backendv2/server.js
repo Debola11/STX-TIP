23d7287faecfe241dc9e719f5643209c0a5099a6
Debola11 <adebolabalogun5@gmail.com>
2026-04-30 16:45:08 +0100

    perf: improved JSON serialization [server.js]

--- stx-tips-backendv2/server.js
+++ stx-tips-backendv2/server.js
@@ -1,4 +1,4 @@
-// @backend: performance pass-2: improved connection pool usage
+// @backend: performance pass-3: improved JSON serialization
 import dotenv from "dotenv";
 import connectDB from "./utils/database.js";
 import app from "./app.js";
