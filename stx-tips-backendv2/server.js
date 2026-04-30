09e4719ca5b8fef2299106371973f461822abff7
Debola11 <adebolabalogun5@gmail.com>
2026-04-30 16:34:10 +0100

    fix: improved error status codes [server.js]

--- stx-tips-backendv2/server.js
+++ stx-tips-backendv2/server.js
@@ -1,4 +1,4 @@
-// @backend: error-handling pass-3: added graceful degradation
+// @backend: error-handling pass-4: improved error status codes
 import dotenv from "dotenv";
 import connectDB from "./utils/database.js";
 import app from "./app.js";
