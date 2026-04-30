51e60d4b68029d9e70dd086c2411c1e14211800b
Debola11 <adebolabalogun5@gmail.com>
2026-04-30 17:05:04 +0100

    style: consistent error object shape [catchAsync.js]

--- stx-tips-backendv2/utils/catchAsync.js
+++ stx-tips-backendv2/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: consistency pass-4: consistent async/await usage
+// @backend: consistency pass-5: consistent error object shape
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
