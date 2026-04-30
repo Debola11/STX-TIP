c7849c6e06ee0d075b2f0f76975e654b1973078d
Debola11 <adebolabalogun5@gmail.com>
2026-04-30 16:58:19 +0100

    fix: improved field type coercion [catchAsync.js]

--- stx-tips-backendv2/utils/catchAsync.js
+++ stx-tips-backendv2/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: validation pass-3: improved email format checking
+// @backend: validation pass-4: improved field type coercion
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
