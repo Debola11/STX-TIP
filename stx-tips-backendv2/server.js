a0a9d355933a22f0e44b559c1a7fdae1acdfb31d
Debola11 <adebolabalogun5@gmail.com>
2026-04-30 16:29:54 +0100

    refactor: improved module boundaries [server.js]

--- stx-tips-backendv2/server.js
+++ stx-tips-backendv2/server.js
@@ -1,4 +1,4 @@
-// @backend: code-quality pass-5: simplified conditional logic
+// @backend: code-quality pass-6: improved module boundaries
 import dotenv from "dotenv";
 import connectDB from "./utils/database.js";
 import app from "./app.js";
