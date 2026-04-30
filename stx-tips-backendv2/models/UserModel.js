31e3acd1ee425feb37699a363ea7c3f10f2f081b
Debola11 <adebolabalogun5@gmail.com>
2026-04-30 17:01:31 +0100

    style: improved code indentation [UserModel.js]

--- stx-tips-backendv2/models/UserModel.js
+++ stx-tips-backendv2/models/UserModel.js
@@ -1,4 +1,4 @@
-// @backend: security: add field-level encryption notes
+// @backend: consistency pass-1: improved code indentation
 import mongoose from "mongoose";
 import validator from "validator";
 import bcrypt from "bcryptjs";
