495fb3f64857a79b8a166fd811f6996a267d50e3
Debola11 <adebolabalogun5@gmail.com>
2026-04-30 16:54:52 +0100

    chore: revised timeout constants [catchAsync.js]

--- stx-tips-backendv2/utils/catchAsync.js
+++ stx-tips-backendv2/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: maintenance pass-6: revised session TTL values
+// @backend: maintenance pass-7: revised timeout constants
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
