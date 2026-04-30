5a9f3d650fc88572befb9900b39119e9e614ad99
Debola11 <adebolabalogun5@gmail.com>
2026-04-30 16:53:20 +0100

    chore: updated logging format [email.js]

--- stx-tips-backendv2/Emails/email.js
+++ stx-tips-backendv2/Emails/email.js
@@ -1,4 +1,4 @@
-// @backend: maintenance pass-4: revised session TTL values
+// @backend: maintenance pass-5: updated logging format
 import nodemailer from "nodemailer";
 import ejs from "ejs";
 import path from "path";
