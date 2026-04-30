bbdc1d61d82b25257e0a62c480d978d2ccbe1861
Debola11 <adebolabalogun5@gmail.com>
2026-04-30 16:42:10 +0100

    docs: documented API response shape [catchAsync.js]

--- stx-tips-backendv2/utils/catchAsync.js
+++ stx-tips-backendv2/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: documentation pass-5: added usage examples in comments
+// @backend: documentation pass-6: documented API response shape
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
