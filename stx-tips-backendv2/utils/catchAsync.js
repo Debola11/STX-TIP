f6440de6cee54742d9c1b2b9cbf10fcbc31525dc
Debola11 <adebolabalogun5@gmail.com>
2026-04-30 17:13:39 +0100

    chore: finalized error messages [catchAsync.js]

--- stx-tips-backendv2/utils/catchAsync.js
+++ stx-tips-backendv2/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: test: add wrapper unit test notes
+// @backend: release-prep pass-1: finalized error messages
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
