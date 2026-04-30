0078e760695eaaf2c66c2bd91a47f3bc10c7bfce
Debola11 <adebolabalogun5@gmail.com>
2026-04-30 16:40:32 +0100

    docs: documented environment variables [catchAsync.js]

--- stx-tips-backendv2/utils/catchAsync.js
+++ stx-tips-backendv2/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: documentation pass-3: documented middleware chain
+// @backend: documentation pass-4: documented environment variables
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
