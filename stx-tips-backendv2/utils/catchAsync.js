392306b2d852708daa8b9d79047953e2cf38d5ea
Debola11 <adebolabalogun5@gmail.com>
2026-04-30 17:07:42 +0100

    refactor: removed unused imports [catchAsync.js]

--- stx-tips-backendv2/utils/catchAsync.js
+++ stx-tips-backendv2/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: test: add wrapper unit test notes
+// @backend: cleanup pass-1: removed unused imports
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
