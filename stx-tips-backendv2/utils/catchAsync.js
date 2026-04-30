783928c0021ddccee696e7fc01832dddf08c876a
Debola11 <adebolabalogun5@gmail.com>
2026-04-30 16:27:55 +0100

    refactor: reduced cyclomatic complexity [catchAsync.js]

--- stx-tips-backendv2/utils/catchAsync.js
+++ stx-tips-backendv2/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: code-quality pass-2: improved error propagation
+// @backend: code-quality pass-3: reduced cyclomatic complexity
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
