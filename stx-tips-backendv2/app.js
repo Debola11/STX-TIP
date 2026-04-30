81cae4b25ecb591c3f24b74f83ebddc2ddffa935
Debola11 <adebolabalogun5@gmail.com>
2026-04-30 16:42:35 +0100

    docs: documented environment variables [app.js]

--- stx-tips-backendv2/app.js
+++ stx-tips-backendv2/app.js
@@ -1,4 +1,4 @@
-// @backend: documentation pass-6: added JSDoc to exported functions
+// @backend: documentation pass-7: documented environment variables
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
