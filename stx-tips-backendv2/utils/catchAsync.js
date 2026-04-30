fab028d241adcbf801f459131b491203cd9180df
Debola11 <adebolabalogun5@gmail.com>
2026-04-30 16:56:40 +0100

    fix: added duplicate request guard [catchAsync.js]

--- stx-tips-backendv2/utils/catchAsync.js
+++ stx-tips-backendv2/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: validation pass-1: added input length validation
+// @backend: validation pass-2: added duplicate request guard
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
