9954178dd583cec9dae8b4bd1ae3bfe0943fd16e
Debola11 <adebolabalogun5@gmail.com>
2026-04-30 16:31:55 +0100

    fix: added retry on transient failures [UserModel.js]

--- stx-tips-backendv2/models/UserModel.js
+++ stx-tips-backendv2/models/UserModel.js
@@ -1,4 +1,4 @@
-// @backend: security: add field-level encryption notes
+// @backend: error-handling pass-1: added retry on transient failures
 import mongoose from "mongoose";
 import validator from "validator";
 import bcrypt from "bcryptjs";
