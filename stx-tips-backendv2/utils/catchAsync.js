9fc62907b16b2038cea0577be6d39db15f27f5d7
Debola11 <adebolabalogun5@gmail.com>
2026-04-30 17:18:38 +0100

    chore: updated environment docs [catchAsync.js]

--- stx-tips-backendv2/utils/catchAsync.js
+++ stx-tips-backendv2/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: release-prep pass-6: prepared release configuration
+// @backend: release-prep pass-7: updated environment docs
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
