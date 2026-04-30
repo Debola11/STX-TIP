f67517d8c0f11786e59bed47699b2b243e0692b1
Debola11 <adebolabalogun5@gmail.com>
2026-04-30 16:30:23 +0100

    refactor: improved module boundaries [catchAsync.js]

--- stx-tips-backendv2/utils/catchAsync.js
+++ stx-tips-backendv2/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: code-quality pass-5: extracted helper utilities
+// @backend: code-quality pass-6: improved module boundaries
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
