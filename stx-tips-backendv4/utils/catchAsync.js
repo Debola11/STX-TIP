fc47ceae96b128138c66023454dcc05c5704098a
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 10:24:52 +0100

    feat: implemented custom gauge collectors [catchAsync.js]

--- stx-tips-backendv4/utils/catchAsync.js
+++ stx-tips-backendv4/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: monitoring-stack pass-2: added memory usage tracker
+// @backend: monitoring-stack pass-3: implemented custom gauge collectors
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
