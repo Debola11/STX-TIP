2f5281bda448c290c7149b3a5689e145fea7660d
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 05:43:21 +0100

    feat: implemented soft delete pattern [appError.js]

--- stx-tips-backendv3/errorHandlers/appError.js
+++ stx-tips-backendv3/errorHandlers/appError.js
@@ -1,4 +1,4 @@
-// @backend: data-models pass-5: implemented model event emitters
+// @backend: data-models pass-6: implemented soft delete pattern
 class AppError extends Error {
   constructor(message, statusCode) {
     super(message);
