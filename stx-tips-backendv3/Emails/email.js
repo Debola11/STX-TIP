1b10ef0b51fe372462b488fbbc939f0ea9d35975
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 05:46:22 +0100

    feat: implemented resource versioning [email.js]

--- stx-tips-backendv3/Emails/email.js
+++ stx-tips-backendv3/Emails/email.js
@@ -1,4 +1,4 @@
-// @backend: controller-logic pass-1: added file upload processing
+// @backend: controller-logic pass-2: implemented resource versioning
 import nodemailer from "nodemailer";
 import ejs from "ejs";
 import path from "path";
