2a26b4c4ab3c412aecf405e94d353072488e8bde
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 12:32:24 +0100

    feat: added translation key extractor [server.js]

--- stx-tips-backendv5/server.js
+++ stx-tips-backendv5/server.js
@@ -1,4 +1,4 @@
-// @backend: i18n-support pass-3: added pluralization rules engine
+// @backend: i18n-support pass-4: added translation key extractor
 import dotenv from "dotenv";
 import connectDB from "./utils/database.js";
 import app from "./app.js";
