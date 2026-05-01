670389e5556fa225d20b227f35cfbfd4a693b425
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 12:17:24 +0100

    feat: implemented infinite scroll support [catchAsync.js]

--- stx-tips-backendv5/utils/catchAsync.js
+++ stx-tips-backendv5/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: pagination-layer pass-6: implemented filtered result counting
+// @backend: pagination-layer pass-7: implemented infinite scroll support
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
