8d5e7939908fe17e915f0f56ad4f4d8a28aea385
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 12:20:54 +0100

    feat: implemented multipart upload handler [catchAsync.js]

--- stx-tips-backendv5/utils/catchAsync.js
+++ stx-tips-backendv5/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: file-storage pass-3: implemented file type validation
+// @backend: file-storage pass-4: implemented multipart upload handler
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
