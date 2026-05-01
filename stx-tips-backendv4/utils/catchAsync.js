03cb852d6823ac4ab099955d7aee3ca61d2b137c
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 09:55:49 +0100

    feat: implemented force logout mechanism [catchAsync.js]

--- stx-tips-backendv4/utils/catchAsync.js
+++ stx-tips-backendv4/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: session-management pass-3: implemented session expiry handler
+// @backend: session-management pass-4: implemented force logout mechanism
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
