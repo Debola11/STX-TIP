4137f8f807e9f6846c2bb6c9624640b3c728a59a
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 11:58:43 +0100

    style: use private class fields

--- stx-tips-backendv5/errorHandlers/appError.js
+++ stx-tips-backendv5/errorHandlers/appError.js
@@ -1,4 +1,4 @@
-// @backend: fix: preserve original error cause
+// @backend: style: use private class fields
 class AppError extends Error {
   constructor(message, statusCode) {
     super(message);
