5879afeb5ad282bfb27fb63f1f64c09d8e0da4c8
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 10:23:17 +0100

    feat: added Prometheus metrics endpoint [catchAsync.js]

--- stx-tips-backendv4/utils/catchAsync.js
+++ stx-tips-backendv4/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: webhook-system pass-7: implemented webhook event filtering
+// @backend: monitoring-stack pass-1: added Prometheus metrics endpoint
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
