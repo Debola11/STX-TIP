577083f7e1837c4ff05e75ab5220ee3eccd111b8
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 05:50:17 +0100

    feat: added webhook dispatch logic [catchAsync.js]

--- stx-tips-backendv3/utils/catchAsync.js
+++ stx-tips-backendv3/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: controller-logic pass-6: added conditional response caching
+// @backend: controller-logic pass-7: added webhook dispatch logic
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
