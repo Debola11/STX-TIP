d8698aa41d6148d8e8103c37dadd305f2142c86c
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 05:55:40 +0100

    feat: added versioned API endpoints [server.js]

--- stx-tips-backendv3/server.js
+++ stx-tips-backendv3/server.js
@@ -1,4 +1,4 @@
-// @backend: route-layer pass-6: implemented dynamic route loading
+// @backend: route-layer pass-7: added versioned API endpoints
 import dotenv from "dotenv";
 import connectDB from "./utils/database.js";
 import app from "./app.js";
