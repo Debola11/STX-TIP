499addd7e3dd01f38b6e388cf257f0905a26f26a
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 05:50:03 +0100

    feat: implemented bulk update handler [UserModel.js]

--- stx-tips-backendv3/models/UserModel.js
+++ stx-tips-backendv3/models/UserModel.js
@@ -1,4 +1,4 @@
-// @backend: controller-logic pass-6: added webhook dispatch logic
+// @backend: controller-logic pass-7: implemented bulk update handler
 import mongoose from "mongoose";
 import validator from "validator";
 import bcrypt from "bcryptjs";
