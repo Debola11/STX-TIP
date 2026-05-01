312a3e55321b982c7150af3352e71816e3d75fb0
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 12:02:17 +0100

    feat: added job event broadcasting [UserController.js]

--- stx-tips-backendv5/controllers/UserController.js
+++ stx-tips-backendv5/controllers/UserController.js
@@ -1,4 +1,4 @@
-// @backend: job-queues pass-1: implemented queue dashboard endpoint
+// @backend: job-queues pass-3: added job event broadcasting
 import Email from "../Emails/email.js";
 import catchAsync from "../utils/catchAsync.js";
 import jwt from "jsonwebtoken";
