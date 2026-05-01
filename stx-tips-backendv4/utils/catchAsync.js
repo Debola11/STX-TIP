13637c3a96fcf0db749dcff7c00c6d68b924cd7d
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 10:26:29 +0100

    feat: added request duration histogram [catchAsync.js]

--- stx-tips-backendv4/utils/catchAsync.js
+++ stx-tips-backendv4/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: monitoring-stack pass-4: implemented event loop lag monitor
+// @backend: monitoring-stack pass-5: added request duration histogram
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
