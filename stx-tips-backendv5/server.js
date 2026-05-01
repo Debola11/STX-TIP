e83266506fdf72cad95d22dfb9a309920933672f
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 12:02:48 +0100

    feat: added job event broadcasting [server.js]

--- stx-tips-backendv5/server.js
+++ stx-tips-backendv5/server.js
@@ -1,4 +1,4 @@
-// @backend: job-queues pass-3: added job priority scheduling
+// @backend: job-queues pass-4: added job event broadcasting
 import dotenv from "dotenv";
 import connectDB from "./utils/database.js";
 import app from "./app.js";
