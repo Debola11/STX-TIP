3dfc205d64fa7042911bcb238a86d867f92ab182
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 10:25:40 +0100

    feat: implemented event loop lag monitor [catchAsync.js]

--- stx-tips-backendv4/utils/catchAsync.js
+++ stx-tips-backendv4/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: monitoring-stack pass-3: implemented custom gauge collectors
+// @backend: monitoring-stack pass-4: implemented event loop lag monitor
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
