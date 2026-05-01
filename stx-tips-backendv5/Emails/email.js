470467f76eec1b4a45c2e7b18ab5b0983bfb1249
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 12:11:40 +0100

    feat: implemented distributed rate counter [email.js]

--- stx-tips-backendv5/Emails/email.js
+++ stx-tips-backendv5/Emails/email.js
@@ -1,4 +1,4 @@
-// @backend: rate-limiting pass-6: added sliding window rate limiter
+// @backend: rate-limiting pass-7: implemented distributed rate counter
 import nodemailer from "nodemailer";
 import ejs from "ejs";
 import path from "path";
