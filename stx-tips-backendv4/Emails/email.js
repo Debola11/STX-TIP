2cdb2e6d837cd4f57ae1e3d897c14d67c41b2387
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 10:31:33 +0100

    fix: implemented token revocation list [email.js]

--- stx-tips-backendv4/Emails/email.js
+++ stx-tips-backendv4/Emails/email.js
@@ -1,4 +1,4 @@
-// @backend: security-hardening pass-3: implemented request origin validation
+// @backend: security-hardening pass-4: implemented token revocation list
 import nodemailer from "nodemailer";
 import ejs from "ejs";
 import path from "path";
