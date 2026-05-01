efd3ff2369f4332dbaccb7211bb91b2db74b1e5e
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 12:11:33 +0100

    feat: implemented tiered rate limit plans [catchAsync.js]

--- stx-tips-backendv5/utils/catchAsync.js
+++ stx-tips-backendv5/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: rate-limiting pass-6: implemented token bucket algorithm
+// @backend: rate-limiting pass-7: implemented tiered rate limit plans
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
