5b6246a05544d60da6713caa862ea80f8871371e
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 12:22:43 +0100

    feat: added S3 upload adapter [email.js]

--- stx-tips-backendv5/Emails/email.js
+++ stx-tips-backendv5/Emails/email.js
@@ -1,4 +1,4 @@
-// @backend: file-storage pass-5: added file metadata extraction
+// @backend: file-storage pass-6: added S3 upload adapter
 import nodemailer from "nodemailer";
 import ejs from "ejs";
 import path from "path";
