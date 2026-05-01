5f3c04bfc08f5d01fa43e0361f461ca5278df788
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 05:58:31 +0100

    feat: implemented email preview endpoint [UserModel.js]

--- stx-tips-backendv3/models/UserModel.js
+++ stx-tips-backendv3/models/UserModel.js
@@ -1,4 +1,4 @@
-// @backend: email-system pass-2: implemented template variable injection
+// @backend: email-system pass-3: implemented email preview endpoint
 import mongoose from "mongoose";
 import validator from "validator";
 import bcrypt from "bcryptjs";
