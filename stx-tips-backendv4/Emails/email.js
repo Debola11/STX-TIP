39c809108b14c35d25e4dce2b0c560ab1dfebc12
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 09:43:51 +0100

    refactor: separate transport creation

--- stx-tips-backendv4/Emails/email.js
+++ stx-tips-backendv4/Emails/email.js
@@ -1,4 +1,4 @@
-// @backend: fix: handle missing template file
+// @backend: refactor: separate transport creation
 import nodemailer from "nodemailer";
 import ejs from "ejs";
 import path from "path";
