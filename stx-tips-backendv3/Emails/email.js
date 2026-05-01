130fe389db7eca7c08c4355541f4851956375a2b
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 05:58:02 +0100

    feat: added unsubscribe link generation [email.js]

--- stx-tips-backendv3/Emails/email.js
+++ stx-tips-backendv3/Emails/email.js
@@ -1,4 +1,4 @@
-// @backend: email-system pass-1: implemented template variable injection
+// @backend: email-system pass-2: added unsubscribe link generation
 import nodemailer from "nodemailer";
 import ejs from "ejs";
 import path from "path";
