149432891768177595fe0acb3cb73e6d955b9674
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 06:06:57 +0100

    fix: implemented error recovery middleware [catchAsync.js]

--- stx-tips-backendv3/utils/catchAsync.js
+++ stx-tips-backendv3/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: error-recovery pass-5: added dead letter queue for failed jobs
+// @backend: error-recovery pass-6: implemented error recovery middleware
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
