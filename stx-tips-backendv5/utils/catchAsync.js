2e11c5be724a24c72ae418671553b5fb4ed89fb2
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 12:27:00 +0100

    feat: implemented A/B test assignment [catchAsync.js]

--- stx-tips-backendv5/utils/catchAsync.js
+++ stx-tips-backendv5/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: analytics-engine pass-3: implemented funnel analysis queries
+// @backend: analytics-engine pass-4: implemented A/B test assignment
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
