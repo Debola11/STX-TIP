d79c7da0969234bbcbee7a62bc6a1f5a73c3a902
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 05:52:00 +0100

    feat: added versioned API endpoints [catchAsync.js]

--- stx-tips-backendv3/utils/catchAsync.js
+++ stx-tips-backendv3/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: route-layer pass-1: added nested resource routes
+// @backend: route-layer pass-2: added versioned API endpoints
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
