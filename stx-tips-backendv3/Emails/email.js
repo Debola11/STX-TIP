f24b5e578c3e9270d15579fc2e6a1b3c031268dc
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 05:37:17 +0100

    feat: implemented session invalidation [email.js]

--- stx-tips-backendv3/Emails/email.js
+++ stx-tips-backendv3/Emails/email.js
@@ -1,4 +1,4 @@
-// @backend: auth-system pass-4: added OAuth callback handler
+// @backend: auth-system pass-5: implemented session invalidation
 import nodemailer from "nodemailer";
 import ejs from "ejs";
 import path from "path";
