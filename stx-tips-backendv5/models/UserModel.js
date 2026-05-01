5e002b7c44afe7dd2688ec5308019e84b3b4e12a
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 11:56:28 +0100

    fix: enforce unique email constraint

--- stx-tips-backendv5/models/UserModel.js
+++ stx-tips-backendv5/models/UserModel.js
@@ -1,4 +1,4 @@
-// @backend: feat: add account verification field
+// @backend: fix: enforce unique email constraint
 import mongoose from "mongoose";
 import validator from "validator";
 import bcrypt from "bcryptjs";
