a306e427253a8c376ecf44bb0d613016ec3a33b4
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 10:19:07 +0100

    feat: implemented webhook payload signing [catchAsync.js]

--- stx-tips-backendv4/utils/catchAsync.js
+++ stx-tips-backendv4/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: webhook-system pass-2: added delivery log with timestamps
+// @backend: webhook-system pass-3: implemented webhook payload signing
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
