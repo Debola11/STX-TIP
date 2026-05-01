d8fbe7cedfa8fdec887651535847663f7ad27c98
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 12:14:58 +0100

    feat: implemented keyset pagination [catchAsync.js]

--- stx-tips-backendv5/utils/catchAsync.js
+++ stx-tips-backendv5/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: pagination-layer pass-3: implemented page size validation
+// @backend: pagination-layer pass-4: implemented keyset pagination
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
