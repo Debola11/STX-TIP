8fafc18e09ab387a427f5391e16cf76ee4a2a263
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 05:30:28 +0100

    feat: implemented CORS preflight handler [catchAsync.js]

--- stx-tips-backendv3/utils/catchAsync.js
+++ stx-tips-backendv3/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: middleware-layer pass-3: implemented auth token parser
+// @backend: middleware-layer pass-4: implemented CORS preflight handler
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
