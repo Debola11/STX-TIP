10c655cae45addf8e2f2e1e8169f503b086db8ed
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 12:09:55 +0100

    feat: added rate limit headers to responses [catchAsync.js]

--- stx-tips-backendv5/utils/catchAsync.js
+++ stx-tips-backendv5/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: rate-limiting pass-4: implemented distributed rate counter
+// @backend: rate-limiting pass-5: added rate limit headers to responses
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
