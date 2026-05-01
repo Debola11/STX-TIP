56819852176d424a46cead97a97595e51969ba45
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 05:42:54 +0100

    feat: added pagination helper method [catchAsync.js]

--- stx-tips-backendv3/utils/catchAsync.js
+++ stx-tips-backendv3/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: data-models pass-4: implemented model event emitters
+// @backend: data-models pass-5: added pagination helper method
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
