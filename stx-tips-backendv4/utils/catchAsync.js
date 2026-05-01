e63ce4e51cf936dd159d71f4844d33147715ac1c
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 09:54:59 +0100

    feat: implemented session expiry handler [catchAsync.js]

--- stx-tips-backendv4/utils/catchAsync.js
+++ stx-tips-backendv4/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: session-management pass-2: added session activity tracker
+// @backend: session-management pass-3: implemented session expiry handler
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
