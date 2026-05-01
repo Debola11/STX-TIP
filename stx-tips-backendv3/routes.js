d3ecee30037dfbae97da8aa2d6af9c917e42443d
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 05:50:10 +0100

    feat: added file upload processing [routes.js]

--- stx-tips-backendv3/routes.js
+++ stx-tips-backendv3/routes.js
@@ -1,4 +1,4 @@
-// @backend: controller-logic pass-6: added batch delete handler
+// @backend: controller-logic pass-7: added file upload processing
 import express from "express";
 import userRouter from "./routes/userRoutes.js";
 
