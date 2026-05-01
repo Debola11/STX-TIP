d1423a8fd027f7647ba9cdfbb74441eb35390d90
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 05:27:38 +0100

    feat: added request logging middleware [server.js]

--- stx-tips-backendv3/server.js
+++ stx-tips-backendv3/server.js
@@ -1,4 +1,4 @@
-// @backend: ci: add startup readiness log
+// @backend: middleware-layer pass-1: added request logging middleware
 import dotenv from "dotenv";
 import connectDB from "./utils/database.js";
 import app from "./app.js";
