be10775013e34936757938b296f65c2f96661b55
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 06:03:44 +0100

    fix: added error correlation IDs [catchAsync.js]

--- stx-tips-backendv3/utils/catchAsync.js
+++ stx-tips-backendv3/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: error-recovery pass-1: added circuit breaker for DB calls
+// @backend: error-recovery pass-2: added error correlation IDs
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
