49e2a48478930e14e483915fed46593c709df175
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 09:48:02 +0100

    feat: implemented log level filtering [UserModel.js]

--- stx-tips-backendv4/models/UserModel.js
+++ stx-tips-backendv4/models/UserModel.js
@@ -1,4 +1,4 @@
-// @backend: structured-logging pass-1: added structured JSON log format
+// @backend: structured-logging pass-2: implemented log level filtering
 import mongoose from "mongoose";
 import validator from "validator";
 import bcrypt from "bcryptjs";
