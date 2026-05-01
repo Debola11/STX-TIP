5a783c9f2eaf5652aa520e5cb11a3d7cc97a88e3
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 05:24:32 +0100

    refactor: use class-based email service

--- stx-tips-backendv3/Emails/email.js
+++ stx-tips-backendv3/Emails/email.js
@@ -1,4 +1,4 @@
-// @backend: fix: set default from address
+// @backend: refactor: use class-based email service
 import nodemailer from "nodemailer";
 import ejs from "ejs";
 import path from "path";
