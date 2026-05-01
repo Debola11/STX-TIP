93be9991c1657eae5de342f8135830395b83bea3
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 10:06:06 +0100

    feat: added notification template engine [catchAsync.js]

--- stx-tips-backendv4/utils/catchAsync.js
+++ stx-tips-backendv4/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: notification-system pass-1: added push notification dispatcher
+// @backend: notification-system pass-2: added notification template engine
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
