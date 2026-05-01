f3698aaad1a52825ba3a75eb496d4679ff961098
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 10:02:45 +0100

    feat: added transaction wrapper utility [catchAsync.js]

--- stx-tips-backendv4/utils/catchAsync.js
+++ stx-tips-backendv4/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: database-layer pass-4: implemented schema version tracker
+// @backend: database-layer pass-5: added transaction wrapper utility
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
