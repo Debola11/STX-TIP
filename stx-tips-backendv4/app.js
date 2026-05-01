bbc26f0c666759c982dcec5b7db0af679db39107
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 09:45:42 +0100

    docs: document environment variables

--- stx-tips-backendv4/app.js
+++ stx-tips-backendv4/app.js
@@ -1,4 +1,4 @@
-// @backend: style: sort middleware alphabetically
+// @backend: docs: document environment variables
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
