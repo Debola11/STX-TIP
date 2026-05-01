861135833fdee49fe29c6132d9ecd1da426a2455
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 05:28:05 +0100

    feat: added request logging middleware [catchAsync.js]

--- stx-tips-backendv3/utils/catchAsync.js
+++ stx-tips-backendv3/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: docs: explain wrapper pattern
+// @backend: middleware-layer pass-1: added request logging middleware
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
