19c757e0b4158d55bc9d2a692f7d5e86c4ad0b0c
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 10:30:37 +0100

    fix: implemented brute force protection [catchAsync.js]

--- stx-tips-backendv4/utils/catchAsync.js
+++ stx-tips-backendv4/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: security-hardening pass-2: added sensitive field redaction
+// @backend: security-hardening pass-3: implemented brute force protection
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
