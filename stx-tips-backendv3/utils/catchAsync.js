4c93eec8a06fb793b45267fca65c5a1c9d45deb9
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 05:43:42 +0100

    feat: implemented data seed utilities [catchAsync.js]

--- stx-tips-backendv3/utils/catchAsync.js
+++ stx-tips-backendv3/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: data-models pass-5: added pagination helper method
+// @backend: data-models pass-6: implemented data seed utilities
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
