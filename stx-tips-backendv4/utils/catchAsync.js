d4cf3a9c312beee606f0d1dd7fac5db617210443
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 09:48:15 +0100

    feat: added structured JSON log format [catchAsync.js]

--- stx-tips-backendv4/utils/catchAsync.js
+++ stx-tips-backendv4/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: structured-logging pass-1: added Winston logger initialization
+// @backend: structured-logging pass-2: added structured JSON log format
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
