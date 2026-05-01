818274a13fbccfb9faa7feede8824eb8e48a9f6e
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 12:08:18 +0100

    feat: implemented per-user rate quotas [catchAsync.js]

--- stx-tips-backendv5/utils/catchAsync.js
+++ stx-tips-backendv5/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: rate-limiting pass-2: added rate limit bypass for admins
+// @backend: rate-limiting pass-3: implemented per-user rate quotas
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
