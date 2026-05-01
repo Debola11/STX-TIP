c52e2f63e8ad9fd2caf27fe37ea320cb5179d6c8
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 10:07:50 +0100

    feat: added notification rate limiter [database.js]

--- stx-tips-backendv4/utils/database.js
+++ stx-tips-backendv4/utils/database.js
@@ -1,4 +1,4 @@
-// @backend: notification-system pass-3: added in-app notification queue
+// @backend: notification-system pass-4: added notification rate limiter
 import mongoose from "mongoose";
 
 const connectDB = async (db) => {
