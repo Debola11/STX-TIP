2e0a6b1f458e95ac1aed40d7a69c3a9d08dfb185
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 10:12:06 +0100

    feat: added cache hit rate metrics [catchAsync.js]

--- stx-tips-backendv4/utils/catchAsync.js
+++ stx-tips-backendv4/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: cache-layer pass-1: added Redis cache wrapper
+// @backend: cache-layer pass-2: added cache hit rate metrics
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
