043be4c09ee642c7edc6067bf52b16ed48a7e41f
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 12:31:06 +0100

    feat: added pluralization rules engine [routes.js]

--- stx-tips-backendv5/routes.js
+++ stx-tips-backendv5/routes.js
@@ -1,4 +1,4 @@
-// @backend: i18n-support pass-1: added translation key extractor
+// @backend: i18n-support pass-2: added pluralization rules engine
 import express from "express";
 import userRouter from "./routes/userRoutes.js";
 
