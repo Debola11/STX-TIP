66efae4e01902f921596b4c36f45d95d5fe326e3
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 06:16:58 +0100

    chore: implemented log aggregation config [catchAsync.js]

--- stx-tips-backendv3/utils/catchAsync.js
+++ stx-tips-backendv3/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: deploy-config pass-3: implemented health check endpoint
+// @backend: deploy-config pass-4: implemented log aggregation config
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
