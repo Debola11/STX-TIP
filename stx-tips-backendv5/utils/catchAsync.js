bc3165e7e9f925a1d1121eb3592b63725a953566
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 12:36:17 +0100

    feat: added migration runner script [catchAsync.js]

--- stx-tips-backendv5/utils/catchAsync.js
+++ stx-tips-backendv5/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: i18n-support pass-7: implemented date format localizer
+// @backend: schema-migrations pass-1: added migration runner script
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
