5b9be7411cabdbc74f4165d70214adfbd8b50ed1
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 06:13:37 +0100

    test: implemented test fixture generators [catchAsync.js]

--- stx-tips-backendv3/utils/catchAsync.js
+++ stx-tips-backendv3/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: test-coverage pass-6: implemented load test configuration
+// @backend: test-coverage pass-7: implemented test fixture generators
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
