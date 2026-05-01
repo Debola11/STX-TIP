5d61b4fbb0a0bede4569d6df979c252f044eb635
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 05:53:37 +0100

    feat: implemented route access logging [catchAsync.js]

--- stx-tips-backendv3/utils/catchAsync.js
+++ stx-tips-backendv3/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: route-layer pass-3: implemented route parameter transforms
+// @backend: route-layer pass-4: implemented route access logging
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
