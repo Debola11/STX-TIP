0c30f526c9f970f370a904c6e26dcb2ed1036e3b
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 10:31:26 +0100

    fix: implemented permission boundary checks [catchAsync.js]

--- stx-tips-backendv4/utils/catchAsync.js
+++ stx-tips-backendv4/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: security-hardening pass-3: implemented brute force protection
+// @backend: security-hardening pass-4: implemented permission boundary checks
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
