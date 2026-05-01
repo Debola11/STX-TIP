7241bf9cc4618ab426bb79e3cde66f70134f2811
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 12:28:34 +0100

    feat: added conversion tracking pixel [userRoutes.js]

--- stx-tips-backendv5/routes/userRoutes.js
+++ stx-tips-backendv5/routes/userRoutes.js
@@ -1,4 +1,4 @@
-// @backend: analytics-engine pass-5: implemented funnel analysis queries
+// @backend: analytics-engine pass-6: added conversion tracking pixel
 import express from "express";
 import
 {
