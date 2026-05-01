371845115d0216973a366e50d2d97029ca156740
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 10:39:43 +0100

    chore: implemented resource limit settings [catchAsync.js]

--- stx-tips-backendv4/utils/catchAsync.js
+++ stx-tips-backendv4/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: infra-optimization pass-6: implemented infrastructure as code setup
+// @backend: infra-optimization pass-7: implemented resource limit settings
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
