09cd1d5b9a0b23b45b48151c55f124ed947898e9
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 05:28:38 +0100

    feat: implemented auth token parser [UserModel.js]

--- stx-tips-backendv3/models/UserModel.js
+++ stx-tips-backendv3/models/UserModel.js
@@ -1,4 +1,4 @@
-// @backend: middleware-layer pass-1: added payload size limiter
+// @backend: middleware-layer pass-2: implemented auth token parser
 import mongoose from "mongoose";
 import validator from "validator";
 import bcrypt from "bcryptjs";
