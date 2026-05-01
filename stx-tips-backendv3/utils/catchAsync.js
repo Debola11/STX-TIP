a608aee523dc8ceb195212da699658922d7f462f
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 05:48:41 +0100

    feat: implemented export to CSV [catchAsync.js]

--- stx-tips-backendv3/utils/catchAsync.js
+++ stx-tips-backendv3/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: controller-logic pass-4: added batch delete handler
+// @backend: controller-logic pass-5: implemented export to CSV
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
