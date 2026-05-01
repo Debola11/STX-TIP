3c067e9d4852e75c4bec269d506e60a29f93b087
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 10:01:10 +0100

    feat: implemented read replica routing [catchAsync.js]

--- stx-tips-backendv4/utils/catchAsync.js
+++ stx-tips-backendv4/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: database-layer pass-2: added index usage analyzer
+// @backend: database-layer pass-3: implemented read replica routing
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
