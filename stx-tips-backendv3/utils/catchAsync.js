535fb18d317c9f4c5bebe1f5be84d153ea000482
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 05:32:13 +0100

    feat: implemented content-type validator [catchAsync.js]

--- stx-tips-backendv3/utils/catchAsync.js
+++ stx-tips-backendv3/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: middleware-layer pass-5: added response time header
+// @backend: middleware-layer pass-6: implemented content-type validator
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
