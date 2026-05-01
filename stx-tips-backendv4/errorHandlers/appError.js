f5834a3869dd1fdff91221594be52898a2623689
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 10:20:22 +0100

    feat: implemented webhook secret rotation [appError.js]

--- stx-tips-backendv4/errorHandlers/appError.js
+++ stx-tips-backendv4/errorHandlers/appError.js
@@ -1,4 +1,4 @@
-// @backend: webhook-system pass-4: added webhook registration endpoint
+// @backend: webhook-system pass-5: implemented webhook secret rotation
 class AppError extends Error {
   constructor(message, statusCode) {
     super(message);
