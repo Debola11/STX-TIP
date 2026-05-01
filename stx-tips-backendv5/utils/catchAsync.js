012055bdf8d33011551a01fc188290719744f62e
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 12:07:29 +0100

    feat: added rate limit bypass for admins [catchAsync.js]

--- stx-tips-backendv5/utils/catchAsync.js
+++ stx-tips-backendv5/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: rate-limiting pass-1: added sliding window rate limiter
+// @backend: rate-limiting pass-2: added rate limit bypass for admins
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
