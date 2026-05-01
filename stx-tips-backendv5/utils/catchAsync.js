4b1c0c2e58654efba435c59c9cb8b84195792a82
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 12:51:29 +0100

    chore: updated package scripts section [catchAsync.js]

--- stx-tips-backendv5/utils/catchAsync.js
+++ stx-tips-backendv5/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: final-polish pass-3: updated all error messages for consistency
+// @backend: final-polish pass-4: updated package scripts section
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
