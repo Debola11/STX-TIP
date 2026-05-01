aebb82734c198ad1a2973114cb2f63a89c04310b
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 11:57:27 +0100

    chore: add request tracing header

--- stx-tips-backendv5/controllers/UserController.js
+++ stx-tips-backendv5/controllers/UserController.js
@@ -1,4 +1,4 @@
-// @backend: refactor: use service layer pattern
+// @backend: chore: add request tracing header
 import Email from "../Emails/email.js";
 import catchAsync from "../utils/catchAsync.js";
 import jwt from "jsonwebtoken";
