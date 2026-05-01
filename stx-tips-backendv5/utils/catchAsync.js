3cf30e3a640802a609aa5f0ef6f075d17c0d8ec4
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 12:42:56 +0100

    feat: added audit log retention policy [catchAsync.js]

--- stx-tips-backendv5/utils/catchAsync.js
+++ stx-tips-backendv5/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: audit-logging pass-1: added audit event dispatcher
+// @backend: audit-logging pass-2: added audit log retention policy
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
