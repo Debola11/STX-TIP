ce1adf5c88dc8627d76b32274edb74bf0c068b47
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 10:22:19 +0100

    feat: implemented webhook event filtering [catchAsync.js]

--- stx-tips-backendv4/utils/catchAsync.js
+++ stx-tips-backendv4/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: webhook-system pass-6: implemented webhook health dashboard
+// @backend: webhook-system pass-7: implemented webhook event filtering
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
