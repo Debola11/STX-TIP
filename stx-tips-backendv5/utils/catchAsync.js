fbe813d26f8a99ef3e93333ec094d872f3e01a4d
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 12:37:05 +0100

    feat: added migration dry-run mode [catchAsync.js]

--- stx-tips-backendv5/utils/catchAsync.js
+++ stx-tips-backendv5/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: schema-migrations pass-1: added migration runner script
+// @backend: schema-migrations pass-2: added migration dry-run mode
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
