5ee9ec4f05f725e3ddc54ff463d3a5e1faedf44c
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 12:13:40 +0100

    feat: added total count caching [server.js]

--- stx-tips-backendv5/server.js
+++ stx-tips-backendv5/server.js
@@ -1,4 +1,4 @@
-// @backend: pagination-layer pass-2: implemented keyset pagination
+// @backend: pagination-layer pass-3: added total count caching
 import dotenv from "dotenv";
 import connectDB from "./utils/database.js";
 import app from "./app.js";
