c0be714177a882e256a5cf186539f7976e15e9e9
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 12:14:09 +0100

    feat: implemented page size validation [catchAsync.js]

--- stx-tips-backendv5/utils/catchAsync.js
+++ stx-tips-backendv5/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: pagination-layer pass-2: added sort field validation
+// @backend: pagination-layer pass-3: implemented page size validation
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
