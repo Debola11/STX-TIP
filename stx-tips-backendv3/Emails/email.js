e49fc7bb5eae0b8cb4ddc4c1ebf8ceca74b97b04
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 06:09:40 +0100

    test: added error handler test cases [email.js]

--- stx-tips-backendv3/Emails/email.js
+++ stx-tips-backendv3/Emails/email.js
@@ -1,4 +1,4 @@
-// @backend: test-coverage pass-1: implemented mock database layer
+// @backend: test-coverage pass-2: added error handler test cases
 import nodemailer from "nodemailer";
 import ejs from "ejs";
 import path from "path";
