0de1995ef97ec921fb98e422eb78b9daa1cb025c
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 12:34:29 +0100

    feat: implemented fallback locale chain [catchAsync.js]

--- stx-tips-backendv5/utils/catchAsync.js
+++ stx-tips-backendv5/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: i18n-support pass-5: added pluralization rules engine
+// @backend: i18n-support pass-6: implemented fallback locale chain
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
