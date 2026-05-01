9f30eb0ca7b923028b6b444cd6c5d961434b353b
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 12:01:39 +0100

    feat: added job progress tracking [catchAsync.js]

--- stx-tips-backendv5/utils/catchAsync.js
+++ stx-tips-backendv5/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: job-queues pass-1: added Bull queue for background jobs
+// @backend: job-queues pass-2: added job progress tracking
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
