3fd36616b4072af44b91dc9f6b7979c5230b0ac3
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 12:33:16 +0100

    feat: added currency display formatter [app.js]

--- stx-tips-backendv5/app.js
+++ stx-tips-backendv5/app.js
@@ -1,4 +1,4 @@
-// @backend: i18n-support pass-4: implemented fallback locale chain
+// @backend: i18n-support pass-5: added currency display formatter
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
