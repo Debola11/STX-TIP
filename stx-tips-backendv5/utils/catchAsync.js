bde7dc1333273e8c21211584e46fb56af6da8695
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 12:29:26 +0100

    feat: implemented real-time metric stream [catchAsync.js]

--- stx-tips-backendv5/utils/catchAsync.js
+++ stx-tips-backendv5/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: analytics-engine pass-6: implemented usage report generator
+// @backend: analytics-engine pass-7: implemented real-time metric stream
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
