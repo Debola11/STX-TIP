077461f2344a2809e002054bc18f115ddc58bcbe
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 10:08:42 +0100

    feat: added notification template engine [email.js]

--- stx-tips-backendv4/Emails/email.js
+++ stx-tips-backendv4/Emails/email.js
@@ -1,4 +1,4 @@
-// @backend: notification-system pass-4: implemented channel priority routing
+// @backend: notification-system pass-5: added notification template engine
 import nodemailer from "nodemailer";
 import ejs from "ejs";
 import path from "path";
