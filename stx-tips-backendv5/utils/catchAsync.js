e20012bfffcfa69b64d207be36b99d27ae40c5c8
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 12:47:57 +0100

    chore: cleaned up unused dependencies [catchAsync.js]

--- stx-tips-backendv5/utils/catchAsync.js
+++ stx-tips-backendv5/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: audit-logging pass-7: implemented compliance report builder
+// @backend: final-polish pass-1: cleaned up unused dependencies
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
