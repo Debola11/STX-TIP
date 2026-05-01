0218a290a073e46383dcf10abd6acbefa61a91cd
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 05:37:10 +0100

    feat: implemented refresh token rotation [catchAsync.js]

--- stx-tips-backendv3/utils/catchAsync.js
+++ stx-tips-backendv3/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: auth-system pass-4: added login attempt throttling
+// @backend: auth-system pass-5: implemented refresh token rotation
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
