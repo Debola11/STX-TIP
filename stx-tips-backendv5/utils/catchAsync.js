f904917ebba09d793af89eaa1a5af6f349bb15ea
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 12:15:47 +0100

    feat: added total count caching [catchAsync.js]

--- stx-tips-backendv5/utils/catchAsync.js
+++ stx-tips-backendv5/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: pagination-layer pass-4: implemented keyset pagination
+// @backend: pagination-layer pass-5: added total count caching
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
