be185fca5a6cb95d0c99a59464c7a684508d7650
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 05:41:20 +0100

    feat: implemented soft delete pattern [catchAsync.js]

--- stx-tips-backendv3/utils/catchAsync.js
+++ stx-tips-backendv3/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: data-models pass-2: added aggregate pipeline helpers
+// @backend: data-models pass-3: implemented soft delete pattern
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
