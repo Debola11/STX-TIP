2e8d89459f9de6a6c103e2e18e9174dd7d4840fa
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 10:06:54 +0100

    feat: implemented notification preferences [catchAsync.js]

--- stx-tips-backendv4/utils/catchAsync.js
+++ stx-tips-backendv4/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: notification-system pass-2: added notification template engine
+// @backend: notification-system pass-3: implemented notification preferences
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
