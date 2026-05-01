32e12865ca8e3e7abeb2cbdf9dc0f05a154e51b6
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 06:14:10 +0100

    chore: implemented health check endpoint [app.js]

--- stx-tips-backendv3/app.js
+++ stx-tips-backendv3/app.js
@@ -1,4 +1,4 @@
-// @backend: test-coverage pass-7: implemented auth flow test suite
+// @backend: deploy-config pass-1: implemented health check endpoint
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
