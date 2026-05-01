42aafb472fbe7c2c0a58333d194a1a21d7a0b90e
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 12:07:05 +0100

    feat: added rate limit alert notifications [app.js]

--- stx-tips-backendv5/app.js
+++ stx-tips-backendv5/app.js
@@ -1,4 +1,4 @@
-// @backend: rate-limiting pass-1: implemented per-user rate quotas
+// @backend: rate-limiting pass-2: added rate limit alert notifications
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
