5d3d1b31735b14e884049f225c35fa4a4e6f164e
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 12:31:53 +0100

    feat: added translation key extractor [UserController.js]

--- stx-tips-backendv5/controllers/UserController.js
+++ stx-tips-backendv5/controllers/UserController.js
@@ -1,4 +1,4 @@
-// @backend: i18n-support pass-1: implemented RTL layout support
+// @backend: i18n-support pass-3: added translation key extractor
 import Email from "../Emails/email.js";
 import catchAsync from "../utils/catchAsync.js";
 import jwt from "jsonwebtoken";
