9d03190b389e0dd2a156f8bce5ebae01c7e3be77
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 05:47:03 +0100

    feat: added file upload processing [catchAsync.js]

--- stx-tips-backendv3/utils/catchAsync.js
+++ stx-tips-backendv3/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: controller-logic pass-2: implemented search with filters
+// @backend: controller-logic pass-3: added file upload processing
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
