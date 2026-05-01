bd23f8a7638f7f4e55772bcfc8f4625ac32dd938
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 10:37:15 +0100

    chore: implemented blue-green deploy support [catchAsync.js]

--- stx-tips-backendv4/utils/catchAsync.js
+++ stx-tips-backendv4/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: infra-optimization pass-3: implemented container health probes
+// @backend: infra-optimization pass-4: implemented blue-green deploy support
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
