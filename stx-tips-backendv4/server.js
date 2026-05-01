844479d47179f915b5a656f50f1d330314b8dc49
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 09:46:48 +0100

    ci: add graceful drain on shutdown

--- stx-tips-backendv4/server.js
+++ stx-tips-backendv4/server.js
@@ -1,4 +1,4 @@
-// @backend: security: set strict transport security
+// @backend: ci: add graceful drain on shutdown
 import dotenv from "dotenv";
 import connectDB from "./utils/database.js";
 import app from "./app.js";
