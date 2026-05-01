00c0f7f4948e52ad2e9e35d73252db00f914635e
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 10:35:09 +0100

    chore: implemented blue-green deploy support [server.js]

--- stx-tips-backendv4/server.js
+++ stx-tips-backendv4/server.js
@@ -1,4 +1,4 @@
-// @backend: infra-optimization pass-1: added PM2 ecosystem config
+// @backend: infra-optimization pass-2: implemented blue-green deploy support
 import dotenv from "dotenv";
 import connectDB from "./utils/database.js";
 import app from "./app.js";
