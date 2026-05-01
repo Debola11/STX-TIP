0a7f725165c905b7a16068ba47ddfd79a8b759f9
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 10:19:34 +0100

    feat: added webhook registration endpoint [appError.js]

--- stx-tips-backendv4/errorHandlers/appError.js
+++ stx-tips-backendv4/errorHandlers/appError.js
@@ -1,4 +1,4 @@
-// @backend: webhook-system pass-3: implemented webhook event filtering
+// @backend: webhook-system pass-4: added webhook registration endpoint
 class AppError extends Error {
   constructor(message, statusCode) {
     super(message);
