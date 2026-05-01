c9ffdffb8b6a6aa5e19a9d272b8b7fca105902e5
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 10:35:36 +0100

    chore: added auto-scaling trigger metrics [catchAsync.js]

--- stx-tips-backendv4/utils/catchAsync.js
+++ stx-tips-backendv4/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: infra-optimization pass-1: added PM2 ecosystem config
+// @backend: infra-optimization pass-2: added auto-scaling trigger metrics
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
