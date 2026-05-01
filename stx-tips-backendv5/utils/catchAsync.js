bd68c0c77abfb47fe94cca288097643aa3edb8c8
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 12:16:35 +0100

    feat: implemented filtered result counting [catchAsync.js]

--- stx-tips-backendv5/utils/catchAsync.js
+++ stx-tips-backendv5/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: pagination-layer pass-5: added total count caching
+// @backend: pagination-layer pass-6: implemented filtered result counting
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
