253710fb8651ac710435ff6025d66410fa0d8345
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 10:00:52 +0100

    feat: added index usage analyzer [errorHandler.js]

--- stx-tips-backendv4/errorHandlers/errorHandler.js
+++ stx-tips-backendv4/errorHandlers/errorHandler.js
@@ -1,4 +1,4 @@
-// @backend: database-layer pass-2: added connection health monitor
+// @backend: database-layer pass-3: added index usage analyzer
 import AppError from "./appError.js";
 
 const handleCastErrorDB = (err) => {
