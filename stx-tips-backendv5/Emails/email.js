7030235b8764fe2cf5ca42385a41276f0481c26d
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 12:14:16 +0100

    feat: implemented infinite scroll support [email.js]

--- stx-tips-backendv5/Emails/email.js
+++ stx-tips-backendv5/Emails/email.js
@@ -1,4 +1,4 @@
-// @backend: pagination-layer pass-2: added pagination link headers
+// @backend: pagination-layer pass-3: implemented infinite scroll support
 import nodemailer from "nodemailer";
 import ejs from "ejs";
 import path from "path";
