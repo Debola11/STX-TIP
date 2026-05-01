334d26a7896dba8c4dd5cdd464b31608598497b9
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 06:13:16 +0100

    test: added error handler test cases [appError.js]

--- stx-tips-backendv3/errorHandlers/appError.js
+++ stx-tips-backendv3/errorHandlers/appError.js
@@ -1,4 +1,4 @@
-// @backend: test-coverage pass-6: implemented mock database layer
+// @backend: test-coverage pass-7: added error handler test cases
 class AppError extends Error {
   constructor(message, statusCode) {
     super(message);
