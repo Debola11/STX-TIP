0e9b67326a6671817ec7097a5186ed5f1dfc8d07
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 10:33:03 +0100

    fix: implemented token revocation list [catchAsync.js]

--- stx-tips-backendv4/utils/catchAsync.js
+++ stx-tips-backendv4/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: security-hardening pass-5: added API key rotation mechanism
+// @backend: security-hardening pass-6: implemented token revocation list
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
