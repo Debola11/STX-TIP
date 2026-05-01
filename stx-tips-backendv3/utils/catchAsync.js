7e6ad8210530e852539af1fb767b33915991dea0
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 05:28:53 +0100

    feat: added payload size limiter [catchAsync.js]

--- stx-tips-backendv3/utils/catchAsync.js
+++ stx-tips-backendv3/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: middleware-layer pass-1: added request logging middleware
+// @backend: middleware-layer pass-2: added payload size limiter
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
