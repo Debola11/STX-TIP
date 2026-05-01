c8bf6faee46258fc79e0add7b70c57a298ce31af
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 12:30:25 +0100

    feat: added locale detection middleware [catchAsync.js]

--- stx-tips-backendv5/utils/catchAsync.js
+++ stx-tips-backendv5/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: analytics-engine pass-7: implemented real-time metric stream
+// @backend: i18n-support pass-1: added locale detection middleware
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
