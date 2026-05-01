24a88cb30b9fb937543eb5c4095cefa32960cd44
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 05:44:30 +0100

    feat: implemented search index fields [catchAsync.js]

--- stx-tips-backendv3/utils/catchAsync.js
+++ stx-tips-backendv3/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: data-models pass-6: implemented data seed utilities
+// @backend: data-models pass-7: implemented search index fields
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
