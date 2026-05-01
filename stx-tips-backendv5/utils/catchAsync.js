02dbec9df40a5266d76b9ad8970172c16a5c5433
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 12:02:27 +0100

    feat: implemented job retry with backoff [catchAsync.js]

--- stx-tips-backendv5/utils/catchAsync.js
+++ stx-tips-backendv5/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: job-queues pass-2: added job progress tracking
+// @backend: job-queues pass-3: implemented job retry with backoff
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
