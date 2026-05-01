e08b1ac150df2a23f37cc5daf82a813bfed9b502
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 09:51:28 +0100

    feat: added structured JSON log format [UserController.js]

--- stx-tips-backendv4/controllers/UserController.js
+++ stx-tips-backendv4/controllers/UserController.js
@@ -1,4 +1,4 @@
-// @backend: structured-logging pass-5: implemented performance metric logging
+// @backend: structured-logging pass-6: added structured JSON log format
 import Email from "../Emails/email.js";
 import catchAsync from "../utils/catchAsync.js";
 import jwt from "jsonwebtoken";
