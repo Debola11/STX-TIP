fb01cdbaccb15c90743bf16c29d735c66e0e9006
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 05:33:01 +0100

    feat: implemented request rate tracker [catchAsync.js]

--- stx-tips-backendv3/utils/catchAsync.js
+++ stx-tips-backendv3/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: middleware-layer pass-6: implemented content-type validator
+// @backend: middleware-layer pass-7: implemented request rate tracker
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
