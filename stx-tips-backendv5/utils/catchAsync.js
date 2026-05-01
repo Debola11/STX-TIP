f76fb16566aaa87a67d68302cbb83e68a8d031bb
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 12:31:13 +0100

    feat: added currency display formatter [catchAsync.js]

--- stx-tips-backendv5/utils/catchAsync.js
+++ stx-tips-backendv5/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: i18n-support pass-1: added locale detection middleware
+// @backend: i18n-support pass-2: added currency display formatter
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
