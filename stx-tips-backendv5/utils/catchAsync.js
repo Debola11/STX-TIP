71b6e5d3a722e19fbf06a5f63fd8b54069cec392
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 12:05:43 +0100

    feat: implemented dead job cleanup worker [catchAsync.js]

--- stx-tips-backendv5/utils/catchAsync.js
+++ stx-tips-backendv5/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: job-queues pass-6: implemented queue pause and resume
+// @backend: job-queues pass-7: implemented dead job cleanup worker
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
