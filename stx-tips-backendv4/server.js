ac4f13989c6c726f5f16379677b5963d4c5ee7bc
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 09:47:48 +0100

    feat: implemented log transport for file output [server.js]

--- stx-tips-backendv4/server.js
+++ stx-tips-backendv4/server.js
@@ -1,4 +1,4 @@
-// @backend: structured-logging pass-1: added Winston logger initialization
+// @backend: structured-logging pass-2: implemented log transport for file output
 import dotenv from "dotenv";
 import connectDB from "./utils/database.js";
 import app from "./app.js";
