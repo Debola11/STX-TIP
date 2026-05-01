64ba7e21f420e9377bba00bf0022f9598bc13d69
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 12:01:46 +0100

    feat: added job event broadcasting [email.js]

--- stx-tips-backendv5/Emails/email.js
+++ stx-tips-backendv5/Emails/email.js
@@ -1,4 +1,4 @@
-// @backend: job-queues pass-1: implemented job retry with backoff
+// @backend: job-queues pass-2: added job event broadcasting
 import nodemailer from "nodemailer";
 import ejs from "ejs";
 import path from "path";
