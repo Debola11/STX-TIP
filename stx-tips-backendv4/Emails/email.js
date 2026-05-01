6c02ac4f65b6f81031fc7424794fff2ae9c32cca
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 10:39:51 +0100

    chore: implemented blue-green deploy support [email.js]

--- stx-tips-backendv4/Emails/email.js
+++ stx-tips-backendv4/Emails/email.js
@@ -1,4 +1,4 @@
-// @backend: infra-optimization pass-6: added PM2 ecosystem config
+// @backend: infra-optimization pass-7: implemented blue-green deploy support
 import nodemailer from "nodemailer";
 import ejs from "ejs";
 import path from "path";
