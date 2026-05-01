cef10c902cf2442da2c0550f39970ba2fe63c89e
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 12:35:52 +0100

    feat: implemented rollback mechanism [app.js]

--- stx-tips-backendv5/app.js
+++ stx-tips-backendv5/app.js
@@ -1,4 +1,4 @@
-// @backend: i18n-support pass-7: implemented RTL layout support
+// @backend: schema-migrations pass-1: implemented rollback mechanism
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
