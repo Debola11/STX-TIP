52e8d4436adf1fc5d8e139f9609cab78c8145a75
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 12:45:21 +0100

    feat: added user action timeline [catchAsync.js]

--- stx-tips-backendv5/utils/catchAsync.js
+++ stx-tips-backendv5/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: audit-logging pass-4: implemented tamper-proof log hashing
+// @backend: audit-logging pass-5: added user action timeline
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
