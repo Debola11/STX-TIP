63f5169457db6c10a6a385754c32e25f58a4a704
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 12:17:53 +0100

    feat: added S3 upload adapter [server.js]

--- stx-tips-backendv5/server.js
+++ stx-tips-backendv5/server.js
@@ -1,4 +1,4 @@
-// @backend: pagination-layer pass-8: implemented page size validation
+// @backend: file-storage pass-1: added S3 upload adapter
 import dotenv from "dotenv";
 import connectDB from "./utils/database.js";
 import app from "./app.js";
