777edb621ea3bfaf89706af3f353c2ccf009245e
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 10:02:18 +0100

    feat: implemented query profiler [server.js]

--- stx-tips-backendv4/server.js
+++ stx-tips-backendv4/server.js
@@ -1,4 +1,4 @@
-// @backend: database-layer pass-4: added data archival job
+// @backend: database-layer pass-5: implemented query profiler
 import dotenv from "dotenv";
 import connectDB from "./utils/database.js";
 import app from "./app.js";
