6395f2b5e86fae059df742aec3a95474217e4688
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 05:52:47 +0100

    feat: implemented route parameter transforms [catchAsync.js]

--- stx-tips-backendv3/utils/catchAsync.js
+++ stx-tips-backendv3/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: route-layer pass-2: added versioned API endpoints
+// @backend: route-layer pass-3: implemented route parameter transforms
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
