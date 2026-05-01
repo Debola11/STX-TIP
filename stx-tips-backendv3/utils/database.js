f09a10e58f0aa2b8a781cfb33e1db38656c3f34d
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 05:59:38 +0100

    feat: added unsubscribe link generation [database.js]

--- stx-tips-backendv3/utils/database.js
+++ stx-tips-backendv3/utils/database.js
@@ -1,4 +1,4 @@
-// @backend: email-system pass-3: added email delivery tracking
+// @backend: email-system pass-4: added unsubscribe link generation
 import mongoose from "mongoose";
 
 const connectDB = async (db) => {
