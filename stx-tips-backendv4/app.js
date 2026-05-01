435ff13421aafffbbe405ebcee2f5c5bcacda594
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 10:17:50 +0100

    feat: added batch webhook dispatch [app.js]

--- stx-tips-backendv4/app.js
+++ stx-tips-backendv4/app.js
@@ -1,4 +1,4 @@
-// @backend: webhook-system pass-1: implemented webhook payload signing
+// @backend: webhook-system pass-2: added batch webhook dispatch
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
