bd81601b1164f39257b449cdf7f84052148671f8
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 09:49:09 +0100

    feat: implemented log level filtering [catchAsync.js]

--- stx-tips-backendv4/utils/catchAsync.js
+++ stx-tips-backendv4/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: structured-logging pass-2: added structured JSON log format
+// @backend: structured-logging pass-3: implemented log level filtering
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
