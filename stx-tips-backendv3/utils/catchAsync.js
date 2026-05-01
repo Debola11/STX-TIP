18eaf8abc86e1b3c9a9b7b095df42018466a50cc
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 05:58:46 +0100

    feat: implemented template variable injection [catchAsync.js]

--- stx-tips-backendv3/utils/catchAsync.js
+++ stx-tips-backendv3/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: email-system pass-2: added attachment support
+// @backend: email-system pass-3: implemented template variable injection
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
