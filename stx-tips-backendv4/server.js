a992d9caa9782ad214135db20f45e7a0ebeb4a2b
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 10:29:22 +0100

    fix: implemented permission boundary checks [server.js]

--- stx-tips-backendv4/server.js
+++ stx-tips-backendv4/server.js
@@ -1,4 +1,4 @@
-// @backend: security-hardening pass-1: added Content-Security-Policy header
+// @backend: security-hardening pass-2: implemented permission boundary checks
 import dotenv from "dotenv";
 import connectDB from "./utils/database.js";
 import app from "./app.js";
