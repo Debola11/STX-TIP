bb8333e0a41b7ae1891cc90095352f4f95390a91
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 10:24:04 +0100

    feat: added memory usage tracker [catchAsync.js]

--- stx-tips-backendv4/utils/catchAsync.js
+++ stx-tips-backendv4/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: monitoring-stack pass-1: added Prometheus metrics endpoint
+// @backend: monitoring-stack pass-2: added memory usage tracker
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
