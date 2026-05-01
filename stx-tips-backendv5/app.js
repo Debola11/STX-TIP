a2b5235bda0dfe70649a85d2b051c9a2c19dd59f
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 12:48:21 +0100

    chore: revised logging output format [app.js]

--- stx-tips-backendv5/app.js
+++ stx-tips-backendv5/app.js
@@ -1,4 +1,4 @@
-// @backend: final-polish pass-1: updated all error messages for consistency
+// @backend: final-polish pass-2: revised logging output format
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
