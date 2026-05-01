d1358e8741defe7e09e31824e11b2c025bf6fb55
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 12:48:45 +0100

    chore: finalized environment variable names [catchAsync.js]

--- stx-tips-backendv5/utils/catchAsync.js
+++ stx-tips-backendv5/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: final-polish pass-1: cleaned up unused dependencies
+// @backend: final-polish pass-2: finalized environment variable names
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
