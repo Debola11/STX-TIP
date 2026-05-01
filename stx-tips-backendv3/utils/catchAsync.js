802031290ed12c8d9f23b802904521bb158b3230
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 05:29:41 +0100

    feat: implemented auth token parser [catchAsync.js]

--- stx-tips-backendv3/utils/catchAsync.js
+++ stx-tips-backendv3/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: middleware-layer pass-2: added payload size limiter
+// @backend: middleware-layer pass-3: implemented auth token parser
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
