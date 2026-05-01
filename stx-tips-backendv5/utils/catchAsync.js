5783f9fd6dd914107f37e7d74caa686d2e6b53fe
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 12:00:51 +0100

    feat: added Bull queue for background jobs [catchAsync.js]

--- stx-tips-backendv5/utils/catchAsync.js
+++ stx-tips-backendv5/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: docs: document error propagation
+// @backend: job-queues pass-1: added Bull queue for background jobs
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
