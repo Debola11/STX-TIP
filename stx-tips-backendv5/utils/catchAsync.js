eb46e3dfe29536b9072ab4e7098fe48412c6e87d
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 12:41:09 +0100

    feat: implemented data transformation step [catchAsync.js]

--- stx-tips-backendv5/utils/catchAsync.js
+++ stx-tips-backendv5/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: schema-migrations pass-6: implemented parallel migration batches
+// @backend: schema-migrations pass-7: implemented data transformation step
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
