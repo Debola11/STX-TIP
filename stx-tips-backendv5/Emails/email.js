55ce7083574ead2e6da5374b842faa4d7d85018f
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 12:39:39 +0100

    feat: added migration dry-run mode [email.js]

--- stx-tips-backendv5/Emails/email.js
+++ stx-tips-backendv5/Emails/email.js
@@ -1,4 +1,4 @@
-// @backend: schema-migrations pass-4: implemented parallel migration batches
+// @backend: schema-migrations pass-5: added migration dry-run mode
 import nodemailer from "nodemailer";
 import ejs from "ejs";
 import path from "path";
