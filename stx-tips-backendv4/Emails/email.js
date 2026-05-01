4a77b0cee80e5eaa7d3f34e656efaf22736248e4
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 10:22:26 +0100

    feat: implemented webhook secret rotation [email.js]

--- stx-tips-backendv4/Emails/email.js
+++ stx-tips-backendv4/Emails/email.js
@@ -1,4 +1,4 @@
-// @backend: webhook-system pass-6: added webhook registration endpoint
+// @backend: webhook-system pass-7: implemented webhook secret rotation
 import nodemailer from "nodemailer";
 import ejs from "ejs";
 import path from "path";
