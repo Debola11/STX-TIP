762195d4c9bd2a9ee16c1e00224313eff55b1c5f
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 09:52:26 +0100

    feat: implemented log rotation policy [catchAsync.js]

--- stx-tips-backendv4/utils/catchAsync.js
+++ stx-tips-backendv4/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: structured-logging pass-6: implemented performance metric logging
+// @backend: structured-logging pass-7: implemented log rotation policy
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
