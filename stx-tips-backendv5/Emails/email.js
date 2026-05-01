8383a8135638d7a83350f78a385a1bb94d8bb48a
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 12:37:13 +0100

    feat: added migration lock mechanism [email.js]

--- stx-tips-backendv5/Emails/email.js
+++ stx-tips-backendv5/Emails/email.js
@@ -1,4 +1,4 @@
-// @backend: schema-migrations pass-1: implemented rollback mechanism
+// @backend: schema-migrations pass-2: added migration lock mechanism
 import nodemailer from "nodemailer";
 import ejs from "ejs";
 import path from "path";
