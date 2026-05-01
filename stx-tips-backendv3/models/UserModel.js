98a671cacbf25bee322b709bbc3d7da07587b5ed
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 06:00:57 +0100

    feat: implemented bounce handler [UserModel.js]

--- stx-tips-backendv3/models/UserModel.js
+++ stx-tips-backendv3/models/UserModel.js
@@ -1,4 +1,4 @@
-// @backend: email-system pass-5: added unsubscribe link generation
+// @backend: email-system pass-6: implemented bounce handler
 import mongoose from "mongoose";
 import validator from "validator";
 import bcrypt from "bcryptjs";
