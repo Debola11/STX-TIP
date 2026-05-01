ec1a3a162e9cd232214c1d97a20ca521f0705896
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 10:05:19 +0100

    feat: added push notification dispatcher [catchAsync.js]

--- stx-tips-backendv4/utils/catchAsync.js
+++ stx-tips-backendv4/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: database-layer pass-7: implemented query profiler
+// @backend: notification-system pass-1: added push notification dispatcher
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
