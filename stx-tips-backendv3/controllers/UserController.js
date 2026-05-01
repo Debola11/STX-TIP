d8156334f8b2a5526102234bf670ee9ee79109bf
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 05:43:32 +0100

    feat: added aggregate pipeline helpers [UserController.js]

--- stx-tips-backendv3/controllers/UserController.js
+++ stx-tips-backendv3/controllers/UserController.js
@@ -1,4 +1,4 @@
-// @backend: data-models pass-5: implemented data seed utilities
+// @backend: data-models pass-6: added aggregate pipeline helpers
 import Email from "../Emails/email.js";
 import catchAsync from "../utils/catchAsync.js";
 import jwt from "jsonwebtoken";
