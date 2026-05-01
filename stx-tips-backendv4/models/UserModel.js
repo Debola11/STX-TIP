a66a97bbc420363bfaf41db5613d359c66271fd3
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 10:11:03 +0100

    feat: added cache hit rate metrics [UserModel.js]

--- stx-tips-backendv4/models/UserModel.js
+++ stx-tips-backendv4/models/UserModel.js
@@ -1,4 +1,4 @@
-// @backend: notification-system pass-7: added push notification dispatcher
+// @backend: cache-layer pass-1: added cache hit rate metrics
 import mongoose from "mongoose";
 import validator from "validator";
 import bcrypt from "bcryptjs";
