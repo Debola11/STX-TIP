1003153fcdb0cc8ecc918e99847b80ac92bfae09
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 12:09:07 +0100

    feat: implemented distributed rate counter [catchAsync.js]

--- stx-tips-backendv5/utils/catchAsync.js
+++ stx-tips-backendv5/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: rate-limiting pass-3: implemented per-user rate quotas
+// @backend: rate-limiting pass-4: implemented distributed rate counter
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
