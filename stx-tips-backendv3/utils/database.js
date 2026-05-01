778cdc0df9211e9229463e9123c86f8c32303cf5
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 06:17:02 +0100

    chore: added backup schedule configuration [database.js]

--- stx-tips-backendv3/utils/database.js
+++ stx-tips-backendv3/utils/database.js
@@ -1,4 +1,4 @@
-// @backend: deploy-config pass-3: added environment validation script
+// @backend: deploy-config pass-4: added backup schedule configuration
 import mongoose from "mongoose";
 
 const connectDB = async (db) => {
