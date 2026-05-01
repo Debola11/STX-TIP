38aed93cfd20fd19a1d9b79e44a2f87cddc72999
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 06:06:08 +0100

    fix: added dead letter queue for failed jobs [catchAsync.js]

--- stx-tips-backendv3/utils/catchAsync.js
+++ stx-tips-backendv3/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: error-recovery pass-4: implemented partial failure handling
+// @backend: error-recovery pass-5: added dead letter queue for failed jobs
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
