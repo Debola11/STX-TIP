ca66b977f92775140d131b6ad0e4e7a4218c94ee
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 12:47:00 +0100

    feat: implemented compliance report builder [catchAsync.js]

--- stx-tips-backendv5/utils/catchAsync.js
+++ stx-tips-backendv5/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: audit-logging pass-6: implemented audit log export to CSV
+// @backend: audit-logging pass-7: implemented compliance report builder
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
