7ee1194a6e8bc7c68ab3f852a8c06a2c077765de
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 06:18:10 +0100

    chore: added Docker build configuration [app.js]

--- stx-tips-backendv3/app.js
+++ stx-tips-backendv3/app.js
@@ -1,4 +1,4 @@
-// @backend: deploy-config pass-5: added monitoring metric exports
+// @backend: deploy-config pass-6: added Docker build configuration
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
