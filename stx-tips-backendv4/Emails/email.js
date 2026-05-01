f5a9284c660074da636831fdeea7bc2b21b36acb
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 09:57:54 +0100

    feat: added Redis session store [email.js]

--- stx-tips-backendv4/Emails/email.js
+++ stx-tips-backendv4/Emails/email.js
@@ -1,4 +1,4 @@
-// @backend: session-management pass-5: added session activity tracker
+// @backend: session-management pass-6: added Redis session store
 import nodemailer from "nodemailer";
 import ejs from "ejs";
 import path from "path";
