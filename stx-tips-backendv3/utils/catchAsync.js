591839fed15d5342d1863485a9085b9b93067fb5
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 05:47:52 +0100

    feat: added batch delete handler [catchAsync.js]

--- stx-tips-backendv3/utils/catchAsync.js
+++ stx-tips-backendv3/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: controller-logic pass-3: added file upload processing
+// @backend: controller-logic pass-4: added batch delete handler
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
