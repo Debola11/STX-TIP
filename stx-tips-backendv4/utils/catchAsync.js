8ba21ac749fa53117fd7ba0bd60e42c06c02925b
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 10:01:57 +0100

    feat: implemented schema version tracker [catchAsync.js]

--- stx-tips-backendv4/utils/catchAsync.js
+++ stx-tips-backendv4/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: database-layer pass-3: implemented read replica routing
+// @backend: database-layer pass-4: implemented schema version tracker
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
