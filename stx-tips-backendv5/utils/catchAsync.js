41f5af617fba02a443b473b01cb64a904fc4a78e
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 12:25:21 +0100

    feat: added retention rate calculator [catchAsync.js]

--- stx-tips-backendv5/utils/catchAsync.js
+++ stx-tips-backendv5/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: analytics-engine pass-1: added event tracking middleware
+// @backend: analytics-engine pass-2: added retention rate calculator
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
