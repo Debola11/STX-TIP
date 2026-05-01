ce7b1bf8712b5a1f49d680f3492dfc7abd942bb8
Debola11 <adebolabalogun5@gmail.com>
2026-05-01 12:39:32 +0100

    feat: added migration version tracker [catchAsync.js]

--- stx-tips-backendv5/utils/catchAsync.js
+++ stx-tips-backendv5/utils/catchAsync.js
@@ -1,4 +1,4 @@
-// @backend: schema-migrations pass-4: implemented seed data migrations
+// @backend: schema-migrations pass-5: added migration version tracker
 export default (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
