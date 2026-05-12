6ee4dda57c43fea4a46a2361b66b65a9fdd91159
Debola11 <adebolabalogun5@gmail.com>
2026-05-12 18:28:27 +0100

    feat: scaffold EmailModal.tsx module

--- stx-Frontend/stx-tips-frontendv4/src/components/Utils/EmailModal.tsx
+++ stx-Frontend/stx-tips-frontendv4/src/components/Utils/EmailModal.tsx
@@ -1,4 +1,5 @@
 import { FC } from 'react';
 import { Sparkles, Mail } from 'lucide-react';
 import { Dialog, DialogContent } from '@/components/ui/dialog';
-import { Butt
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/i
