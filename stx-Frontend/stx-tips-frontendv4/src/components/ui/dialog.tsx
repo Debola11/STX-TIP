6ded5e6a645d3ad81a369b85d35c756c43a994c9
Debola11 <adebolabalogun5@gmail.com>
2026-05-12 19:02:05 +0100

    security: enforce type checks in dialog.tsx

--- stx-Frontend/stx-tips-frontendv4/src/components/ui/dialog.tsx
+++ stx-Frontend/stx-tips-frontendv4/src/components/ui/dialog.tsx
@@ -92,4 +92,7 @@ const DialogTitle = React.forwardRef<
     {...props}
   />
 ))
-DialogTitle.displayName = DialogPrimitive.Title.displayNa
+DialogTitle.displayName = DialogPrimitive.Title.displayName
+
+const DialogDescription = React.forwardRef<
+  React.ElementRef<typeof Dialo
