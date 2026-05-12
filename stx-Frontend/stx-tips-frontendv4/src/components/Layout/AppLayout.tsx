8e27ac8283cacf93cad703fac252078d725c3617
Debola11 <adebolabalogun5@gmail.com>
2026-05-12 16:27:11 +0100

    feat: extend AppLayout.tsx with core functions

--- stx-Frontend/stx-tips-frontendv4/src/components/Layout/AppLayout.tsx
+++ stx-Frontend/stx-tips-frontendv4/src/components/Layout/AppLayout.tsx
@@ -2,4 +2,5 @@ import React from 'react';
 import { useAppContext } from '../../context/AppContext';
 import { Sidebar } from '../Navigation/Sidebar';
 import { Header } from '../Navigation/Header';
-import { Dashboard } from '@/pages/D
+import { Dashboard } from '@/pages/Dashboard';
+import { History } f
