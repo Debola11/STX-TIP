168d251122d4ba5c39d1cbec7d86fe7b530f5ad8
Debola11 <adebolabalogun5@gmail.com>
2026-05-12 18:58:34 +0100

    docs: add usage example for card.tsx

--- stx-Frontend/stx-tips-frontendv4/src/components/ui/card.tsx
+++ stx-Frontend/stx-tips-frontendv4/src/components/ui/card.tsx
@@ -57,4 +57,4 @@ const CardContent = React.forwardRef<
   HTMLDivElement,
   React.HTMLAttributes<HTMLDivElement>
 >(({ className, ...props }, ref) => (
-  <div ref
+  <div ref={ref} className={cn("p-6 pt-0", clas
