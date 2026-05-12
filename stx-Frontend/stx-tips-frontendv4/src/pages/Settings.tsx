154e2f28e64d81329908e114b35e596781efdbd9
Debola11 <adebolabalogun5@gmail.com>
2026-05-12 19:52:21 +0100

    feat: add base structure to Settings.tsx

--- stx-Frontend/stx-tips-frontendv4/src/pages/Settings.tsx
+++ stx-Frontend/stx-tips-frontendv4/src/pages/Settings.tsx
@@ -2,4 +2,15 @@ import React, { useState } from 'react';
 import { Bell, Shield, Wallet, Globe } from 'lucide-react';
 
 export const Settings: React.FC = () => {
-  const [notifications, setNoti
+  const [notifications, setNotifications] = useState({
+    tips: true,
+    milestones: true,
+    news: false
+  });
+
+  const [privacy, setPrivacy] = useState({
+    public: true,
+    showAmount: true
+  });
+
+ 
