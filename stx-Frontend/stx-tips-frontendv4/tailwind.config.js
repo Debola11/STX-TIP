ad611785d82a5417f3c95bb9c6354de6ab73d223
Debola11 <adebolabalogun5@gmail.com>
2026-05-12 15:45:56 +0100

    security: restrict exports in tailwind.config.js

--- stx-Frontend/stx-tips-frontendv4/tailwind.config.js
+++ stx-Frontend/stx-tips-frontendv4/tailwind.config.js
@@ -45,4 +45,5 @@ export default {
   			input: 'hsl(var(--input))',
   			ring: 'hsl(var(--ring))',
   			chart: {
-  				'1': 'hsl(var(--chart-1)
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart
