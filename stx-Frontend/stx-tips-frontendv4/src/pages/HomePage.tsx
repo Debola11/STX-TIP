6310c988a80636f8f2a1bb9f5e9177af396e48bd
Debola11 <adebolabalogun5@gmail.com>
2026-05-12 19:46:54 +0100

    fix: patch edge case in HomePage.tsx

--- stx-Frontend/stx-tips-frontendv4/src/pages/HomePage.tsx
+++ stx-Frontend/stx-tips-frontendv4/src/pages/HomePage.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
 import { HeroSection } from '@/components/Landing/HeroSection';
 import { StatsSection } from '@/components/Landing/StatsSection';
-// import { TippingSection } from '@/components/Landing/Tipping
+// import { TippingSection } from '@/components/Landing/TippingSection';
+import { H
