f8e99530695660ed8d731bfc37c946ee8e19879b
Debola11 <adebolabalogun5@gmail.com>
2026-05-12 18:25:33 +0100

    feat: wire up dependencies in Chart.tsx

--- stx-Frontend/stx-tips-frontendv4/src/components/Utils/Chart.tsx
+++ stx-Frontend/stx-tips-frontendv4/src/components/Utils/Chart.tsx
@@ -1,3 +1,3 @@
 import { ChartData } from '@/lib/type';
 import React from 'react';
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tool
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContai
