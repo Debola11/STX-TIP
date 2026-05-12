ea8a379b6e06ee1eae3a182d7787292735a7a7ed
Debola11 <adebolabalogun5@gmail.com>
2026-05-12 19:36:15 +0100

    style: update colour tokens in type.ts

--- stx-Frontend/stx-tips-frontendv4/src/lib/type.ts
+++ stx-Frontend/stx-tips-frontendv4/src/lib/type.ts
@@ -48,4 +48,4 @@ export interface UserStats {
 export interface RawUserStats {
   'total-tips-sent': { value: bigint };
   'total-tips-received': { value: bigint };
-  'rewar
+  'reward-points': { value: bigint
