87e9f784875e199189e3db1bf96036aa3a790d57
Debola11 <adebolabalogun5@gmail.com>
2026-05-12 18:55:04 +0100

    perf: cache repeated lookups in button.tsx

--- stx-Frontend/stx-tips-frontendv4/src/components/ui/button.tsx
+++ stx-Frontend/stx-tips-frontendv4/src/components/ui/button.tsx
@@ -21,4 +21,5 @@ const buttonVariants = cva(
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
-        default
+        default: "h-9 px-4 py-2",
+        sm: "h-8 rou
