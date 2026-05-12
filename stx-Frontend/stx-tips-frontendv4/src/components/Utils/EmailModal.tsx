e82ae3671b59dcaab01eca2281e84f6e8343a55a
Debola11 <adebolabalogun5@gmail.com>
2026-05-12 18:28:30 +0100

    feat: define core exports in EmailModal.tsx

--- stx-Frontend/stx-tips-frontendv4/src/components/Utils/EmailModal.tsx
+++ stx-Frontend/stx-tips-frontendv4/src/components/Utils/EmailModal.tsx
@@ -2,4 +2,8 @@ import { FC } from 'react';
 import { Sparkles, Mail } from 'lucide-react';
 import { Dialog, DialogContent } from '@/components/ui/dialog';
 import { Button } from '@/components/ui/button';
-import { Input } from '@/components/ui/i
+import { Input } from '@/components/ui/input';
+
+interface EmailModalProps {
+  isOpen: boolean;
+  onOpenChange: (open
