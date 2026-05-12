eadb851a1d7927f71b11b029f0515cbbf828a164
Debola11 <adebolabalogun5@gmail.com>
2026-05-12 16:27:01 +0100

    feat: add primary logic to AppLayout.tsx

--- stx-Frontend/stx-tips-frontendv4/src/components/Layout/AppLayout.tsx
+++ stx-Frontend/stx-tips-frontendv4/src/components/Layout/AppLayout.tsx
@@ -1,3 +1,4 @@
 import React from 'react';
 import { useAppContext } from '../../context/AppContext';
-import { Sidebar } from '../Navigation/
+import { Sidebar } from '../Navigation/Sidebar';
+import { Header } fro
