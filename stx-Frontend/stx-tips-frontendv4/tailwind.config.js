0f6ed9f26e863b4d6c0e5f153329a94a52336289
Debola11 <adebolabalogun5@gmail.com>
2026-05-12 15:45:42 +0100

    docs: annotate side effects in tailwind.config.js

--- stx-Frontend/stx-tips-frontendv4/tailwind.config.js
+++ stx-Frontend/stx-tips-frontendv4/tailwind.config.js
@@ -41,4 +41,5 @@ export default {
   				DEFAULT: 'hsl(var(--destructive))',
   				foreground: 'hsl(var(--destructive-foreground))'
   			},
-  			border: 'hsl(v
+  			border: 'hsl(var(--border))',
+  			input: 'hs
