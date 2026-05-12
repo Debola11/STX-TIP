88ce947ad69c715a13c87f303997b8c455aed0af
Debola11 <adebolabalogun5@gmail.com>
2026-05-12 19:11:49 +0100

    feat: scaffold AuthContext.tsx module

--- stx-Frontend/stx-tips-frontendv4/src/context/AuthContext.tsx
+++ stx-Frontend/stx-tips-frontendv4/src/context/AuthContext.tsx
@@ -1,4 +1,5 @@
 // import { createContext, useEffect, useReducer } from "react";
 
 // const initialState = {
-//   user: localStorage.getItem("user") ? JSON.parse(local
+//   user: localStorage.getItem("user") ? JSON.parse(localStorage.getItem("user") as string) : null,
+//   token: localStorage.getItem
