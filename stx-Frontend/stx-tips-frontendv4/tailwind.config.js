d61f7ee9d59f82de4ca941efd0d491bcc4db74e0
Debola11 <adebolabalogun5@gmail.com>
2026-05-12 15:46:11 +0100

    test: add edge case coverage for tailwind.config.js

--- stx-Frontend/stx-tips-frontendv4/tailwind.config.js
+++ stx-Frontend/stx-tips-frontendv4/tailwind.config.js
@@ -49,4 +49,7 @@ export default {
   				'2': 'hsl(var(--chart-2))',
   				'3': 'hsl(var(--chart-3))',
   				'4': 'hsl(var(--chart-4))',
-  				'5': 'hsl(va
+  				'5': 'hsl(var(--chart-5))'
+  			}
+  		}
+  	
