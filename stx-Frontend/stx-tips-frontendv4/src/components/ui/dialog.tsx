fa502f7ead50a5deeb2cb2984924499c10e0f691
Debola11 <adebolabalogun5@gmail.com>
2026-05-12 19:01:24 +0100

    style: apply Tailwind classes to dialog.tsx

--- stx-Frontend/stx-tips-frontendv4/src/components/ui/dialog.tsx
+++ stx-Frontend/stx-tips-frontendv4/src/components/ui/dialog.tsx
@@ -54,4 +54,7 @@ DialogContent.displayName = DialogPrimitive.Content.displayName
 const DialogHeader = ({
   className,
   ...props
-}: React.HTMLAttributes<HTMLDivElement>
+}: React.HTMLAttributes<HTMLDivElement>) => (
+  <div
+    className={cn(
+      "flex flex-col space-y-1.5 text-center s
