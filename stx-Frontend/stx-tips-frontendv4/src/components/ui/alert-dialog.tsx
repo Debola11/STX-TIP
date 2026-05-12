832124cd15f43981da9b3400a16964f321c6664a
Debola11 <adebolabalogun5@gmail.com>
2026-05-12 18:47:08 +0100

    feat: scaffold alert-dialog.tsx module

--- stx-Frontend/stx-tips-frontendv4/src/components/ui/alert-dialog.tsx
+++ stx-Frontend/stx-tips-frontendv4/src/components/ui/alert-dialog.tsx
@@ -2,4 +2,8 @@ import * as React from "react"
 import * as AlertDialogPrimitive from "@radix-ui/react-alert-dialog"
 
 import { cn } from "@/lib/utils"
-import { buttonVariants } from "@/components
+import { buttonVariants } from "@/components/ui/button"
+
+const AlertDialog = AlertDialogPrimitive.Root
+
+const AlertDialogTrigger = Al
