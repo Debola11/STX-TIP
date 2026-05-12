11c7b08aa0fcf4a58bef2ed0c2a18446060dfc5f
Debola11 <adebolabalogun5@gmail.com>
2026-05-12 19:44:14 +0100

    chore: add type annotations to History.tsx

--- stx-Frontend/stx-tips-frontendv4/src/pages/History.tsx
+++ stx-Frontend/stx-tips-frontendv4/src/pages/History.tsx
@@ -23,4 +23,5 @@ export const History: React.FC = () => {
           <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 h-5 w-5" />
           <input
             type="text"
-            placeholder="
+            placeholder="Search transactions..."
+            className="w-full pl-10 
