c398abc15730ba96ad9ff2fc94afd14be23598e2
Debola11 <adebolabalogun5@gmail.com>
2026-05-12 18:54:28 +0100

    refactor: improve readability of button.tsx

--- stx-Frontend/stx-tips-frontendv4/src/components/ui/button.tsx
+++ stx-Frontend/stx-tips-frontendv4/src/components/ui/button.tsx
@@ -12,4 +12,6 @@ const buttonVariants = cva(
         default:
           "bg-primary text-primary-foreground shadow hover:bg-primary/90",
         destructive:
-          "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90"
+          "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
+        outline:
+          "border bo
