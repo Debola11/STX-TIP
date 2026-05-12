9bf02c5c2f932707996b9f31300f990ce6df1b70
Debola11 <adebolabalogun5@gmail.com>
2026-05-12 19:15:12 +0100

    feat: wire up dependencies in useTipTransaction.ts

--- stx-Frontend/stx-tips-frontendv4/src/hooks/useTipTransaction.ts
+++ stx-Frontend/stx-tips-frontendv4/src/hooks/useTipTransaction.ts
@@ -2,4 +2,5 @@ import { useState } from 'react';
 import { useConnect } from '@stacks/connect-react';
 import { AnchorMode, PostConditionMode } from '@stacks/transactions';
 import { NETWORK, CONTRACT_ADDRESS, CONTRACT_NAME } from '@/config/stacks';
-import { prepareTipArgs } from '../components/Util
+import { prepareTipArgs } from '../components/Utils/stackUtils';
+import { notifyTipSent } from '.
