af6386b52b0347f13684fd3893ef7b5ff8663e7e
Debola11 <adebolabalogun5@gmail.com>
2026-05-13 16:58:09 +0100

    test: cover main path in tailwind.config.js

--- stx-Frontend/stx-tips-frontendv5/tailwind.config.js
+++ stx-Frontend/stx-tips-frontendv5/tailwind.config.js
@@ -47,4 +47,5 @@ export default {
   				'1': 'hsl(var(--chart-1))',
   				'2': 'hsl(var(--chart-2))',
   				'3': 'hsl(var(--chart-3))',
-  				'4': 'hsl(var(-
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(va
