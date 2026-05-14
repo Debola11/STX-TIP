49e60b795fe9d9261842172f7287731185f845b8
Debola11 <adebolabalogun5@gmail.com>
2026-05-14 06:02:13 +0100

    perf: lazy-load dependencies in dialog.tsx

--- stx-Frontend/stx-tips-frontendv5/src/components/ui/dialog.tsx
+++ stx-Frontend/stx-tips-frontendv5/src/components/ui/dialog.tsx
@@ -44,4 +44,7 @@ const DialogContent = React.forwardRef<
       {children}
       <DialogPrimitive.Close className="absolute right-4 top-4 rounded-full opacity-70 bg-slate-50 font-bold text-violet-600 p-1 border border-violet-600 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground">
         <X className="h-4 w-4" />
-        <span className="sr-only">Close</span
+        <span className="sr-only">Close</span>
+      </DialogPrimitive.Close>
+    </DialogPrimitive.Content>
+  </DialogPorta
