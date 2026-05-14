abc66cdf62adbd471925cd56a7f840dc14805c38
Debola11 <adebolabalogun5@gmail.com>
2026-05-14 12:13:29 +0100

    docs: annotate side effects in HomePage.tsx

--- stx-Frontend/stx-tips-frontendv5/src/pages/HomePage.tsx
+++ stx-Frontend/stx-tips-frontendv5/src/pages/HomePage.tsx
@@ -16,4 +16,5 @@ export const HomePage: React.FC = () => {
       <HeroSection />
       <StatsSection />
 
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16 spa
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16 space-y-16">
+        <T
