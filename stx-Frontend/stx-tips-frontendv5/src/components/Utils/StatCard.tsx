3b636825a3f8a45105c014a81e57d3b63ad74af3
Debola11 <adebolabalogun5@gmail.com>
2026-05-14 05:36:03 +0100

    chore: add type annotations to StatCard.tsx

--- stx-Frontend/stx-tips-frontendv5/src/components/Utils/StatCard.tsx
+++ stx-Frontend/stx-tips-frontendv5/src/components/Utils/StatCard.tsx
@@ -3,4 +3,4 @@ import React from 'react';
 
 export const StatCard: React.FC<Stat> = ({ title, value, change, icon }) => (
   <div className="bg-white rounded-xl shadow-sm p-6 hover:shadow-md transition-shadow">
-    <div className="flex items-
+    <div className="flex items-center justif
