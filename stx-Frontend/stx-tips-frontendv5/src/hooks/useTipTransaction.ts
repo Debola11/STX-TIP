223932f011b3c6efba91872ebe8aee401da701b0
Debola11 <adebolabalogun5@gmail.com>
2026-05-14 06:15:00 +0100

    feat: add primary logic to useTipTransaction.ts

--- stx-Frontend/stx-tips-frontendv5/src/hooks/useTipTransaction.ts
+++ stx-Frontend/stx-tips-frontendv5/src/hooks/useTipTransaction.ts
@@ -1,4 +1,5 @@
 import { useState } from 'react';
 import { useConnect } from '@stacks/connect-react';
 import { AnchorMode, PostConditionMode } from '@stacks/transactions';
-import { NETWORK, CONTRACT_ADDRE
+import { NETWORK, CONTRACT_ADDRESS, CONTRACT_NAME } from '@/config/stacks';
+imp
