583fbade384d6aeb678b9406048b4efab33eb02e
Debola11 <adebolabalogun5@gmail.com>
2026-05-14 05:34:34 +0100

    security: harden error handling in EmailModal.tsx

--- stx-Frontend/stx-tips-frontendv5/src/components/Utils/EmailModal.tsx
+++ stx-Frontend/stx-tips-frontendv5/src/components/Utils/EmailModal.tsx
@@ -70,4 +70,5 @@ const EmailModal: FC<EmailModalProps> = ({
                 </div>
               ) : (
                 <div className="flex items-center justify-center space-x-2">
-     
+                  <Sparkles className="w-5 h-5" />
+                  <span>Comple
