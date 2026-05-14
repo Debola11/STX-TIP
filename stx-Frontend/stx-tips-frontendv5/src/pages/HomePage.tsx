a7a7708a8c3f730905c0be8c039cdaf4f674a07d
Debola11 <adebolabalogun5@gmail.com>
2026-05-14 12:11:36 +0100

    feat: extend HomePage.tsx with core functions

--- stx-Frontend/stx-tips-frontendv5/src/pages/HomePage.tsx
+++ stx-Frontend/stx-tips-frontendv5/src/pages/HomePage.tsx
@@ -1,3 +1,4 @@
 import React from 'react';
 import { HeroSection } from '@/components/Landing/HeroSection';
-import { StatsSection } from '@/components/Landin
+import { StatsSection } from '@/components/Landing/StatsSection';
+// 
