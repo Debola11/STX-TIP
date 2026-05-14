e68c70f753d884983d2fd1adbaa7dc843eb75570
Debola11 <adebolabalogun5@gmail.com>
2026-05-14 05:59:54 +0100

    docs: clarify return types in card.tsx

--- stx-Frontend/stx-tips-frontendv5/src/components/ui/card.tsx
+++ stx-Frontend/stx-tips-frontendv5/src/components/ui/card.tsx
@@ -57,4 +57,6 @@ const CardContent = React.forwardRef<
   HTMLDivElement,
   React.HTMLAttributes<HTMLDivElement>
 >(({ className, ...props }, ref) => (
-  <div ref={ref} className={cn("p-6 pt-0", clas
+  <div ref={ref} className={cn("p-6 pt-0", className)} {...props} />
+))
+CardContent.
