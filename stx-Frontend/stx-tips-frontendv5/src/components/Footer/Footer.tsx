c924138464f4285bbc88fd37aaa915785391e0ca
Debola11 <adebolabalogun5@gmail.com>
2026-05-14 04:45:31 +0100

    feat: initialise Footer.tsx

---
+++ stx-Frontend/stx-tips-frontendv5/src/components/Footer/Footer.tsx
@@ -0,0 +1,6 @@
+// import React from 'react';
+// import { ExternalLink, Github } from 'lucide-react';
+
+// export const Footer: React.FC = () => {
+//   return (
+//     <footer className="bg-gray-900 text-white 
