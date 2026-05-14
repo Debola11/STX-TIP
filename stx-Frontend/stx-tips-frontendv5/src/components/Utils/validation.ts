a65407cd38c702757f8f88f04fb8264b89c57abd
Debola11 <adebolabalogun5@gmail.com>
2026-05-14 05:48:17 +0100

    style: add responsive breakpoints to validation.ts

--- stx-Frontend/stx-tips-frontendv5/src/components/Utils/validation.ts
+++ stx-Frontend/stx-tips-frontendv5/src/components/Utils/validation.ts
@@ -1,2 +1,2 @@
 export const validateStacksAddress = (address: string): boolean => {
-    return address.startsWith('ST') || address.startsWith('
+    return address.startsWith('ST') || address.startsWith('SP')
