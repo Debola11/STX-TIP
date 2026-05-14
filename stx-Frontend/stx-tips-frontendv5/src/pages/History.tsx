282ce2c2036383e3b6282d9a23b1073c04aa24fd
Debola11 <adebolabalogun5@gmail.com>
2026-05-14 12:09:36 +0100

    chore: format History.tsx

--- stx-Frontend/stx-tips-frontendv5/src/pages/History.tsx
+++ stx-Frontend/stx-tips-frontendv5/src/pages/History.tsx
@@ -27,4 +27,7 @@ export const History: React.FC = () => {
             className="w-full pl-10 pr-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-violet-600"
             value={searchTerm}
             onChange={(e) => setSearchTerm(e.target.value)}
-      
+          />
+        </div>
+        <div className="relative">
+   
