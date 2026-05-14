092817ddf6b689405598f4470edc9aeb3e439230
Debola11 <adebolabalogun5@gmail.com>
2026-05-14 12:12:04 +0100

    refactor: clean up imports in HomePage.tsx

--- stx-Frontend/stx-tips-frontendv5/src/pages/HomePage.tsx
+++ stx-Frontend/stx-tips-frontendv5/src/pages/HomePage.tsx
@@ -2,4 +2,5 @@ import React from 'react';
 import { HeroSection } from '@/components/Landing/HeroSection';
 import { StatsSection } from '@/components/Landing/StatsSection';
 // import { TippingSection } from '@/components/Landing/TippingSection';
-import { HowItWorksSection } from '@/components/Landing/HowItWorksSect
+import { HowItWorksSection } from '@/components/Landing/HowItWorksSection';
+import { FAQSe
