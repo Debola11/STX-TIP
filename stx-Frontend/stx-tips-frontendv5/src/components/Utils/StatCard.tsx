ba206ea9f05dafbf27f15a1897915037d6836686
Debola11 <adebolabalogun5@gmail.com>
2026-05-14 05:35:53 +0100

    refactor: simplify logic in StatCard.tsx

--- stx-Frontend/stx-tips-frontendv5/src/components/Utils/StatCard.tsx
+++ stx-Frontend/stx-tips-frontendv5/src/components/Utils/StatCard.tsx
@@ -2,4 +2,5 @@ import { Stat } from '@/lib/type';
 import React from 'react';
 
 export const StatCard: React.FC<Stat> = ({ title, value, change, icon }) => (
-  <div className="bg-white rounded-xl shadow-sm p-6 hover:shadow-md transition-s
+  <div className="bg-white rounded-xl shadow-sm p-6 hover:shadow-md transition-shadow">
+    <
