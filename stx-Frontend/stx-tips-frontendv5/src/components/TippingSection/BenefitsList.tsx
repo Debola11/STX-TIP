4c34d7e3750a74b929e3eb8d4e30fc5d436a5cc8
Debola11 <adebolabalogun5@gmail.com>
2026-05-14 05:13:38 +0100

    style: update layout in BenefitsList.tsx

--- stx-Frontend/stx-tips-frontendv5/src/components/TippingSection/BenefitsList.tsx
+++ stx-Frontend/stx-tips-frontendv5/src/components/TippingSection/BenefitsList.tsx
@@ -17,4 +17,5 @@ export const BenefitsList: React.FC = () => {
       variants={itemVariants}
     >
       <h3 className="font-medium flex items-center gap-2 text-lg mb-6">
-        <Gift className="h-5 w-5 text-violet-6
+        <Gift className="h-5 w-5 text-violet-600" />
+        <span>Why 
