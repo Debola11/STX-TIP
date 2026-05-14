b2dfb8d9a520a42f53d48e2733728f508b59b17b
Debola11 <adebolabalogun5@gmail.com>
2026-05-14 06:02:27 +0100

    style: add responsive breakpoints to dialog.tsx

--- stx-Frontend/stx-tips-frontendv5/src/components/ui/dialog.tsx
+++ stx-Frontend/stx-tips-frontendv5/src/components/ui/dialog.tsx
@@ -57,4 +57,10 @@ const DialogHeader = ({
 }: React.HTMLAttributes<HTMLDivElement>) => (
   <div
     className={cn(
-      "flex flex-col space-y-1.5 text-center s
+      "flex flex-col space-y-1.5 text-center sm:text-left",
+      className
+    )}
+    {...props}
+  />
+)
+DialogHeader.display
