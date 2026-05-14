e47bef08b210fd3f7d2b43a06e15f5019b3f65ae
Debola11 <adebolabalogun5@gmail.com>
2026-05-14 12:17:28 +0100

    feat: flesh out Settings.tsx implementation

--- stx-Frontend/stx-tips-frontendv5/src/pages/Settings.tsx
+++ stx-Frontend/stx-tips-frontendv5/src/pages/Settings.tsx
@@ -36,4 +36,4 @@ export const Settings: React.FC = () => {
                   onChange={(e) => setNotifications({ ...notifications, tips: e.target.checked })}
                   className="sr-only peer"
                 />
-          
+                <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-violet-300 rounded-full peer peer-checked:after:translate-x-full peer-chec
