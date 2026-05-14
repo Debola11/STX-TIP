2515d5c17945842f389a83e5995227fc633c373a
Debola11 <adebolabalogun5@gmail.com>
2026-05-14 05:31:01 +0100

    style: align spacing in Chart.tsx

--- stx-Frontend/stx-tips-frontendv5/src/components/Utils/Chart.tsx
+++ stx-Frontend/stx-tips-frontendv5/src/components/Utils/Chart.tsx
@@ -18,4 +18,4 @@ export const Chart: React.FC<ChartProps> = ({ data }) => (
             background: 'white', 
             border: 'none', 
             borderRadius: '8px', 
-            boxShadow: '0
+            boxShadow: '0 4px 6px -1px rgb(0 0
