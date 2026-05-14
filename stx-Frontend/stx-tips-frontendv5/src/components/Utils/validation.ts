928a5e012f533ca59e10c0feaba2c766bd7d1b95
Debola11 <adebolabalogun5@gmail.com>
2026-05-14 05:48:38 +0100

    docs: clarify return types in validation.ts

--- stx-Frontend/stx-tips-frontendv5/src/components/Utils/validation.ts
+++ stx-Frontend/stx-tips-frontendv5/src/components/Utils/validation.ts
@@ -1,2 +1,2 @@
 export const validateStacksAddress = (address: string): boolean => {
-    return address.startsWith('ST') || address.startsWith('SP') && address.length >
+    return address.startsWith('ST') || address.startsWith('SP') && address.length >= 41
