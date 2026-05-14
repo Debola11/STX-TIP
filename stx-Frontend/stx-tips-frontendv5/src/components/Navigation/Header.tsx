a58595b715d00eb9b7ff3dde7549926aec3ab7c2
Debola11 <adebolabalogun5@gmail.com>
2026-05-14 05:06:38 +0100

    fix: patch edge case in Header.tsx

--- stx-Frontend/stx-tips-frontendv5/src/components/Navigation/Header.tsx
+++ stx-Frontend/stx-tips-frontendv5/src/components/Navigation/Header.tsx
@@ -48,3 +48,13 @@ export const Header = () => {
           setWalletConnected(true);
           await checkUserExists(userData?.profile?.stxAddress?.testnet);
         }
+      };
+  
+      checkAuth();
+    }, []);
+
+    const checkUserExists = async (walletAddress: string | null) =>
+      {
+        if (!walletAddress) return;
+    
+        try
