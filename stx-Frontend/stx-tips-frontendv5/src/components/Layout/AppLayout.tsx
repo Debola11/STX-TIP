58c2cc029ad15234e739a12ca7abe226d59dca94
Debola11 <adebolabalogun5@gmail.com>
2026-05-14 05:04:17 +0100

    chore: update constants in AppLayout.tsx

--- stx-Frontend/stx-tips-frontendv5/src/components/Layout/AppLayout.tsx
+++ stx-Frontend/stx-tips-frontendv5/src/components/Layout/AppLayout.tsx
@@ -16,4 +16,5 @@ export const AppLayout: React.FC = () =>
         return (
             <div className="min-h-screen bg-gray-50">
                 <Header />
-                <main cla
+                <main className="pt-16">
+               
