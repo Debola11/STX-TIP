f26c884cfe51e0ab03ab4f5bc9ec9f52ed58954e
Debola11 <adebolabalogun5@gmail.com>
2026-05-14 05:38:55 +0100

    chore: update constants in TransactionCard.tsx

--- stx-Frontend/stx-tips-frontendv5/src/components/Utils/TransactionCard.tsx
+++ stx-Frontend/stx-tips-frontendv5/src/components/Utils/TransactionCard.tsx
@@ -8,4 +8,4 @@ interface TransactionCardProps {
 export const TransactionCard: React.FC<TransactionCardProps> = ({ transaction }) => (
   <div className="flex items-center justify-between py-3 border-b last:border-0 hover:bg-gray-50 transition-colors">
     <div className="flex items-center space-x-4">
-      <div className="w-10 h-10 r
+      <div className="w-10 h-10 rounded-full bg-gradi
