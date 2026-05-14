218057216d0cd889dde43d2bf72cb8f2abc431bf
Debola11 <adebolabalogun5@gmail.com>
2026-05-14 05:39:45 +0100

    docs: add JSDoc to TransactionCard.tsx

--- stx-Frontend/stx-tips-frontendv5/src/components/Utils/TransactionCard.tsx
+++ stx-Frontend/stx-tips-frontendv5/src/components/Utils/TransactionCard.tsx
@@ -13,4 +13,5 @@ export const TransactionCard: React.FC<TransactionCardProps> = ({ transaction })
       </div>
       <div>
         <p className="font-medium">
-          {transaction.sender.slice(0, 6)}...{transaction.sender.s
+          {transaction.sender.slice(0, 6)}...{transaction.sender.slice(-4)}
+        </
