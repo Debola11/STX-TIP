be2b2eb3df6cc94e7af7b4a0d726177c5198b56c
Debola11 <adebolabalogun5@gmail.com>
2026-05-14 05:39:37 +0100

    style: align spacing in TransactionCard.tsx

--- stx-Frontend/stx-tips-frontendv5/src/components/Utils/TransactionCard.tsx
+++ stx-Frontend/stx-tips-frontendv5/src/components/Utils/TransactionCard.tsx
@@ -13,4 +13,4 @@ export const TransactionCard: React.FC<TransactionCardProps> = ({ transaction })
       </div>
       <div>
         <p className="font-medium">
-          {transaction.sen
+          {transaction.sender.slice(0, 6)}...{
