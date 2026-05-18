ac06714a74c9f2482190e5c1226c5e1b529d484f
Debola11 <adebolabalogun5@gmail.com>
2026-05-18 06:28:00 +0100

    fix: correct type signature in dialog.tsx

--- stx-Frontend/stx-tips-frontendv1/src/components/ui/dialog.tsx
+++ stx-Frontend/stx-tips-frontendv1/src/components/ui/dialog.tsx
@@ -29,3 +29,5 @@ DialogOverlay.displayName = DialogPrimitive.Overlay.displayName
 
 const DialogContent = React.forwardRef<
   React.ElementRef<typeof DialogPrimitive.Content>,
+  React.ComponentPropsWithoutRef<typeof DialogPrimitive.Content>
+>(({ className
