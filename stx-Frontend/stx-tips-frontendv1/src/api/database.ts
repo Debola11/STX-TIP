c462a12a8558dbce87d47275d7bcbf084724e378
Debola11 <adebolabalogun5@gmail.com>
2026-05-18 05:10:25 +0100

    feat: wire up dependencies in database.ts

--- stx-Frontend/stx-tips-frontendv1/src/api/database.ts
+++ stx-Frontend/stx-tips-frontendv1/src/api/database.ts
@@ -8,4 +8,8 @@ export class APIError extends Error
     constructor(public status: number, message: string)
     {
         super(message);
-      
+        this.name = 'APIError';
+    }
+}
+
+export async functi
