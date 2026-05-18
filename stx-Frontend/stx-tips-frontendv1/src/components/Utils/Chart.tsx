1eae117b9848ea6ffd2eefe92acecd72251c243f
Debola11 <adebolabalogun5@gmail.com>
2026-05-18 05:56:37 +0100

    feat: complete core of Chart.tsx

--- stx-Frontend/stx-tips-frontendv1/src/components/Utils/Chart.tsx
+++ stx-Frontend/stx-tips-frontendv1/src/components/Utils/Chart.tsx
@@ -2,4 +2,5 @@ import { ChartData } from '@/lib/type';
 import React from 'react';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
 
-interface ChartPro
+interface ChartProps {
+  data: ChartDat
