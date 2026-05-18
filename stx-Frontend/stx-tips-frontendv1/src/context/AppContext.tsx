3bdb607d6fd06cff927348c8aa9606d06163e1d4
Debola11 <adebolabalogun5@gmail.com>
2026-05-18 15:33:40 +0100

    security: restrict exports in AppContext.tsx

--- stx-Frontend/stx-tips-frontendv1/src/context/AppContext.tsx
+++ stx-Frontend/stx-tips-frontendv1/src/context/AppContext.tsx
@@ -39,4 +39,5 @@ export const AppProvider: React.FC<{ children: React.ReactNode }> = ({ children 
 
 export const useAppContext = () =>
 {
-  const context =
+  const context = useContext(AppContext);
+ 
