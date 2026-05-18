c0f9a65aa9f2126fd7e6b641947c094a9b5650cb
Debola11 <adebolabalogun5@gmail.com>
2026-05-18 05:56:33 +0100

    feat: flesh out Chart.tsx implementation

--- stx-Frontend/stx-tips-frontendv1/src/components/Utils/Chart.tsx
+++ stx-Frontend/stx-tips-frontendv1/src/components/Utils/Chart.tsx
@@ -1,3 +1,5 @@
 import { ChartData } from '@/lib/type';
 import React from 'react';
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts'
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+
+interface ChartPro
