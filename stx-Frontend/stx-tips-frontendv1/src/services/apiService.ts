fae0b198b278cab2b27125024e31224eb4d690be
Debola11 <adebolabalogun5@gmail.com>
2026-05-18 16:20:28 +0100

    refactor: improve readability of apiService.ts

--- stx-Frontend/stx-tips-frontendv1/src/services/apiService.ts
+++ stx-Frontend/stx-tips-frontendv1/src/services/apiService.ts
@@ -14,4 +14,5 @@ export const fetchUserWalletInfo = async (walletAddress: string) =>
         return data;
     } catch (error)
     {
-        console.error('Error fetching user w
+        console.error('Error fetching user wallet info:', error);
+    
