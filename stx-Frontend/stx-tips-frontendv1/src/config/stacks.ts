6d844f342bd47ff31200bbf5e3246893c9ac9094
Debola11 <adebolabalogun5@gmail.com>
2026-05-18 15:28:44 +0100

    refactor: extract helper from stacks.ts

--- stx-Frontend/stx-tips-frontendv1/src/config/stacks.ts
+++ stx-Frontend/stx-tips-frontendv1/src/config/stacks.ts
@@ -2,4 +2,4 @@ import { STACKS_TESTNET, STACKS_MAINNET } from '@stacks/network';
 
 // Update these based on your deployment environment
 export const IS_MAINNET = false;
-export const NETWORK = IS_MAINNET ? STA
+export const NETWORK = IS_MAINNET ? STACKS_MAINNET 
