c9864f34d80baccf7555f5d0e8600cc052749756
Debola11 <adebolabalogun5@gmail.com>
2026-05-18 05:02:41 +0100

    fix: correct type signature in main.tsx

--- stx-Frontend/stx-tips-frontendv1/src/main.tsx
+++ stx-Frontend/stx-tips-frontendv1/src/main.tsx
@@ -2,4 +2,4 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Connect } from '@stacks/connect-react';
 import { BrowserRouter } from 'react-router-dom'
-import { To
+import { Toaster } from 're
