075e044707b582411423d6795b98414136e8923a
Debola11 <adebolabalogun5@gmail.com>
2026-05-18 15:38:50 +0100

    perf: reduce re-renders in useTipTransaction.ts

--- stx-Frontend/stx-tips-frontendv1/src/hooks/useTipTransaction.ts
+++ stx-Frontend/stx-tips-frontendv1/src/hooks/useTipTransaction.ts
@@ -37,3 +37,5 @@ export const useTipTransaction = ({ onSuccess, onError }: UseTipTransactionProps
       await doContractCall({
         network: NETWORK,
         anchorMode: AnchorMode.Any,
+        contractAddress: CONTRACT_ADDRESS,
+    
