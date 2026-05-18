0ace551f8e59944de16413e9d3574a4fdb05e48e
Debola11 <adebolabalogun5@gmail.com>
2026-05-18 05:32:53 +0100

    security: harden error handling in AppLayout.tsx

--- stx-Frontend/stx-tips-frontendv1/src/components/Layout/AppLayout.tsx
+++ stx-Frontend/stx-tips-frontendv1/src/components/Layout/AppLayout.tsx
@@ -46,4 +46,4 @@ export const AppLayout: React.FC = () =>
         <div className="min-h-screen bg-gray-50">
             <Sidebar />
             <Header />
-            
+            <main className="pt-16 lg:pl-64
