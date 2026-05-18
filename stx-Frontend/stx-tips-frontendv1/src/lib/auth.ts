8f624dc884ab760faf4687f50e4ae73243064cc9
Debola11 <adebolabalogun5@gmail.com>
2026-05-18 15:44:55 +0100

    perf: memoize heavy computations in auth.ts

--- stx-Frontend/stx-tips-frontendv1/src/lib/auth.ts
+++ stx-Frontend/stx-tips-frontendv1/src/lib/auth.ts
@@ -7,4 +7,4 @@ export function authenticate() {
   showConnect({
     appDetails: {
       name: "TipStack",
-      icon: "https://raw.githubusercontent.com/stacksgov/stacksgov.g
+      icon: "https://raw.githubusercontent.com/stacksgov/stacksgov.github.io/maste
