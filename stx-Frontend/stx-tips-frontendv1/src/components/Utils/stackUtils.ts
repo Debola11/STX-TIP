4daa589adc88718836de7e117a14a0f7185fe7f7
Debola11 <adebolabalogun5@gmail.com>
2026-05-18 06:10:37 +0100

    feat: add primary logic to stackUtils.ts

--- stx-Frontend/stx-tips-frontendv1/src/components/Utils/stackUtils.ts
+++ stx-Frontend/stx-tips-frontendv1/src/components/Utils/stackUtils.ts
@@ -3,4 +3,8 @@
 // // Convert STX amount to microstacks
 // export const toMicroStacks = (amount: string): number =>
 // {
-//     return Math.floor(parseFloat(amount) * 1000000)
+//     return Math.floor(parseFloat(amount) * 1000000);
+// };
+
+// // Prepare contract call arguments
+// export
