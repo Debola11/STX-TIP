1e4e9a450f334eec9ea06605f108c7916f5d3ecc
Debola11 <adebolabalogun5@gmail.com>
2026-05-18 06:16:28 +0100

    feat: complete core of alert-dialog.tsx

--- stx-Frontend/stx-tips-frontendv1/src/components/ui/alert-dialog.tsx
+++ stx-Frontend/stx-tips-frontendv1/src/components/ui/alert-dialog.tsx
@@ -19,4 +19,8 @@ const AlertDialogOverlay = React.forwardRef<
       "fixed inset-0 z-50 bg-black/80 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0",
       className
     )}
-    {..
+    {...props}
+    ref={ref}
+  />
+))
+AlertDialogOverlay.displayName = AlertDialogPrimitive.Overl
