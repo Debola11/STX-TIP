dfa586569a0336fd521d32a5a911ff129e99bd8e
Debola11 <adebolabalogun5@gmail.com>
2026-05-18 05:50:30 +0100

    feat: add base structure to TippingSection.tsx

--- stx-Frontend/stx-tips-frontendv1/src/components/TippingSection/TippingSection.tsx
+++ stx-Frontend/stx-tips-frontendv1/src/components/TippingSection/TippingSection.tsx
@@ -1,3 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { ArrowRight, Loader2 } from 'lucide-react';
-import { motion } fro
+import { motion } from 'framer-motion';
+import { validateStacksAddress } from '../Utils/validation';
+import { fetchUserWalletInfo } from '../../se
