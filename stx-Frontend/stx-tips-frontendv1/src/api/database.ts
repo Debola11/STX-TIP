83b98df0ce2645dd44b519f00e0f03f3c10fffc0
Debola11 <adebolabalogun5@gmail.com>
2026-05-18 05:10:14 +0100

    feat: define core exports in database.ts

--- stx-Frontend/stx-tips-frontendv1/src/api/database.ts
+++ stx-Frontend/stx-tips-frontendv1/src/api/database.ts
@@ -1,4 +1,6 @@
 import { UserWalletData } from "@/lib/type";
 
 // const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'https://api.yourbackend.com';
-const API_BASE_URL = 
+const API_BASE_URL = 'https://api.yourbackend.com';
+
+export class APIError 
