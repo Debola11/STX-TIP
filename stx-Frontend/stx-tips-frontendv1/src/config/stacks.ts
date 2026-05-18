33bf425742fe69562d2cc9e75584ba3142ce3bdc
Debola11 <adebolabalogun5@gmail.com>
2026-05-18 15:28:55 +0100

    refactor: improve readability of stacks.ts

--- stx-Frontend/stx-tips-frontendv1/src/config/stacks.ts
+++ stx-Frontend/stx-tips-frontendv1/src/config/stacks.ts
@@ -4,4 +4,4 @@ import { STACKS_TESTNET, STACKS_MAINNET } from '@stacks/network';
 export const IS_MAINNET = false;
 export const NETWORK = IS_MAINNET ? STACKS_MAINNET : STACKS_TESTNET;
 
-// Se
+// Set the approp
