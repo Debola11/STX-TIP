3e3a467e6efac07ac22e41e55d8b6fe0804da883
Debola11 <adebolabalogun5@gmail.com>
2026-05-18 15:25:28 +0100

    fix: patch edge case in input.tsx

--- stx-Frontend/stx-tips-frontendv1/src/components/ui/input.tsx
+++ stx-Frontend/stx-tips-frontendv1/src/components/ui/input.tsx
@@ -3,4 +3,5 @@ import * as React from "react"
 import { cn } from "@/lib/utils"
 
 const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
-  ({ className, type, ...prop
+  ({ className, type, ...props }, ref) => {
+ 
