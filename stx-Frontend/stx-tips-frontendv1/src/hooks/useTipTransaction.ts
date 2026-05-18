e6f0abfb68b2571818b8c904894eab6b35018690
Debola11 <adebolabalogun5@gmail.com>
2026-05-18 15:37:28 +0100

    feat: implement main handler in useTipTransaction.ts

--- stx-Frontend/stx-tips-frontendv1/src/hooks/useTipTransaction.ts
+++ stx-Frontend/stx-tips-frontendv1/src/hooks/useTipTransaction.ts
@@ -2,4 +2,4 @@ import { useState } from 'react';
 import { useConnect } from '@stacks/connect-react';
 import { AnchorMode, PostConditionMode } from '@stacks/transactions';
 import { NETWORK, CONTRACT_ADDRESS, CONTRACT_NAME } from '@/config/stacks';
-imp
+import { prepareTipArgs } from '../components/Util
