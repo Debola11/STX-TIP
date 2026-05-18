6a6479c62dd4d0b70cb63c4f9cae42cf897d88c0
Debola11 <adebolabalogun5@gmail.com>
2026-05-18 05:36:18 +0100

    feat: add primary logic to Sidebar.tsx

--- stx-Frontend/stx-tips-frontendv1/src/components/Navigation/Sidebar.tsx
+++ stx-Frontend/stx-tips-frontendv1/src/components/Navigation/Sidebar.tsx
@@ -1,3 +1,6 @@
 import React from 'react';
 import { Trophy, History, Gift, Settings, LogOut, X } from 'lucide-react';
 import { useAppContext } from '../../context/AppContext';
+import { NavItem } from '@/lib/type';
+
+e
