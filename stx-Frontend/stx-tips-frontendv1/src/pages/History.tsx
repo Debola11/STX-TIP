506c7bb9af5dbd24a6277eaf2bcf5dcd85e69dc8
Debola11 <adebolabalogun5@gmail.com>
2026-05-18 16:05:27 +0100

    chore: remove unused vars in History.tsx

--- stx-Frontend/stx-tips-frontendv1/src/pages/History.tsx
+++ stx-Frontend/stx-tips-frontendv1/src/pages/History.tsx
@@ -26,4 +26,5 @@ export const History: React.FC = () => {
             placeholder="Search transactions..."
             className="w-full pl-10 pr-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-violet-600"
             value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
       
