db287cbee1026289cde915a874b77afe354dbfc7
Debola11 <adebolabalogun5@gmail.com>
2026-05-18 05:57:08 +0100

    refactor: consolidate exports in Chart.tsx

--- stx-Frontend/stx-tips-frontendv1/src/components/Utils/Chart.tsx
+++ stx-Frontend/stx-tips-frontendv1/src/components/Utils/Chart.tsx
@@ -9,4 +9,5 @@ interface ChartProps {
 export const Chart: React.FC<ChartProps> = ({ data }) => (
   <div className="h-80">
     <ResponsiveContainer width="100%" height="100%">
-      <LineChart data={
+      <LineChart data={data}>
+        <Carte
