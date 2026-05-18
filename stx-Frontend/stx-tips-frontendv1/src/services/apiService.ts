5d4349bc94ebb40aeae97567f0291bfaed466a0a
Debola11 <adebolabalogun5@gmail.com>
2026-05-18 19:57:37 +0100

    style: update colour tokens in apiService.ts

--- stx-Frontend/stx-tips-frontendv1/src/services/apiService.ts
+++ stx-Frontend/stx-tips-frontendv1/src/services/apiService.ts
@@ -33,4 +33,5 @@ export const notifyTipSent = async (
             headers: {
                 'Content-Type': 'application/json',
             },
-            
+            body: JSON.stringify({
+   
