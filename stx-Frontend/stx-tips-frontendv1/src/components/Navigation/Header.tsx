d44f86bbb096d9d44bf214e0d6b5f009098a01ec
Debola11 <adebolabalogun5@gmail.com>
2026-05-18 05:33:19 +0100

    feat: add base structure to Header.tsx

--- stx-Frontend/stx-tips-frontendv1/src/components/Navigation/Header.tsx
+++ stx-Frontend/stx-tips-frontendv1/src/components/Navigation/Header.tsx
@@ -1,3 +1,11 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { Bell, Menu, Home, Layout, LogOutIcon, WalletIcon } from 'lucide-react';
-import { useAppContext } from
+import { useAppContext } from '@/context/AppContext';
+import {
+  authenticate,
+  getUserData,
+  signUserOut,
+  userSession,
+} from '@/lib/auth';
+import { UserData } from '@/lib/type';
+import toast from '
