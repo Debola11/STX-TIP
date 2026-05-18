11bfc4c64e01f547bd6350c99417ead97077ab15
Debola11 <adebolabalogun5@gmail.com>
2026-05-18 05:24:22 +0100

    test: cover main path in HowItWorksSection.tsx

--- stx-Frontend/stx-tips-frontendv1/src/components/Landing/HowItWorksSection.tsx
+++ stx-Frontend/stx-tips-frontendv1/src/components/Landing/HowItWorksSection.tsx
@@ -91,4 +91,5 @@ export const HowItWorksSection = () => {
                     {step.description}
                   </p>
                   <div className="absolute bottom-4 right-4 opacity-0 group-hover:opacity-100 transition-opacity">
-             
+                    <ArrowRight className="w-5 h-5 text-violet-500" />
+                
