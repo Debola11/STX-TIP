98288f21c4617d40c287de512ca0826436ffea25
Debola11 <adebolabalogun5@gmail.com>
2026-05-18 04:41:25 +0100

    updated frontend

--- stx-Frontend/stx-tips-frontendv4/src/components/Utils/validation.ts
+++
@@ -1,4 +0,0 @@
-export const validateStacksAddress = (address: string): boolean => {
-    return address.startsWith('ST') || address.startsWith('SP') && address.length >= 41;
-    // return true;
-  };
