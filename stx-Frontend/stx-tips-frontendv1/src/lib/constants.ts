59438aa40c1f47773e81562903ea46d07599d73c
Debola11 <adebolabalogun5@gmail.com>
2026-05-18 15:47:22 +0100

    refactor: simplify logic in constants.ts

--- stx-Frontend/stx-tips-frontendv1/src/lib/constants.ts
+++ stx-Frontend/stx-tips-frontendv1/src/lib/constants.ts
@@ -1,3 +1,3 @@
 // Network configuration
 export const NETWORK_CONFIG = {
-    API_URL: 'https://api.platform.hiro.so/v1/ext/0833814e84b812825680a1419eb34308/stacks-blockc
+    API_URL: 'https://api.platform.hiro.so/v1/ext/0833814e84b812825680a1419eb34308/stacks-blockchain-api'
