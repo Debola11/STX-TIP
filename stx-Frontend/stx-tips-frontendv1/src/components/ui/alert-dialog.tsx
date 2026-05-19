fcc22298948dc56b399f242400494d305f5a30da
Debola11 <adebolabalogun5@gmail.com>
2026-05-19 12:06:36 +0100

    feat: define core exports in alert-dialog.tsx

--- stx-Frontend/stx-tips-frontendv1/src/components/ui/alert-dialog.tsx
+++ stx-Frontend/stx-tips-frontendv1/src/components/ui/alert-dialog.tsx
@@ -6,4 +6,8 @@ import { buttonVariants } from "@/components/ui/button"
 
 const AlertDialog = AlertDialogPrimitive.Root
 
-const AlertDialogTrigger = Al
+const AlertDialogTrigger = AlertDialogPrimitive.Trigger
+
+const AlertDialogPortal = AlertDialogPrimitive.Portal
+
+const 
