7b1dfb6e0754993d530e5136c3d0a46ef0887d99
Debola11 <adebolabalogun5@gmail.com>
2026-05-19 10:57:17 +0100

    feat: add primary logic to database.ts

--- stx-Frontend/stx-tips-frontendv1/src/api/database.ts
+++ stx-Frontend/stx-tips-frontendv1/src/api/database.ts
@@ -3,4 +3,6 @@ import { UserWalletData } from "@/lib/type";
 // const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'https://api.yourbackend.com';
 const API_BASE_URL = 'https://api.yourbackend.com';
 
-export class APIError 
+export class APIError extends Error
+{
+    constructor(public status: number,
