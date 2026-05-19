d8984a057f0fd3eddcae1e61a9cf3fa0ae65d12f
Debola11 <adebolabalogun5@gmail.com>
2026-05-19 10:36:15 +0100

    style: update colour tokens in tailwind.config.js

--- stx-Frontend/stx-tips-frontendv1/tailwind.config.js
+++ stx-Frontend/stx-tips-frontendv1/tailwind.config.js
@@ -34,4 +34,6 @@ export default {
   			},
   			accent: {
   				DEFAULT: 'hsl(var(--accent))',
-  				foreground: 'hsl(var(--accent-foreground)
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			destructive: {
