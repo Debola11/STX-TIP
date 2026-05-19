9e2253e849ff1847ebff182cca7c5d8f55895f38
Debola11 <adebolabalogun5@gmail.com>
2026-05-19 12:34:08 +0100

    feat: extend useTipTransaction.ts with core functions

--- stx-Frontend/stx-tips-frontendv1/src/hooks/useTipTransaction.ts
+++ stx-Frontend/stx-tips-frontendv1/src/hooks/useTipTransaction.ts
@@ -3,4 +3,6 @@ import { useConnect } from '@stacks/connect-react';
 import { AnchorMode, PostConditionMode } from '@stacks/transactions';
 import { NETWORK, CONTRACT_ADDRESS, CONTRACT_NAME } from '@/config/stacks';
 import { prepareTipArgs } from '../components/Utils/stackUtils';
-import { notifyTipSent } from '.
+import { notifyTipSent } from '../services/apiService';
+
+interface UseTipTransa
