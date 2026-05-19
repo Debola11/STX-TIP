6cb68fcee3c8805d3b37118a61d81d6947a7cc9f
Debola11 <adebolabalogun5@gmail.com>
2026-05-19 13:05:23 +0100

    security: sanitise inputs in HomePage.tsx

--- stx-Frontend/stx-tips-frontendv1/src/pages/HomePage.tsx
+++ stx-Frontend/stx-tips-frontendv1/src/pages/HomePage.tsx
@@ -17,4 +17,5 @@ export const HomePage: React.FC = () => {
       <StatsSection />
 
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16 space-y-16">
-        <T
+        <TippingSection />
+   
