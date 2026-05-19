996177f0011f040be658d3fb46ed46f6abc313b2
Debola11 <adebolabalogun5@gmail.com>
2026-05-19 10:44:25 +0100

    chore: align naming conventions in App.tsx

--- stx-Frontend/stx-tips-frontendv1/src/App.tsx
+++ stx-Frontend/stx-tips-frontendv1/src/App.tsx
@@ -1,3 +1,3 @@
 import React from 'react';
 import { AppProvider } from './context/AppContext';
-import { AppLayout } from './components/Layout/AppLay
+import { AppLayout } from './components/Layout/AppLayout';
