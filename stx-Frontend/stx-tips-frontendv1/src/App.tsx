ea4c5b16814310e75138e1747882551585200978
Debola11 <adebolabalogun5@gmail.com>
2026-05-19 10:43:04 +0100

    feat: initialise App.tsx

--- stx-Frontend/stx-tips-frontendv1/src/App.tsx
+++ stx-Frontend/stx-tips-frontendv1/src/App.tsx
@@ -1,13 +1,1 @@
-import React from 'react';
-import { AppProvider } from './context/AppContext';
-import { AppLayout } from './components/Layout/AppLayout';
-
-const App: React.FC = () => {
-  return (
-    <AppProvider>
-      <AppLayout />
-    </AppProvider>
-  );
-};
-
-export default App;
+import
