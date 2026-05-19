2e36e13a94b05d7f3d9048a0795382a241cdf2b8
Debola11 <adebolabalogun5@gmail.com>
2026-05-19 12:25:34 +0100

    fix: correct type signature in stacks.ts

--- stx-Frontend/stx-tips-frontendv1/src/config/stacks.ts
+++ stx-Frontend/stx-tips-frontendv1/src/config/stacks.ts
@@ -1,4 +1,5 @@
 import { STACKS_TESTNET, STACKS_MAINNET } from '@stacks/network';
 
 // Update these based on your deployment environment
-export const IS_MAINNET 
+export const IS_MAINNET = false;
+exp
