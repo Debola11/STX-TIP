b1438e3f3dc04dbf03863a972bd59a9faa4985bc
Debola11 <adebolabalogun5@gmail.com>
2026-05-19 12:13:05 +0100

    feat: extend button.tsx with core functions

--- stx-Frontend/stx-tips-frontendv1/src/components/ui/button.tsx
+++ stx-Frontend/stx-tips-frontendv1/src/components/ui/button.tsx
@@ -5,4 +5,4 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap ro
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition
