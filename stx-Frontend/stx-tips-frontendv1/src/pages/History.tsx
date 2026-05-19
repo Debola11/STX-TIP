ca904717c674fe3cccf76646e8673631717b300c
Debola11 <adebolabalogun5@gmail.com>
2026-05-19 13:01:01 +0100

    refactor: consolidate exports in History.tsx

--- stx-Frontend/stx-tips-frontendv1/src/pages/History.tsx
+++ stx-Frontend/stx-tips-frontendv1/src/pages/History.tsx
@@ -20,4 +20,5 @@ export const History: React.FC = () => {
 
       <div className="flex flex-col sm:flex-row gap-4">
         <div className="relative flex-1">
-          <Search className="absolute left-3 top-1/2
+          <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 h-5 w-5" />
+      
