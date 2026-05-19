5005670f89bdcf5a892cac43e8cec9348fa93686
Debola11 <adebolabalogun5@gmail.com>
2026-05-19 13:09:57 +0100

    refactor: clean up imports in Settings.tsx

--- stx-Frontend/stx-tips-frontendv1/src/pages/Settings.tsx
+++ stx-Frontend/stx-tips-frontendv1/src/pages/Settings.tsx
@@ -52,4 +52,4 @@ export const Settings: React.FC = () => {
                   onChange={(e) => setNotifications({ ...notifications, milestones: e.target.checked })}
                   className="sr-only peer"
                 />
-                <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-violet-300 rounded-full peer peer-ch
+                <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-violet-300 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:bord
