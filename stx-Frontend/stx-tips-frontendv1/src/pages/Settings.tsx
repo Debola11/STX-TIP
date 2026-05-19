1064e39a4e423fc821dd7765c3c6a240d8309921
Debola11 <adebolabalogun5@gmail.com>
2026-05-19 13:10:52 +0100

    style: update layout in Settings.tsx

--- stx-Frontend/stx-tips-frontendv1/src/pages/Settings.tsx
+++ stx-Frontend/stx-tips-frontendv1/src/pages/Settings.tsx
@@ -92,4 +92,8 @@ export const Settings: React.FC = () => {
                   onChange={(e) => setPrivacy({ ...privacy, public: e.target.checked })}
                   className="sr-only peer"
                 />
-                <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-violet-300 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 af
+                <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-violet-300 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-violet-600"></div>
+              </label>
+            </div>
+
+            <div className
