d752e97fbaee8efe080cadcc829dfec4a07ab354
Debola11 <adebolabalogun5@gmail.com>
2026-05-19 13:04:38 +0100

    perf: lazy-load dependencies in HomePage.tsx

--- stx-Frontend/stx-tips-frontendv1/src/pages/HomePage.tsx
+++ stx-Frontend/stx-tips-frontendv1/src/pages/HomePage.tsx
@@ -6,4 +6,6 @@ import { HowItWorksSection } from '@/components/Landing/HowItWorksSection';
 import { FAQSection } from '@/components/Landing/FAQSection';
 import { NewsletterSection } from '@/components/Landing/NewsletterSection';
 import { Footer } from '@/components/Footer/Footer';
-import { TippingSection } from '@/components/TippingSection/Tip
+import { TippingSection } from '@/components/TippingSection/TippingSection';
+
+expor
