b11ce373c4fdf175cc1d12e5d118d5acf97c99d1
Debola11 <adebolabalogun5@gmail.com>
2026-05-21 12:40:03 +0100

    feat: complete core of app.js

--- stx-tips-Backend/stx-tips-backendv1/app.js
+++ stx-tips-Backend/stx-tips-backendv1/app.js
@@ -2,4 +2,5 @@ import express from "express";
 import cors from "cors";
 import morgan from "morgan";
 import helmet from "helmet";
-import globalErrorHandler from "./errorHandlers/errorHand
+import globalErrorHandler from "./errorHandlers/errorHandler.js";
+import { m
