e1e9f0bbafa90b358ec42182f9d18fdecc00a1fc
Debola11 <adebolabalogun5@gmail.com>
2026-05-21 12:51:53 +0100

    feat: add base structure to UserController.js

--- stx-tips-Backend/stx-tips-backendv1/controllers/UserController.js
+++ stx-tips-Backend/stx-tips-backendv1/controllers/UserController.js
@@ -1,3 +1,5 @@
 import Email from "../Emails/email.js";
 import catchAsync from "../utils/catchAsync.js";
-import jwt from "
+import jwt from "jsonwebtoken";
+import User from "../models/UserModel.js";
+import AppError from "../errorHandlers/appError.
