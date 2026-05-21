08e0f14147674570ae172d8b3be980804870b6c7
Debola11 <adebolabalogun5@gmail.com>
2026-05-21 14:49:42 +0100

    docs: add usage example for database.js

--- stx-tips-Backend/stx-tips-backendv1/utils/database.js
+++ stx-tips-Backend/stx-tips-backendv1/utils/database.js
@@ -5,4 +5,5 @@ const connectDB = async (db) => {
     const con = await mongoose.connect(db);
     console.log(`Database connected with ${con.connection.host}`);
   } catch (err) {
-    console.log(e
+    console.log(err);
+ 
