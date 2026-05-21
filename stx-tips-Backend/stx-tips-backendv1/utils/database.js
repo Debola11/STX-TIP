70a9f0db1c5ae538ac218c598257633edc026289
Debola11 <adebolabalogun5@gmail.com>
2026-05-21 14:49:09 +0100

    perf: optimise query builder in database.js

--- stx-tips-Backend/stx-tips-backendv1/utils/database.js
+++ stx-tips-Backend/stx-tips-backendv1/utils/database.js
@@ -3,4 +3,4 @@ import mongoose from "mongoose";
 const connectDB = async (db) => {
   try {
     const con = await mongoose.connect(db);
-    console.log(`Database connected with ${con.connect
+    console.log(`Database connected with ${con.connection.ho
