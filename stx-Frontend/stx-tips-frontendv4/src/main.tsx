6260856401ee80866b78352e252aa7d8a2145738
Debola11 <adebolabalogun5@gmail.com>
2026-05-21 16:45:13 +0100

    feat: flesh out main.tsx implementation

--- stx-Frontend/stx-tips-frontendv4/src/main.tsx
+++ stx-Frontend/stx-tips-frontendv4/src/main.tsx
@@ -1,3 +1,4 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { Connect } from '@stacks/connect-reac
+import { Connect } from '@stacks/connect-react';
+import { Bro
