9e600c946b91becd6617e7aaf4ea80d9259f139e
Debola11 <adebolabalogun5@gmail.com>
2026-05-21 13:00:42 +0100

    test: add edge case coverage for errorHandler.js

--- stx-tips-Backend/stx-tips-backendv1/errorHandlers/errorHandler.js
+++ stx-tips-Backend/stx-tips-backendv1/errorHandlers/errorHandler.js
@@ -104,4 +104,5 @@ export default (err, req, res, next) => {
     if (error.code === 11000) error = handleDuplicateFieldsDB(error);
     if (error.name === "ValidationError")
       error = handleValidationErrorDB(error);
+    if (error.name === "JsonWebTokenError") error = handleJWTError();
     if (error.name ===
