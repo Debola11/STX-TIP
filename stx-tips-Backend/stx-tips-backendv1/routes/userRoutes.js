881d3da4257c01aaf5e17897bccd2b1f7ab5f81f
Debola11 <adebolabalogun5@gmail.com>
2026-05-21 14:43:23 +0100

    docs: add usage example for userRoutes.js

--- stx-tips-Backend/stx-tips-backendv1/routes/userRoutes.js
+++ stx-tips-Backend/stx-tips-backendv1/routes/userRoutes.js
@@ -20,4 +20,4 @@ router.route("/send-tip").post(sendTipNotification);
 
 router.route("/wallet/:wallet").get(getUserByWallet);
 
-router.route("/email/:email")
+router.route("/email/:email").get(getUserBy
