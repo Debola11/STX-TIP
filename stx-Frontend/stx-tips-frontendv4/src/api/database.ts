cfe3f82f95c723da41a5cac7c2e3ec022be829ff
Debola11 <adebolabalogun5@gmail.com>
2026-05-21 16:54:50 +0100

    feat: scaffold database.ts module

--- stx-Frontend/stx-tips-frontendv4/src/api/database.ts
+++ stx-Frontend/stx-tips-frontendv4/src/api/database.ts
@@ -1,3 +1,4 @@
 import { UserWalletData } from "@/lib/type";
 
-// const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL |
+// const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'https://api.yourbackend.com';
+const API_BASE_URL = 
