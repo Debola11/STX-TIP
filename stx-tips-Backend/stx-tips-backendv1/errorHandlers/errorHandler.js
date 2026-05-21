1bfa7fe190ecc0d2bcd3f82855f6edcf36dd2696
Debola11 <adebolabalogun5@gmail.com>
2026-05-21 12:59:55 +0100

    docs: add JSDoc to errorHandler.js

--- stx-tips-Backend/stx-tips-backendv1/errorHandlers/errorHandler.js
+++ stx-tips-Backend/stx-tips-backendv1/errorHandlers/errorHandler.js
@@ -85,4 +85,7 @@ const sendErrorProd = (err, req, res) => {
 export default (err, req, res, next) => {
   // console.log(err.stack);
 
-  err.statusCode = err.statusCod
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
+
+  if (process.env.NOD
