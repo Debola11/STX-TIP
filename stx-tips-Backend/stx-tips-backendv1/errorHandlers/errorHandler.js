a859cc1469333f23c16ab94b1641e41075b393a2
Debola11 <adebolabalogun5@gmail.com>
2026-05-21 12:58:11 +0100

    feat: extend errorHandler.js with core functions

--- stx-tips-Backend/stx-tips-backendv1/errorHandlers/errorHandler.js
+++ stx-tips-Backend/stx-tips-backendv1/errorHandlers/errorHandler.js
@@ -13,4 +13,6 @@ const handleDuplicateFieldsDB = (err) => {
 };
 
 const handleValidationErrorDB = (err) => {
-  const errors = Object.values(err.errors).map((el) 
+  const errors = Object.values(err.errors).map((el) => el.message);
+
+  const message = `Invalid input data. ${errors.join(
