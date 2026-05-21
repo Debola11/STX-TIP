3220cde382b2ba961dbed30b6119c80187c65621
Debola11 <adebolabalogun5@gmail.com>
2026-05-21 12:59:59 +0100

    docs: document middleware in errorHandler.js

--- stx-tips-Backend/stx-tips-backendv1/errorHandlers/errorHandler.js
+++ stx-tips-Backend/stx-tips-backendv1/errorHandlers/errorHandler.js
@@ -88,4 +88,5 @@ export default (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "error";
 
-  if (process.env.NOD
+  if (process.env.NODE_ENV === "development") {
+    if (err.name === "CastError") err = han
