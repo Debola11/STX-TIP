08c67ac32e4f91375df52b6ee9bd5c5d0e3a3a07
Debola11 <adebolabalogun5@gmail.com>
2026-05-21 12:59:03 +0100

    chore: update constants in errorHandler.js

--- stx-tips-Backend/stx-tips-backendv1/errorHandlers/errorHandler.js
+++ stx-tips-Backend/stx-tips-backendv1/errorHandlers/errorHandler.js
@@ -49,4 +49,7 @@ const sendErrorProd = (err, req, res) => {
   if (req.originalUrl.startsWith("/api")) {
     // A) Operational, trusted error: send message to client
     if (err.isOperational) {
-      return res.status(err.statusCode).json(
+      return res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message,
+      });
