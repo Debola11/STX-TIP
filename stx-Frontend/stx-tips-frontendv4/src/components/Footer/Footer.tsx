bd8944f0445f0895c3681ab829697c5f1ad90e8e
Debola11 <adebolabalogun5@gmail.com>
2026-05-21 16:58:10 +0100

    feat: add base structure to Footer.tsx

--- stx-Frontend/stx-tips-frontendv4/src/components/Footer/Footer.tsx
+++ stx-Frontend/stx-tips-frontendv4/src/components/Footer/Footer.tsx
@@ -3,4 +3,8 @@
 
 // export const Footer: React.FC = () => {
 //   return (
-//     <footer className="bg-gray-900 text-white 
+//     <footer className="bg-gray-900 text-white py-12">
+//       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+//         <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
+//           <div className="space-y-4">
+//         
