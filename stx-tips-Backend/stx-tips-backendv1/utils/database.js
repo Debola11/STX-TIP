951a15f4cf505bf347787bcd4a86263e6fc02f86
Debola11 <adebolabalogun5@gmail.com>
2026-05-21 14:48:58 +0100

    perf: reduce db round trips in database.js

--- stx-tips-Backend/stx-tips-backendv1/utils/database.js
+++ stx-tips-Backend/stx-tips-backendv1/utils/database.js
@@ -3,4 +3,4 @@ import mongoose from "mongoose";
 const connectDB = async (db) => {
   try {
     const con = await mongoose.connect(db);
-    console.log(`Database connected 
+    console.log(`Database connected with $
