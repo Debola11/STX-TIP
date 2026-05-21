6a012ce48739b7191d92b749d8f143f24cf434eb
Debola11 <adebolabalogun5@gmail.com>
2026-05-21 12:44:43 +0100

    docs: add JSDoc to server.js

--- stx-tips-Backend/stx-tips-backendv1/server.js
+++ stx-tips-Backend/stx-tips-backendv1/server.js
@@ -22,4 +22,5 @@ const port = process.env.PORT || 3000;
 
 // Listen to the port
 const server = app.listen(port, () => {
-  console.log(`App running on port ${port}..
+  console.log(`App running on port ${port}...`);
+  connectDB(D
