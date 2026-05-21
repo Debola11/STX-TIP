4887e0d2e0925de65bdf6fd736a78a8e51440fef
Debola11 <adebolabalogun5@gmail.com>
2026-05-21 13:01:53 +0100

    refactor: extract helper from UserModel.js

--- stx-tips-Backend/stx-tips-backendv1/models/UserModel.js
+++ stx-tips-Backend/stx-tips-backendv1/models/UserModel.js
@@ -5,4 +5,5 @@ import bcrypt from "bcryptjs";
 const userSchema = new mongoose.Schema({
     email: {
         type: String,
-        required: [true, "Please provide your em
+        required: [true, "Please provide your email"],
+       
