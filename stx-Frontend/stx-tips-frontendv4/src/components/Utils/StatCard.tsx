650b39c24341ad4c34c38ce02cb35b5faa3116c5
Debola11 <adebolabalogun5@gmail.com>
2026-05-28 18:36:15 +0100

    refactor: clean up imports in StatCard.tsx

--- stx-Frontend/stx-tips-frontendv4/src/components/Utils/StatCard.tsx
+++ stx-Frontend/stx-tips-frontendv4/src/components/Utils/StatCard.tsx
@@ -2,4 +2,4 @@ import { Stat } from '@/lib/type';
 import React from 'react';
 
 export const StatCard: React.FC<Stat> = ({ title, value, change, icon }) => (
-  <div className="bg-white rounded-xl shadow-sm p-6 ho
+  <div className="bg-white rounded-xl shadow-sm p-6 hover:shadow-md
