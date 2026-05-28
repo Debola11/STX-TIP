bb1e1e8f79633f748ccbe0b2f1d111d2c0b4558c
Debola11 <adebolabalogun5@gmail.com>
2026-05-28 18:19:15 +0100

    docs: add JSDoc to RecipientInput.tsx

--- stx-Frontend/stx-tips-frontendv4/src/components/TippingSection/RecipientInput.tsx
+++ stx-Frontend/stx-tips-frontendv4/src/components/TippingSection/RecipientInput.tsx
@@ -68,4 +68,6 @@ export const RecipientInput: React.FC<RecipientInputProps> = ({
       </AnimatePresence>
 
       <AnimatePresence>
-        
+        {userInfo.found && (
+          <motion.div 
+            initial={{ o
