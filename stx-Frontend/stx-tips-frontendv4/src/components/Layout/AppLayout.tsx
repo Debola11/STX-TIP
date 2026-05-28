bce8405e67c71d5316686951a28c127adf982fbe
Debola11 <adebolabalogun5@gmail.com>
2026-05-28 18:01:59 +0100

    feat: flesh out AppLayout.tsx implementation

--- stx-Frontend/stx-tips-frontendv4/src/components/Layout/AppLayout.tsx
+++ stx-Frontend/stx-tips-frontendv4/src/components/Layout/AppLayout.tsx
@@ -3,4 +3,5 @@ import { useAppContext } from '../../context/AppContext';
 import { Sidebar } from '../Navigation/Sidebar';
 import { Header } from '../Navigation/Header';
 import { Dashboard } from '@/pages/Dashboard';
-import { History } f
+import { History } from '@/pages/History';
+import {
