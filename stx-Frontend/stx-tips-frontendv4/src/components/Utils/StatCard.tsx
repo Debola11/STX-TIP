5b016983cf38ac671ea6291b396b0049fc2b698a
Debola11 <adebolabalogun5@gmail.com>
2026-05-28 18:36:29 +0100

    refactor: improve readability of StatCard.tsx

--- stx-Frontend/stx-tips-frontendv4/src/components/Utils/StatCard.tsx
+++ stx-Frontend/stx-tips-frontendv4/src/components/Utils/StatCard.tsx
@@ -3,4 +3,4 @@ import React from 'react';
 
 export const StatCard: React.FC<Stat> = ({ title, value, change, icon }) => (
   <div className="bg-white rounded-xl shadow-sm p-6 hover:shadow-md transition-shadow">
-    <div className
+    <div className="flex items-
