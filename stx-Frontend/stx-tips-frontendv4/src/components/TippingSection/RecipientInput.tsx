f936e49a1836264832cdaca3d4d44832e0837adc
Debola11 <adebolabalogun5@gmail.com>
2026-05-28 18:19:23 +0100

    docs: add usage example for RecipientInput.tsx

--- stx-Frontend/stx-tips-frontendv4/src/components/TippingSection/RecipientInput.tsx
+++ stx-Frontend/stx-tips-frontendv4/src/components/TippingSection/RecipientInput.tsx
@@ -71,4 +71,6 @@ export const RecipientInput: React.FC<RecipientInputProps> = ({
         {userInfo.found && (
           <motion.div 
             initial={{ opacity: 0, height: 0 }}
-            animate={{ opacity: 1, height: '
+            animate={{ opacity: 1, height: 'auto' }}
+            exit={{ opacity: 0, height: 0 }}
+            cl
