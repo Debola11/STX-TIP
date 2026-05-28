74c265f494ec0ffdc4675737b4dfb426e406a1ae
Debola11 <adebolabalogun5@gmail.com>
2026-05-28 18:30:21 +0100

    refactor: simplify logic in Chart.tsx

--- stx-Frontend/stx-tips-frontendv4/src/components/Utils/Chart.tsx
+++ stx-Frontend/stx-tips-frontendv4/src/components/Utils/Chart.tsx
@@ -9,4 +9,4 @@ interface ChartProps {
 export const Chart: React.FC<ChartProps> = ({ data }) => (
   <div className="h-80">
     <ResponsiveContainer width="100%" height="100%">
-  
+      <LineChart data={
