e9223d2c876f0e9acc18468dbd5da833eb7bcb53
Debola11 <adebolabalogun5@gmail.com>
2026-05-29 16:44:34 +0100

    fix: handle null check in constants.ts

--- stx-Frontend/stx-tips-frontendv4/src/lib/constants.ts
+++ stx-Frontend/stx-tips-frontendv4/src/lib/constants.ts
@@ -1,3 +1,3 @@
 // Network configuration
 export const NETWORK_CONFIG = {
-    API_URL: 'https://api.platform.hiro.so/v1/ext/0833814e84
+    API_URL: 'https://api.platform.hiro.so/v1/ext/0833814e84b81282568
