7e4edbacb166b1ee88094c8588d780d284dbb800
Debola11 <adebolabalogun5@gmail.com>
2026-05-29 16:41:40 +0100

    fix: guard against undefined in auth.ts

--- stx-Frontend/stx-tips-frontendv4/src/lib/auth.ts
+++ stx-Frontend/stx-tips-frontendv4/src/lib/auth.ts
@@ -1,4 +1,6 @@
 import { AppConfig, UserSession, showConnect } from "@stacks/connect";
 
 export const appConfig = new AppConfig(["store_write", "publish_data"]);
-export const userSession = new UserSession({ appCon
+export const userSession = new UserSession({ appConfig });
+
+expor
